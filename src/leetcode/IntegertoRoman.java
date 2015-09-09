2758f55cb637b50b6e4e39612721e17c09f69b1d
wenlong.wang <wenlong.wang@qunar.com>
2015-09-09 20:48:59 +0800

    roman<-->integer

---
+++ src/leetcode/IntegertoRoman.java
@@ -0,0 +1,22 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年9月9日<p>
+//-------------------------------------------------------
+public class IntegertoRoman {
+
+    public String intToRoman(int num) {
+        int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String[] numerals = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < values.length; i++)
+            for (; num >= values[i]; num -= values[i], result.append(numerals[i]));
+        return result.toString();
+    }
+
+}
