eecc98e7ee43a5eb9f7db126373774724663f52f
wenlong.wang <wenlong.wang@qunar.com>
2015-09-09 20:57:05 +0800

    14

---
+++ src/leetcode/LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年9月9日<p>
+//-------------------------------------------------------
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        if (null == strs || strs.length == 0) return "";
+        StringBuilder sb = new StringBuilder();
+        A: for (int i = 0; i < strs[0].length(); sb.append(strs[0].charAt(i)), i++)
+            for (int j = 1; j < strs.length; j++)
+                if (i > strs[j].length() - 1 || strs[j].charAt(i) != strs[0].charAt(i)) break A;
+        return sb.toString();
+    }
+
+}
