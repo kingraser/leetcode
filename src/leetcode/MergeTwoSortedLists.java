97029346ec5cc04ef3c40318b07a104ff6b42dd3
wenlong.wang <wenlong.wang@qunar.com>
2015-09-10 18:57:21 +0800

    21

---
+++ src/leetcode/MergeTwoSortedLists.java
@@ -0,0 +1,26 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+import leetcode.common.ListNode;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年9月10日<p>
+//-------------------------------------------------------
+public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
