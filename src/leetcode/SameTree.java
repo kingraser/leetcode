d608a2f935eda359d25cb5139187dc31b7192f0f
wenlong.wang <wenlong.wang@qunar.com>
2015-09-11 14:31:01 +0800

    100

---
+++ src/leetcode/SameTree.java
@@ -0,0 +1,26 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+import leetcode.common.TreeNode;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年9月11日<p>
+//-------------------------------------------------------
+public class SameTree {
+
+    /*
+    Given two binary trees, write a function to check if they are equal or not.    
+    Two binary trees are considered equal if they are structurally identical and the nodes have the same value. 
+    */
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null && q != null || p != null && q == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
