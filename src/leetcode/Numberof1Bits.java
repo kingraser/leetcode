5b42b8bcfe47971d43cc8bcaa1b8964c3bfa6333
wenlong.wang <wenlong.wang@qunar.com>
2015-09-11 14:36:17 +0800

    191

---
+++ src/leetcode/Numberof1Bits.java
@@ -0,0 +1,31 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年9月11日<p>
+//-------------------------------------------------------
+public class Numberof1Bits {
+    /*
+    Write a function that takes an unsigned integer 
+    and returns the number of ’1' bits it has (also known as the Hamming weight).  
+    For example, 
+    the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, 
+    so the function should return 3.
+    */
+
+    public int hammingWeight(int n) {
+        return Integer.bitCount(n);
+    }
+
+    public int hammingWeightII(int n) {
+        int result = 0;
+        for (; n != 0; n >>>= 1)
+            if ((n & 1) == 1) result++;
+        return result;
+    }
+
+}
