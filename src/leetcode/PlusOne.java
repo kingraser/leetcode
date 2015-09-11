c09801b2e077b0daf40162340cbb6a21056accda
wenlong.wang <wenlong.wang@qunar.com>
2015-09-11 14:07:09 +0800

    66

---
+++ src/leetcode/PlusOne.java
@@ -0,0 +1,32 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年9月11日<p>
+//-------------------------------------------------------
+public class PlusOne {
+
+    public int[] plusOne(int[] digits) {
+        for (int l = digits.length - 1; l > -1;)
+            if (digits[l] == 9) digits[l--] = 0;
+            else {
+                digits[l]++;
+                return digits;
+            }
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
+    }
+
+    @Test
+    public void test() {
+        Assert.assertArrayEquals(new int[] { 1, 0, 0, 0 }, plusOne(new int[] { 9, 9, 9 }));
+    }
+}
