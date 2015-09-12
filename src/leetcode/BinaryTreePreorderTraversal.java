8aacf06f7c5c9d961051f42be14d0825975c66a6
wenlong.wang <wenlong.wang@qunar.com>
2015-09-12 19:33:17 +0800

    Binary Tree Preorder Traversal

---
+++ src/leetcode/BinaryTreePreorderTraversal.java
@@ -0,0 +1,52 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+import leetcode.common.TreeNode;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年9月12日<p>
+//-------------------------------------------------------
+public class BinaryTreePreorderTraversal {
+    /*
+    Given a binary tree, return the preorder traversal of its nodes' values.
+    
+    For example:
+    Given binary tree {1,#,2,3},
+    
+       1
+        \
+         2
+        /
+       3
+    
+    return [1,2,3]. 
+    */
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = Lists.newLinkedList();
+        if (root == null) return list;
+        list.add(root.val);
+        list.addAll(preorderTraversal(root.left));
+        list.addAll(preorderTraversal(root.right));
+        return list;
+    }
+
+    @Test
+    public void test() {
+        TreeNode n1 = new TreeNode(1), n2 = new TreeNode(2), n3 = new TreeNode(3);
+        n1.right = n2;
+        n2.left = n3;
+        Assert.assertEquals(Lists.newArrayList(1, 2, 3), preorderTraversal(n1));
+    }
+}
