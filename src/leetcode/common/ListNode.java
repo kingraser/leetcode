7fd89403c16c7ceb756137fd44327ff050c4629c
wenlong.wang <wenlong.wang@qunar.com>
2015-09-12 10:50:29 +0800

    rewrite tostring and equals

--- src/leetcode/common/ListNode.java
+++ src/leetcode/common/ListNode.java
@@ -5,8 +5,11 @@
  */
 package leetcode.common;
 
+import java.util.List;
+
 import com.google.common.base.MoreObjects;
 import com.google.common.base.MoreObjects.ToStringHelper;
+import com.google.common.collect.Lists;
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月6日<p>
@@ -24,23 +27,30 @@ public class ListNode {
 
     @Override
     public String toString() {
+        return toString(Lists.newArrayList());
+    };
+
+    private String toString(List<ListNode> fathers) {
         ListNode march = this;
         ToStringHelper helper = MoreObjects.toStringHelper(ListNode.class);
-        for (; march != null; helper.add("val", march.val), march = march.next);
+        for (; march != null && !thereIsCircle(march, fathers); fathers.add(march), helper.add("val",
+                march.val), march = march.next);
         return helper.toString();
-    };
+    }
+
+    private boolean thereIsCircle(ListNode node, List<ListNode> fathers) {
+        for (ListNode father : fathers)
+            if (node == father) return true;
+        return false;
+    }
 
     @Override
     public boolean equals(Object o) {
-        if (o instanceof ListNode) {
-            ListNode another = (ListNode) o;
-            if (val == another.val) {
-                if (next == null) {
-                    if (another.next == null) return true;
-                } else if (another.next != null) return next.equals(another.next);
-            }
-        }
-        return false;
+        if (null == o) return false;
+        if (!(o instanceof ListNode)) return false;
+        ListNode another = (ListNode) o;
+        if (val != another.val) return false;
+        return next == null ? another.next == null : next.equals(another);
     }
 
 }
