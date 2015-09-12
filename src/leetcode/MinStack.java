474a0ce595bf144e8d2270e667ee80fa5d419061
wenlong.wang <wenlong.wang@qunar.com>
2015-09-12 14:18:03 +0800

    minstack

---
+++ src/leetcode/MinStack.java
@@ -0,0 +1,45 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+import java.util.Stack;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年9月12日<p>
+//-------------------------------------------------------
+public class MinStack {
+
+    /*
+    Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+    
+    push(x) -- Push element x onto stack.
+    pop() -- Removes the element on top of the stack.
+    top() -- Get the top element.
+    getMin() -- Retrieve the minimum element in the stack.    
+    */
+
+    private Stack<Integer> stack = new Stack<Integer>();
+
+    private Stack<Integer> minStack = new Stack<Integer>();
+
+    public void push(int x) {
+        if (minStack.isEmpty() || x <= minStack.peek()) minStack.push(x);
+        stack.push(x);
+    }
+
+    public void pop() {
+        if (stack.peek().equals(minStack.peek())) minStack.pop();
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
