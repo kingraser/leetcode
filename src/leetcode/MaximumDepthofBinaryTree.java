eb7a936cc63250428801455739c491f700044701
wenlong.wang <wenlong.wang@qunar.com>
2015-09-13 13:19:10 +0800

    Maximum Depth of Binary Tree

---
+++ src/leetcode/MaximumDepthofBinaryTree.java
@@ -0,0 +1,18 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+import leetcode.common.TreeNode;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年9月13日<p>
+//-------------------------------------------------------
+public class MaximumDepthofBinaryTree {
+
+    public int maxDepth(TreeNode root) {
+        return root == null ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
