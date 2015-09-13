1d985d70abbdfb1d0597a540f97a7e8a89cbfc0b
wenlong.wang <wenlong.wang@qunar.com>
2015-09-13 14:38:34 +0800

    Implement Queue using Stacks

---
+++ src/leetcode/ImplementQueueusingStacks.java
@@ -0,0 +1,39 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+import java.util.Stack;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年9月13日<p>
+//-------------------------------------------------------
+public class ImplementQueueusingStacks {
+
+    private Stack<Integer> in = new Stack<Integer>(), out = new Stack<Integer>();
+
+    // Push element x to the back of queue.
+    public void push(int x) {
+        in.push(x);
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+        peek();
+        out.pop();
+    }
+
+    // Get the front element.
+    public int peek() {
+        if (out.isEmpty()) while (!in.isEmpty())
+            out.push(in.pop());
+        return out.peek();
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty();
+    }
+}
