363915857c2cb19e5889018d9fe0cbf38433299c
wenlong.wang <wenlong.wang@qunar.com>
2015-09-14 12:07:04 +0800

    Summary Ranges

---
+++ src/leetcode/SummaryRanges.java
@@ -0,0 +1,42 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年9月14日<p>
+//-------------------------------------------------------
+public class SummaryRanges {
+
+    /*    
+    Given a sorted integer array without duplicates, return the summary of its ranges.
+    
+    For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"]. 
+    */
+
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = Lists.newArrayListWithCapacity(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            int start = nums[i];
+            for (; i + 1 < nums.length && nums[i + 1] == nums[i] + 1; i++);
+            if (start != nums[i]) result.add(String.format("%d->%d", start, nums[i]));
+            else result.add(Integer.toString(start));
+        }
+        return result;
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(Lists.newArrayList("0->2", "4->5", "7"), summaryRanges(new int[] { 0, 1, 2, 4, 5, 7 }));
+    }
+
+}
