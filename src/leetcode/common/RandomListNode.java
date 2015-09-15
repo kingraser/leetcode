db2a0bb3e8d59d8428c9de52364e521799bbcfe8
wenlong.wang <wenlong.wang@qunar.com>
2015-09-15 20:06:31 +0800

    common class

---
+++ src/leetcode/common/RandomListNode.java
@@ -0,0 +1,21 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode.common;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年9月15日<p>
+//-------------------------------------------------------
+public class RandomListNode {
+
+    public int label;
+
+    public RandomListNode next, random;
+
+    public RandomListNode(int x) {
+        this.label = x;
+    }
+
+}
