321b9c7a265317f15482f1baa4d693368007f1ea
wenlong.wang <wenlong.wang@qunar.com>
2015-10-10 15:13:13 +0800

    贪心

---
+++ src/leetcode/Candy.java
@@ -0,0 +1,41 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+import java.util.Arrays;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年10月10日<p>
+//-------------------------------------------------------
+public class Candy {
+    /*
+    There are N children standing in a line. Each child is assigned a rating value.
+    
+    You are giving candies to these children subjected to the following requirements:
+    
+    Each child must have at least one candy.
+    Children with a higher rating get more candies than their neighbors.
+    
+    What is the minimum candies you must give? 
+    */
+
+    public int candy(int[] ratings) {
+        int[] candy = new int[ratings.length];
+        Arrays.fill(candy, 1);
+        for (int i = 1; i < ratings.length; i++)
+            if (ratings[i] > ratings[i - 1]) candy[i] = candy[i - 1] + 1;
+        for (int i = ratings.length - 2; i > -1; i--)
+            if (ratings[i] > ratings[i + 1] && candy[i] <= candy[i + 1]) candy[i] = candy[i + 1] + 1;
+        return sum(candy);
+    }
+
+    private int sum(int[] candy) {
+        int sum = 0;
+        for (int i = 0; i < candy.length; sum += candy[i++]);
+        return sum;
+    }
+
+}
