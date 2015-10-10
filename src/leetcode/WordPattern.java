673e2d53972a6564fcdf08735159ac113b2840ee
wenlong.wang <wenlong.wang@qunar.com>
2015-10-10 15:16:44 +0800

    hash map

--- src/leetcode/WordPattern.java
+++ src/leetcode/WordPattern.java
@@ -16,6 +16,22 @@ import org.junit.Test;
 //-------------------------------------------------------
 public class WordPattern {
 
+    /*
+    Given a pattern and a string str, find if str follows the same pattern.
+    
+    Examples:    
+    pattern = "abba", str = "dog cat cat dog" should return true.
+    pattern = "abba", str = "dog cat cat fish" should return false.
+    pattern = "aaaa", str = "dog cat cat dog" should return false.
+    pattern = "abba", str = "dog dog dog dog" should return false.
+    
+    Notes:    
+    pattern contains only lowercase alphabetical letters, and str contains words separated by a single space. 
+    Each word in str contains only lowercase alphabetical letters.
+    Both pattern and str do not have leading or trailing spaces.
+    Each letter in pattern must map to a word with length that is at least 1.    
+    */
+
     public boolean wordPattern(String pattern, String str) {
         String[] array = str.split(" ");
         if (array.length != pattern.length()) return false;
