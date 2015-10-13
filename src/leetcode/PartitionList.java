fcb450187fff94f5a5ad9ffe76c9e27ed05ac049
wenlong.wang <wenlong.wang@qunar.com>
2015-10-13 21:04:23 +0800

    linkedlist 2pointers

---
+++ src/leetcode/PartitionList.java
@@ -0,0 +1,48 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import leetcode.common.ListNode;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年10月13日<p>
+//-------------------------------------------------------
+public class PartitionList {
+    /*
+    Given a linked list and a value x, 
+    partition it such that all nodes less than x come before nodes greater than or equal to x.
+    
+    You should preserve the original relative order of the nodes in each of the two partitions.
+    
+    For example,
+    Given 1->4->3->2->5->2 and x = 3,
+    return 1->2->2->4->3->5.   
+    */
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode left = new ListNode(-1), right = new ListNode(-1), l = left, r = right;
+        for (ListNode cur = head; cur != null; cur = cur.next)
+            if (cur.val < x) {
+                l.next = cur;
+                l = cur;
+            } else {
+                r.next = cur;
+                r = cur;
+            }
+        l.next = right.next;
+        r.next = null;
+        return left.next;
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(ListNode.generateNodes(1, 2, 2, 4, 3, 5),
+                partition(ListNode.generateNodes(1, 4, 3, 2, 5, 2), 3));
+    }
+}
