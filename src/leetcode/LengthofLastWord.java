80355c209271251d3c240574e2581b6bca33a708
wenlong.wang <wenlong.wang@qunar.com>
2015-10-13 14:18:30 +0800

    string

--- src/leetcode/LengthofLastWord.java
+++ src/leetcode/LengthofLastWord.java
@@ -13,6 +13,16 @@ import org.junit.Test;
 //-------------------------------------------------------
 public class LengthofLastWord {
 
+    /*
+    Given a string s consists of upper/lower-case alphabets and empty space characters ' ', 
+    return the length of last word in the string.    
+    If the last word does not exist, return 0.    
+    Note: A word is defined as a character sequence consists of non-space characters only.
+    
+    For example,
+    Given s = "Hello World",
+    return 5. 
+    */
     public int lengthOfLastWord(String s) {
         int len = s.length() - 1, lastLength = 0;
         for (; len > -1 && s.charAt(len) == ' '; len--);
