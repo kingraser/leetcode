d12905a47122eace7b62a5844783304ee532a085
wenlong.wang <wenlong.wang@qunar.com>
2015-10-13 14:09:17 +0800

    binary tree dfs

--- src/leetcode/BinaryTreePaths.java
+++ src/leetcode/BinaryTreePaths.java
@@ -17,6 +17,20 @@ import leetcode.common.TreeNode;
 //-------------------------------------------------------
 public class BinaryTreePaths {
 
+    /*
+    Given a binary tree, return all root-to-leaf paths.
+    
+    For example, given the following binary tree:
+    
+       1
+     /   \
+    2     3
+     \
+      5
+    
+    All root-to-leaf paths are:["1->2->5", "1->3"]
+    */
+    
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> answer = new ArrayList<String>();
         if (root != null) searchBT(root, "", answer);
