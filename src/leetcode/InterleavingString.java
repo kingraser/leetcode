58f922ae37ee492eeb2de893f2a540a9d9175be6
wenlong.wang <wenlong.wang@qunar.com>
2015-10-14 17:07:01 +0800

    dp

---
+++ src/leetcode/InterleavingString.java
@@ -0,0 +1,48 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年10月14日<p>
+//-------------------------------------------------------
+public class InterleavingString {
+
+    /*
+    Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+    
+    For example,
+    Given:
+    s1 = "aabcc",
+    s2 = "dbbca",
+    
+    When s3 = "aadbbcbcac", return true.
+    When s3 = "aadbbbaccc", return false. 
+    */
+
+    /*
+    设状态f[i][j],表示s1[0,i]和s2[0,j],匹配s3[0,i+j]。
+    如果s1的最后一个字符等于s3的最后一个字符,则f[i][j]=f[i-1][j];
+    如果s2的最后一个字符等于s3的最后一个字符,则f[i][j]=f[i][j-1]。
+    因此状态转移方程如下:
+    f[i][j]=(s1[i-1]==s3[i+j-1]&&f[i-1][j])||(s2[j-1]==s3[i+j-1]&&f[i][j-1]);
+    */
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) return false;
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+        dp[0][0] = true;
+        for (int i = 0; i < s2.length(); ++i)
+            dp[0][i + 1] = dp[0][i] && s2.charAt(i) == s3.charAt(i);
+        for (int i = 0; i < s1.length(); ++i)
+            dp[i + 1][0] = dp[i][0] && s1.charAt(i) == s3.charAt(i);
+        for (int i = 0; i < s1.length(); ++i)
+            for (int j = 0; j < s2.length(); ++j)
+                dp[i + 1][j + 1] = (dp[i][j + 1] && s1.charAt(i) == s3.charAt(i + j + 1))
+                        || (dp[i + 1][j] && s2.charAt(j) == s3.charAt(i + j + 1));
+        return dp[s1.length()][s2.length()];
+    }
+
+}
