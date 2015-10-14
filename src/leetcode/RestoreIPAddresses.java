a13627b6a46bee160f0910275f8c73776fc81bb5
wenlong.wang <wenlong.wang@qunar.com>
2015-10-14 16:20:31 +0800

    string backtracking dfs

---
+++ src/leetcode/RestoreIPAddresses.java
@@ -0,0 +1,20 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年10月14日<p>
+//-------------------------------------------------------
+public class RestoreIPAddresses {
+    /*
+    Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+    
+    For example:
+    Given "25525511135",
+    
+    return ["255.255.11.135", "255.255.111.35"]. (Order does not matter) 
+    */
+}
