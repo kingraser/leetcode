3bbbbb0d330bfa6b454f1a0e6809bc5502c95be3
wenlong.wang <wenlong.wang@qunar.com>
2015-10-14 14:49:23 +0800

    dp

--- src/leetcode/ClimbingStairs.java
+++ src/leetcode/ClimbingStairs.java
@@ -5,6 +5,9 @@
  */
 package leetcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月13日<p>
 //-------------------------------------------------------
@@ -16,11 +19,34 @@ public class ClimbingStairs {
     In how many distinct ways can you climb to the top? 
     */
 
+    /*
+    设f(n)表示爬n阶楼梯的不同方法数,为了爬到第n阶楼梯,有两个选择:
+        从第n−1阶前进1步;
+        从第n−1阶前进2步;    
+    因此,有f(n)=f(n−1)+f(n−2)。这是一个斐波那契数列。
+    方法1,递归,太慢;方法2,迭代。
+    方法3,数学公式。
+    */
+
     public int climbStairs(int n) {
-        if (n < 3) return n;
-        int stepTwo = 1, stepOne = 2, result = 0;
-        for (int i = 0; i < n - 2; result = stepOne + stepTwo, stepTwo = stepOne, stepOne = result, i++);
-        return result;
+        int prev = 0, cur = 1;
+        for (int i = 1; i <= n; i++) {
+            int tmp = cur;
+            cur += prev;
+            prev = tmp;
+        }
+        return cur;
+    }
+
+    public int climbStairsI(int n) {
+        return (int) ((Math.pow((1 + Math.sqrt(5)) / 2, n + 1) + Math.pow((1 - Math.sqrt(5)) / 2, n + 1)) / Math.sqrt(5)
+                + 0.5);
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(3, climbStairs(3));
+        Assert.assertEquals(3, climbStairsI(3));
     }
 
 }
---
+++ src/leetcode/DecodeWays.java
@@ -0,0 +1,53 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年10月14日<p>
+//-------------------------------------------------------
+public class DecodeWays {
+    /*
+    A message containing letters from A-Z is being encoded to numbers using the following mapping:
+    
+    'A' -> 1
+    'B' -> 2
+    ...
+    'Z' -> 26
+    
+    Given an encoded message containing digits, determine the total number of ways to decode it.
+    
+    For example,
+    Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+    
+    The number of ways decoding "12" is 2.   
+    */
+
+    /*
+    跟Climbing Stairs很类似,不过多加一些判断逻辑。   
+    */
+
+    @Test
+    public void test() {
+        Assert.assertEquals(2, numDecodings("12"));
+    }
+
+    public int numDecodings(String s) {
+        if (StringUtils.isBlank(s) || s.charAt(0) == '0') return 0;
+        int prev = 0, cur = 1;
+        for (int i = 1; i <= s.length(); ++i) {
+            if (s.charAt(i - 1) == '0') cur = 0;
+            if (i < 2 || !(s.charAt(i - 2) == '1' || (s.charAt(i - 2) == '2' && s.charAt(i - 1) <= '6'))) prev = 0;
+            int tmp = cur;
+            cur = prev + cur;
+            prev = tmp;
+        }
+        return cur;
+    }
+}
