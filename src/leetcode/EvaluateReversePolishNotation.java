97534c580ee58896c0d2fbb651aae36ba939ccdd
wenlong.wang <wenlong.wang@qunar.com>
2015-10-16 15:39:17 +0800

    stack

---
+++ src/leetcode/EvaluateReversePolishNotation.java
@@ -0,0 +1,39 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package leetcode;
+
+import java.util.Set;
+import java.util.Stack;
+
+import com.google.common.collect.Sets;
+
+//--------------------- Change Logs----------------------
+// <p>@author wit Initial Created at 2015年10月16日<p>
+//-------------------------------------------------------
+public class EvaluateReversePolishNotation {
+    /*
+    Evaluate the value of an arithmetic expression in Reverse Polish Notation.    
+    Valid operators are +, -, *, /. Each operand may be an integer or another expression.    
+    Some examples:    
+      ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+      ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6    
+    */
+
+    public int evalRPN(String[] tokens) {
+        Set<String> set = Sets.newHashSet("+", "-", "*", "/");
+        Stack<Integer> integers = new Stack<>();
+        for (String s : tokens)
+            if (set.contains(s)) {
+                int right = integers.pop(), left = integers.pop();
+                if (s.equals("+")) left += right;
+                else if (s.equals("-")) left -= right;
+                else if (s.equals("*")) left *= right;
+                else left /= right;
+                integers.push(left);
+            } else integers.push(Integer.parseInt(s));
+        return integers.pop();
+    }
+}
