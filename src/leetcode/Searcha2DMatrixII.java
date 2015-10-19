c7bee75ba9f262b9bbb0636fded8a410579f0a7a
wenlong.wang <wenlong.wang@qunar.com>
2015-10-19 17:09:19 +0800

    divide and conquer binary search

--- src/leetcode/Searcha2DMatrixII.java
+++ src/leetcode/Searcha2DMatrixII.java
@@ -11,35 +11,31 @@ import org.junit.Test;
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年10月12日<p>
 //-------------------------------------------------------
-public class Searcha2DMatrix {
+public class Searcha2DMatrixII {
     /*
-    Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
-    
-    Integers in each row are sorted from left to right.
-    The first integer of each row is greater than the last integer of the previous row.
-    
-    For example,
-    
+    Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:    
+    Integers in each row are sorted in ascending from left to right.
+    Integers in each column are sorted in ascending from top to bottom.    
+    For example,  
     Consider the following matrix:
     
     [
-      [1,   3,  5,  7],
-      [10, 11, 16, 20],
-      [23, 30, 34, 50]
-    ]
-    
-    Given target = 3, return true.
+        [1,   4,  7, 11, 15],
+        [2,   5,  8, 12, 19],
+        [3,   6,  9, 16, 22],
+        [10, 13, 14, 17, 24],
+        [18, 21, 23, 26, 30]
+    ]    
+    Given target = 5, return true.
+    Given target = 20, return false.
     */
 
     //O(n+m)
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
-        int row = 0, col = matrix[0].length - 1;
-        while (row < matrix.length && col > -1) {
-            if (matrix[row][col] == target) return true;
-            else if (matrix[row][col] > target) col--;
-            else row++;
-        }
+        for (int i = 0, j = matrix[0].length - 1; i < matrix.length && j >= 0;)
+            if (target == matrix[i][j]) return true;
+            else if (target < matrix[i][j]) --j;
+            else++i;
         return false;
     }
 
