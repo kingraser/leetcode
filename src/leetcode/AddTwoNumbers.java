4586b5917dc34287b62e9addf55f4d3c1540632a
kinghost <kingraser@gmail.com>
2016-03-16 16:11:24 +0800

    linkedlist math

--- src/leetcode/AddTwoNumbers.java
+++ src/leetcode/AddTwoNumbers.java
@@ -5,8 +5,6 @@
  */
 package leetcode;
 
-import java.math.BigInteger;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -18,43 +16,41 @@ import leetcode.common.ListNode;
 public class AddTwoNumbers {
 
     /*
-     给两个链表,反向数位表示整数,返回一个链表表示和
+             给两个链表,反向数位表示整数,返回一个链表表示和
     (2 -> 4 -> 3)342 + (5 -> 6 -> 4)465
     Output: 7 -> 0 -> 8 807
     
-    简单模拟
-     */
+            简单模拟
+    */
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        char[] result = new StringBuilder().append(getNum(l1).add(getNum(l2))).reverse().toString().toCharArray();
-        ListNode head = new ListNode(0), marcher = head, temp;
-        for (int i = 0; i < result.length; i++, marcher.next = temp, marcher = temp)
-            temp = new ListNode(result[i] - '0');
-        return head.next;
+        return addTwoNumbers(l1, l2, 0);
     }
 
-    public BigInteger getNum(ListNode node) {
-        StringBuilder sb = new StringBuilder();
-        for (; node != null; node = node.next)
-            sb.append(node.val);
-        return new BigInteger(sb.reverse().toString());
+    private ListNode addTwoNumbers(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null) return addTwoNumbers(l2, carry);
+        if (l2 == null) return addTwoNumbers(l1, carry);
+        l1.next = addTwoNumbers(l1.next, l2.next, add(l1, l2.val + carry));
+        return l1;
+    }
+
+    private ListNode addTwoNumbers(ListNode l, int i) {
+        if (l == null) return i == 0 ? null : new ListNode(i);
+        if (add(l, i) == 1) l.next = addTwoNumbers(l.next, 1);
+        return l;
+    }
+
+    private int add(ListNode l, int i) {
+        l.val += i;
+        if (l.val < 10) return 0;
+        l.val -= 10;
+        return 1;
     }
 
     @Test
     public void test() {
-        ListNode l1 = new ListNode(2);
-        l1.next = new ListNode(4);
-        l1.next.next = new ListNode(3);
-
-        ListNode l2 = new ListNode(5);
-        l2.next = new ListNode(6);
-        l2.next.next = new ListNode(4);
-
-        ListNode l3 = new ListNode(7);
-        l3.next = new ListNode(0);
-        l3.next.next = new ListNode(8);
-
-        Assert.assertEquals(l3, addTwoNumbers(l1, l2));
+        Assert.assertEquals(ListNode.generateNodes(7, 0, 8),
+                addTwoNumbers(ListNode.generateNodes(2, 4, 3), ListNode.generateNodes(5, 6, 4)));
     }
 
 }
