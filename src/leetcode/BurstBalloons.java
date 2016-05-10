3f5ca4d058ac4738a40b781dda1bfe657db4c6e5
kinghost <kingraser@gmail.com>
2016-05-10 16:35:57 +0800

    dp

---
+++ src/leetcode/BurstBalloons.java
@@ -0,0 +1,55 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2015 Sogou.com. All Rights Reserved.
+ */
+package leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+//--------------------- Change Logs----------------------
+//@author wangwenlong Initial Created at 2016年3月4日;
+//-------------------------------------------------------
+public class BurstBalloons {
+    /*
+    Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by array nums. 
+    You are asked to burst all the balloons. If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins. 
+    Here left and right are adjacent indices of i. After the burst, the left and right then becomes adjacent.  
+    Find the maximum coins you can collect by bursting the balloons wisely.
+    
+    Note:
+    (1) You may imagine nums[-1] = nums[n] = 1. They are not real therefore you can not burst them.
+    (2) 0 ≤ n ≤ 500, 0 ≤ nums[i] ≤ 100
+    
+    Example:    
+    Given [3, 1, 5, 8]    
+    Return 167    
+    nums = [3,1,5,8] --> [3,5,8] -->   [3,8]   -->  [8]  --> []
+    coins =  3*1*5      +  3*5*8    +  1*3*8      + 1*8*1   = 167
+    */
+
+    @Test
+    public void test() {
+        Assert.assertEquals(167, maxCoins(new int[] { 3, 1, 5, 8 }));
+    }
+
+    /*
+            以最后被爆破的气球m为界限，把数组分为左右两个子区域
+    dp[l][r] = max(dp[l][r], nums[l] * nums[m] * nums[r] + dp[l][m] + dp[m][r])
+    */
+
+    public int maxCoins(int[] nums) {
+        int len = nums.length, len2 = len + 2;
+        int[] newnums = new int[len2];
+        for (int i = 0; i < len; i++)
+            newnums[i + 1] = nums[i];
+        newnums[0] = newnums[len + 1] = 1;
+        int[][] DP = new int[len2][len2];
+        for (int k = 2; k < len2; k++)
+            for (int l = 0; l + k < len2; l++)
+                for (int m = l + 1, h = l + k; m < h; m++)
+                    DP[l][h] = Math.max(DP[l][h], newnums[l] * newnums[m] * newnums[h] + DP[l][m] + DP[m][h]);
+        return DP[0][nums.length + 1];
+    }
+}
