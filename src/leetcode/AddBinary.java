c9b2a9820caee3c29d3b6fa726042a4b0f023194
kinghost <kingraser@gmail.com>
2016-05-10 12:12:55 +0800

    math string

--- src/leetcode/AddBinary.java
+++ src/leetcode/AddBinary.java
@@ -5,6 +5,9 @@
  */
 package leetcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月13日<p>
 //-------------------------------------------------------
@@ -17,18 +20,24 @@ public class AddBinary {
     Return "100".
     */
 
+    @Test
+    public void test() {
+        Assert.assertEquals("100", addBinary("11", "1"));
+        Assert.assertEquals("100", addBinaryII("11", "1"));
+    }
+
     public String addBinary(String a, String b) {
         return new java.math.BigInteger(a, 2).add(new java.math.BigInteger(b, 2)).toString(2);
     }
 
     public String addBinaryII(String a, String b) {
         StringBuilder result = new StringBuilder();
-        for (int i = a.length() - 1, j = b.length() - 1, ia, ib, c = 0, r = 0; i >= 0 || j >= 0 || c == 1;) {
+        for (int i = a.length() - 1, j = b.length() - 1, ia, ib, c = 0, v = 0; i >= 0 || j >= 0 || c == 1;) {
             ia = (i >= 0) ? a.charAt(i--) - '0' : 0;
             ib = (j >= 0) ? b.charAt(j--) - '0' : 0;
-            r = ia ^ ib ^ c;
+            v = ia ^ ib ^ c;
             c = ((ia + ib + c) > 1) ? 1 : 0;
-            result.append(r);
+            result.append(v);
         }
         return result.reverse().toString();
     }
