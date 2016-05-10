a66f4fbafe3822f674b69e4edc0740dde87faaf3
kinghost <kingraser@gmail.com>
2016-05-10 17:44:04 +0800

    binary tree

--- src/leetcode/FlattenBinaryTreetoLinkedList.java
+++ src/leetcode/FlattenBinaryTreetoLinkedList.java
@@ -5,7 +5,11 @@
  */
 package leetcode;
 
+import java.util.Objects;
 import java.util.Stack;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 import leetcode.common.TreeNode;
 
@@ -39,12 +43,28 @@ public class FlattenBinaryTreetoLinkedList {
                5
                 \
                  6
-     */
+    */
+
+    @Test
+    public void test() {
+        TreeNode root = TreeNode.generateTree("1,2,3,n,n,4,n,n,5,n,6,n,n");
+        flatten(root);
+        Assert.assertEquals(TreeNode.generateTree("1,n,2,n,3,n,4,n,5,n,6,n,n"), root);
+        root = TreeNode.generateTree("1,2,3,n,n,4,n,n,5,n,6,n,n");
+        flattenII(root);
+        Assert.assertEquals(TreeNode.generateTree("1,n,2,n,3,n,4,n,5,n,6,n,n"), root);
+        root = TreeNode.generateTree("1,2,3,n,n,4,n,n,5,n,6,n,n");
+        flattenIII(root);
+        Assert.assertEquals(TreeNode.generateTree("1,n,2,n,3,n,4,n,5,n,6,n,n"), root);
+        root = TreeNode.generateTree("1,2,3,n,n,4,n,n,5,n,6,n,n");
+        flattenIV(root);
+        Assert.assertEquals(TreeNode.generateTree("1,n,2,n,3,n,4,n,5,n,6,n,n"), root);
+    }
 
     public void flatten(TreeNode root) {
-        for (TreeNode pre; root != null; root = root.right)
-            if (root.left != null) {
-                for (pre = root.left; pre.right != null; pre = pre.right);
+        for (TreeNode pre; Objects.nonNull(root); root = root.right)
+            if (Objects.nonNull(root.left)) {
+                for (pre = root.left; Objects.nonNull(pre.right); pre = pre.right);
                 pre.right = root.right;
                 root.right = root.left;
                 root.left = null;
@@ -52,12 +72,12 @@ public class FlattenBinaryTreetoLinkedList {
     }
 
     public void flattenII(TreeNode root) {
-        if (root == null) return;// 终止条件
+        if (Objects.isNull(root)) return;//终止条件
         flattenII(root.left);
         flattenII(root.right);
-        if (null == root.left) return;
-        TreeNode p = root.left;// 三方合并,将左子树所形成的链表插入到 root 和 root->right 之间
-        for (; p.right != null; p = p.right); //寻找左链表最后一个节点
+        if (Objects.isNull(root.left)) return;
+        TreeNode p = root.left;//三方合并,将左子树所形成的链表插入到 root 和 root->right 之间
+        for (; Objects.nonNull(p.right); p = p.right); //寻找左链表最后一个节点
         p.right = root.right;
         root.right = root.left;
         root.left = null;
@@ -67,21 +87,21 @@ public class FlattenBinaryTreetoLinkedList {
         flatten(root, null);
     }
 
-    private TreeNode flatten(TreeNode root, TreeNode tail) { // 把 root 所代表树变成链表后,tail 跟在该链表后面
-        if (null == root) return tail;
+    private TreeNode flatten(TreeNode root, TreeNode tail) { //把 root所代表树变成链表后,tail跟在该链表后面
+        if (Objects.isNull(root)) return tail;
         root.right = flatten(root.left, flatten(root.right, tail));
         root.left = null;
         return root;
     }
 
     public void flattenIV(TreeNode root) {
-        if (root == null) return;
+        if (Objects.isNull(root)) return;
         Stack<TreeNode> s = new Stack<>();
         s.push(root);
         while (!s.empty()) {
             TreeNode p = s.pop();
-            if (p.right != null) s.push(p.right);
-            if (p.left != null) s.push(p.left);
+            if (Objects.nonNull(p.right)) s.push(p.right);
+            if (Objects.nonNull(p.left)) s.push(p.left);
             p.left = null;
             if (!s.empty()) p.right = s.peek();
         }
