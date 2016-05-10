7b9d054e4de4943b984d01417a324b78c9464619
kinghost <kingraser@gmail.com>
2016-05-10 16:55:55 +0800

    two pointers

--- src/leetcode/ContainerWithMostWater.java
+++ src/leetcode/ContainerWithMostWater.java
@@ -10,9 +10,9 @@ package leetcode;
 //-------------------------------------------------------
 public class ContainerWithMostWater {
     /*
-    给定 int[] 求两个板能存住的最大水量
-    思路:两个指针头,尾.移动矮的一方至比起始移动时高或不能移动为止.水位法保存水量
-     */
+            给定 int[] 求两个板能存住的最大水量
+            思路:两个指针头,尾.移动矮的一方至比起始移动时高或不能移动为止.水位法保存水量
+    */
 
     public int maxArea(int[] height) {
         int i = 0, j = height.length - 1, min = Math.min(height[i], height[j]), max = min * (j - i);
