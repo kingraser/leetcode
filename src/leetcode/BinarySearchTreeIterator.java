bc25c8732cd3228746c7e138f4ce8c85720f967d
kinghost <kingraser@gmail.com>
2016-05-10 16:06:06 +0800

    binary tree

--- src/leetcode/BinarySearchTreeIterator.java
+++ src/leetcode/BinarySearchTreeIterator.java
@@ -5,6 +5,7 @@
  */
 package leetcode;
 
+import java.util.Objects;
 import java.util.Stack;
 
 import leetcode.common.TreeNode;
@@ -15,39 +16,35 @@ import leetcode.common.TreeNode;
 public class BinarySearchTreeIterator {
 
     /*
-    Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.    
+    Implement an iterator over a binary search tree (BST). 
+    Your iterator will be initialized with the root node of a BST.    
     Calling next() will return the next smallest number in the BST.    
-    Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree. 
+    Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, 
+    where h is the height of the tree. 
     */
     public class BSTIterator {
 
-        Stack<TreeNode> stack = null;
-
-        TreeNode current = null;
+        private Stack<TreeNode> stack = new Stack<TreeNode>();
 
         public BSTIterator(TreeNode root) {
-            current = root;
-            stack = new Stack<>();
+            pushLeft(root);
         }
 
-        /**
-         * @return whether we have a next smallest number
-         */
         public boolean hasNext() {
-            return !stack.isEmpty() || current != null;
+            return !stack.isEmpty();
         }
 
-        /**
-         * @return the next smallest number
-         */
         public int next() {
-            while (current != null) {
-                stack.push(current);
-                current = current.left;
+            TreeNode node = stack.pop();
+            pushLeft(node.right);
+            return node.val;
+        }
+
+        private void pushLeft(TreeNode node) {
+            while (Objects.nonNull(node)) {
+                stack.push(node);
+                node = node.left;
             }
-            TreeNode t = stack.pop();
-            current = t.right;
-            return t.val;
         }
     }
 }
