aeb92f4b668399ddbbc42e8a00eda2066d7bc6fb
kinghost <kingraser@gmail.com>
2016-05-12 16:15:49 +0800

    stack

--- src/leetcode/EvaluateReversePolishNotation.java
+++ src/leetcode/EvaluateReversePolishNotation.java
@@ -8,6 +8,9 @@ package leetcode;
 import java.util.Set;
 import java.util.Stack;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import com.google.common.collect.Sets;
 
 //--------------------- Change Logs----------------------
@@ -21,6 +24,12 @@ public class EvaluateReversePolishNotation {
       ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
       ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6    
     */
+
+    @Test
+    public void test() {
+        Assert.assertEquals(9, evalRPN(new String[] { "2", "1", "+", "3", "*" }));
+        Assert.assertEquals(6, evalRPN(new String[] { "4", "13", "5", "/", "+" }));
+    }
 
     public int evalRPN(String[] tokens) {
         Set<String> set = Sets.newHashSet("+", "-", "*", "/");
