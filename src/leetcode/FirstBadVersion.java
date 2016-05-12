1f990a01d2fddfc21aa803d66ecb5b74ff573e93
kinghost <kingraser@gmail.com>
2016-05-12 16:20:25 +0800

    binary search

--- src/leetcode/FirstBadVersion.java
+++ src/leetcode/FirstBadVersion.java
@@ -10,15 +10,15 @@ package leetcode;
 //-------------------------------------------------------
 public class FirstBadVersion {
     /*
-    假定有1...n n个版本，其中有些版本有问题
-    若一个版本有问题，则其后继版本均有问题
-    现有api:isBadVersion判断一个版本是否有问题
-    最小化api调用次数找到最开始有问题的版本
-    
-    解法
-    显然的二分查找
+            假定有1...n n个版本，其中有些版本有问题
+            若一个版本有问题，则其后继版本均有问题
+            现有api:isBadVersion判断一个版本是否有问题
+            最小化api调用次数找到最开始有问题的版本
+            
+            解法
+            显然的二分查找
     logN
-     */
+    */
 
     public int firstBadVersion(int n) {
         int l = 1, r = n;
