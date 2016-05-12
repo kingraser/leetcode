1e534e24354dd363f98c1f0df8e486f39224c444
kinghost <kingraser@gmail.com>
2016-05-12 16:11:31 +0800

    math binary search

--- src/leetcode/DivideTwoIntegers.java
+++ src/leetcode/DivideTwoIntegers.java
@@ -14,9 +14,9 @@ public class DivideTwoIntegers {
     Divide two integers without using multiplication, division and mod operator.
     If it is overflow, return MAX_INT. 
     
-    不能用乘、除和取模,那剩下的,还有加、减和位运算。
-    最简单的方法,是不断减去被除数。在这个基础上,可以做一点优化,每次把被除数翻倍,
-    从而加速。
+            不能用乘、除和取模,那剩下的,还有加、减和位运算。
+            最简单的方法,是不断减去被除数。在这个基础上,可以做一点优化,每次把被除数翻倍,
+            从而加速。
     */
 
     public static int divide(int dividend, int divisor) {
