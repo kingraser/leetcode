556cc26c00d0ec4e7cd3eb9d846bf15f977f5ba1
kinghost <kingraser@gmail.com>
2016-05-20 19:16:57 +0800

    bit manipulation

--- src/leetcode/SingleNumber.java
+++ src/leetcode/SingleNumber.java
@@ -5,6 +5,9 @@
  */
 package leetcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月14日<p>
 //-------------------------------------------------------
@@ -13,8 +16,13 @@ public class SingleNumber {
     //Given an array of integers, every element appears twice except for one. Find that single one.
     public int singleNumber(int[] nums) {
         int res = nums[0];
-        for (int i = 1; i < nums.length; res = res ^ nums[i], i++);
+        for (int i = 1; i < nums.length; res = res ^ nums[i++]);
         return res;
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(3, singleNumber(new int[] { 1, 1, 2, 3, 2 }));
     }
 
 }
