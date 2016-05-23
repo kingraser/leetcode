85d7fa93ad22028398c536afb7418adfc8b97318
kinghost <kingraser@gmail.com>
2016-05-23 17:19:41 +0800

    tree dfs dp

--- src/leetcode/HouseRobberIII.java
+++ src/leetcode/HouseRobberIII.java
@@ -61,9 +61,7 @@ public class HouseRobberIII {
     //first:without current node, second:with current node 
     private int[] robSub(TreeNode root) {
         if (root == null) return new int[2];
-        int[] left = robSub(root.left), right = robSub(root.right), res = new int[2];
-        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
-        res[1] = root.val + left[0] + right[0];
-        return res;
+        int[] left = robSub(root.left), right = robSub(root.right);
+        return new int[] { Math.max(left[0], left[1]) + Math.max(right[0], right[1]), root.val + left[0] + right[0] };
     }
 }
