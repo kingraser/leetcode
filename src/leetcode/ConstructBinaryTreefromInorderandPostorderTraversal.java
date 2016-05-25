12133dff341414d0c2d389664ef710168b05b395
kinghost <kingraser@gmail.com>
2016-05-25 17:32:23 +0800

    tree array dfs

--- src/leetcode/ConstructBinaryTreefromInorderandPostorderTraversal.java
+++ src/leetcode/ConstructBinaryTreefromInorderandPostorderTraversal.java
@@ -5,6 +5,9 @@
  */
 package leetcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import leetcode.common.TreeNode;
 
 //--------------------- Change Logs----------------------
@@ -12,10 +15,30 @@ import leetcode.common.TreeNode;
 //-------------------------------------------------------
 public class ConstructBinaryTreefromInorderandPostorderTraversal {
 
+    @Test
+    public void test() {
+        Assert.assertEquals(TreeNode.generateTree("1,2,n,4,n,n,3,5,n,n,n"),
+                buildTree(new int[] { 2, 4, 1, 5, 3 }, new int[] { 4, 2, 5, 3, 1 }));
+    }
+
+    /**
+     * @param i 中根遍历节点数组
+     * @param p 后根遍历节点数组
+     * @return 构造树的根节点
+     */
     public TreeNode buildTree(int[] i, int[] p) {//in order post order
         return buildTree(i, p, 0, i.length, 0, p.length);
     }
 
+    /**
+     * @param i 中根遍历节点数组
+     * @param p 后根遍历节点数组
+     * @param iFirst 子树的最左叶子节点
+     * @param iLast 子树的最右叶子节点
+     * @param pFirst 子树的最左叶子节点
+     * @param pLast 子树的根节点
+     * @return 构造树的根节点
+     */
     private TreeNode buildTree(int[] i, int[] p, int iFirst, int iLast, int pFirst, int pLast) {
         if (pFirst == pLast || iFirst == iLast) return null;
         TreeNode root = new TreeNode(p[pLast - 1]);
--- src/leetcode/ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ src/leetcode/ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -5,6 +5,9 @@
  */
 package leetcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import leetcode.common.TreeNode;
 
 //--------------------- Change Logs----------------------
@@ -12,10 +15,30 @@ import leetcode.common.TreeNode;
 //-------------------------------------------------------
 public class ConstructBinaryTreefromPreorderandInorderTraversal {
 
-    public TreeNode buildTree(int[] p, int[] i) {//previous order in order
+    @Test
+    public void test() {
+        Assert.assertEquals(TreeNode.generateTree("1,2,n,4,n,n,3,5,n,n,n"),
+                buildTree(new int[] { 1, 2, 4, 3, 5 }, new int[] { 2, 4, 1, 5, 3 }));
+    }
+
+    /**
+     * @param p 先根遍历节点数组
+     * @param i 中根遍历节点数组
+     * @return 构造树的根节点
+     */
+    public TreeNode buildTree(int[] p, int[] i) {
         return buildTree(p, i, 0, p.length, 0, i.length);
     }
 
+    /**
+     * @param p 先根遍历节点数组
+     * @param i 中根遍历节点数组
+     * @param pFirst 子树的根节点
+     * @param pLast 子树的最右节点
+     * @param iFirst 子树的最左节点
+     * @param iLast 子树的最右节点
+     * @return 构造树的根节点
+     */
     private TreeNode buildTree(int[] p, int[] i, int pFirst, int pLast, int iFirst, int iLast) {
         if (pFirst == pLast || iFirst == iLast) return null;
         TreeNode root = new TreeNode(p[pFirst]);
