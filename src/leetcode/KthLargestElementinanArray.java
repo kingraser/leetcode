5039f5b08391cee37d27ccf13dd9015231b4bf62
kinghost <kingraser@gmail.com>
2016-05-27 18:13:51 +0800

    heap divide-conquer

--- src/leetcode/KthLargestElementinanArray.java
+++ src/leetcode/KthLargestElementinanArray.java
@@ -5,7 +5,7 @@
  */
 package leetcode;
 
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 import org.junit.Assert;
@@ -26,7 +26,9 @@ public class KthLargestElementinanArray {
 
     @Test
     public void test() {
-        Assert.assertEquals(3, findKthLargestII(new int[] { 7, 6, 5, 4, 3, 2, 1 }, 5));
+        Assert.assertEquals(3, findKthLargest(new int[] { 5, 3, 2, 4, 7, 1, 6 }, 5));
+        Assert.assertEquals(3, findKthLargestII(new int[] { 5, 3, 2, 4, 7, 1, 6 }, 5));
+        Assert.assertEquals(5, findKthLargestII(new int[] { 5, 3, 2, 4, 7, 1, 6 }, 3));
     }
 
     //heap
@@ -36,29 +38,24 @@ public class KthLargestElementinanArray {
 
     private int findKth(int[] nums, int k, boolean isLarge) {
         if (k > (nums.length + 1) >> 1) return findKth(nums, nums.length - k + 1, !isLarge);
-        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(nums.length, new Comparator<Integer>() {
-
-            @Override
-            public int compare(Integer i1, Integer i2) {
-                int val;
-                if (i1 == i2) val = 0;
-                else if (i1 < i2) val = 1;
-                else val = -1;
-                return isLarge ? val : -val;
-            }
-        });
-        for (int i : nums)
-            queue.add(i);
-        while (--k > 0)
-            queue.poll();
+        PriorityQueue<Integer> queue = new PriorityQueue<>(nums.length, (i1, i2) -> isLarge ? i2 - i1 : i1 - i2);
+        Arrays.stream(nums).forEach(i -> queue.add(i));
+        for (; --k > 0; queue.poll());
         return queue.poll();
     }
 
-    //quicksort
+    //quick sort
     public int findKthLargestII(int[] nums, int k) {
-        return findK(nums, nums.length - k, 0, nums.length - 1);
+        return findK(nums, nums.length - k, 0, nums.length - 1);//k large = length - k + 1 small, so index = length - k
     }
 
+    /**
+     * @param nums
+     * @param k 查找的下标
+     * @param from 起始下标 inclusive
+     * @param to 结束下标 inclusive
+     * @return 返回排序后下标为k的元素,亦即第k+1个元素
+     */
     private int findK(int[] nums, int k, int from, int to) {
         if (from >= to) return nums[from];
         int m = partition(nums, from, to);
@@ -67,15 +64,22 @@ public class KthLargestElementinanArray {
         else return findK(nums, k, from, m - 1);
     }
 
+    /**
+     * @param nums
+     * @param from 起始下标 inclusive
+     * @param to 结束下标 inclusive
+     * @return index k where element k is smaller than elements in [from,k) and is larger than elements in (k,end]
+     */
     private int partition(int[] nums, int from, int to) {
-        int x = nums[from], m = from, n = from + 1;
-        for (; n <= to; n++)
-            if (nums[n] < x) swap(nums, ++m, n);
-        swap(nums, from, m);
-        return m;
+        int e = nums[from], idx = from;//第一个元素e, e排序后的下标
+        for (int i = from + 1; i <= to; i++)//遍历e以后的元素
+            if (nums[i] < e) swap(nums, ++idx, i);//将比e优先度低的元素连续排列
+        swap(nums, from, idx);//将e放置在比其优先度低的元素后
+        return idx;
     }
 
     private void swap(int[] nums, int i, int j) {
+        if (i == j) return;
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
