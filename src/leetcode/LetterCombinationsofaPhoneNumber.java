0bd34ae677eead546e41a07e76a193f4690cfac6
kinghost <kingraser@gmail.com>
2016-05-27 18:54:35 +0800

    backtracking string

--- src/leetcode/LetterCombinationsofaPhoneNumber.java
+++ src/leetcode/LetterCombinationsofaPhoneNumber.java
@@ -5,14 +5,15 @@
  */
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 //--------------------- Change Logs----------------------
@@ -33,20 +34,22 @@ public class LetterCombinationsofaPhoneNumber {
     Note:
     Although the above answer is in lexicographical order, your answer could be in any order you want. 
     */
-    public List<String> letterCombinations(String digits) {
-        if (StringUtils.isBlank(digits)) return Lists.newArrayList();
-        LinkedList<String> ans = Lists.newLinkedList(Lists.newArrayList(""));
-        List<String> map = Lists.newArrayList("abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz");
-        for (int i = 0; i < digits.length(); i++)
-            for (String t = ans.peek(); ans.peek().length() == i; t = ans.remove(), t = ans.peek())
-                for (int j = 0; j < map.get(digits.charAt(i) - '2').length(); ans
-                        .add(t + map.get(digits.charAt(i) - '2').charAt(j)), j++);
+
+    String[] map = new String[] { "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+
+    public List<String> letterCombinations(String s) {
+        if (s == null || s.length() == 0) return new ArrayList<>();
+        LinkedList<String> ans = new LinkedList<>(Arrays.asList(""));
+        for (int i = 0; i < s.length(); i++)
+            for (String t; (t = ans.peekFirst()).length() == i; t = ans.pollFirst())
+                for (char c : map[s.charAt(i) - '2'].toCharArray())
+                    ans.add(t + c);
         return ans;
     }
 
     @Test
     public void test() {
-        Assert.assertEquals(Sets.newTreeSet(Lists.newArrayList("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf")),
-                Sets.newTreeSet(letterCombinations("23")));
+        Assert.assertEquals(Sets.newHashSet("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"),
+                new HashSet<>(letterCombinations("23")));
     }
 }
