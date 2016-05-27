4d419ae344932ccc4163dbe146a423f5bf76f5eb
kinghost <kingraser@gmail.com>
2016-05-27 19:03:20 +0800

    array union-find

--- src/leetcode/LongestConsecutiveSequence.java
+++ src/leetcode/LongestConsecutiveSequence.java
@@ -5,9 +5,12 @@ package leetcode;
  * Copyright (c) 2012 Qunar.com. All Rights Reserved.
  */
 
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-import com.google.common.collect.Sets;
+import org.junit.Assert;
+import org.junit.Test;
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年10月16日<p>
@@ -25,22 +28,26 @@ public class LongestConsecutiveSequence {
     */
 
     /*
-    分析
-    如果允许 O(n log n) 的复杂度,那么可以先排序,可是本题要求 O(n)。
-    由于序列里的元素是无序的,又要求 O(n),首先要想到用哈希表。
-    用一个哈希表 unordered_map<int, bool> used 记录每个元素是否使用,
-    对每个元素,以该元素为中心,往左右扩张,直到不连续为止,记录下最长的长度。
+            分析
+            如果允许 O(n log n) 的复杂度,那么可以先排序,可是本题要求 O(n)。
+            由于序列里的元素是无序的,又要求 O(n),首先要想到用哈希表。
+            用一个哈希表<int, boolean>used记录每个元素是否使用,
+            对每个元素,以该元素为中心,往左右扩张,直到不连续为止,记录下最长的长度。
     */
 
+    @Test
+    public void test() {
+        Assert.assertEquals(4, longestConsecutive(new int[] { 100, 4, 200, 1, 3, 2 }));
+    }
+
     public int longestConsecutive(int[] nums) {
-        Set<Integer> set = Sets.newHashSet();
-        for (int e : nums)
-            set.add(e);
+        if (nums == null || nums.length == 0) return 0;
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
         int max = 1;
         for (int e : nums) {
             int count = 1;
-            for (int left = e - 1; set.contains(left); count++, set.remove(left--));
-            for (int right = e + 1; set.contains(right); count++, set.remove(right++));
+            for (int left = e - 1; set.remove(left--); count++);
+            for (int right = e + 1; set.remove(right++); count++);
             max = Math.max(count, max);
         }
         return max;
