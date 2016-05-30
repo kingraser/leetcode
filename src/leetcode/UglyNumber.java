bbc357858a9b08886fb3c82a44ca5f55be500114
kinghost <kingraser@gmail.com>
2016-05-30 17:58:31 +0800

    math

--- src/leetcode/UglyNumber.java
+++ src/leetcode/UglyNumber.java
@@ -14,13 +14,13 @@ import org.junit.Test;
 public class UglyNumber {
     /*
     ugly number：一个数的质因数只有2,3,5（1也是ugly number）
-    判断一个数是否是ugly number
-     */
+            判断一个数是否是ugly number
+    */
 
     public boolean isUgly(int num) {
         if (num <= 0) return false;
-        while (num % 2 == 0)
-            num /= 2;
+        while ((num & 1) == 0)
+            num >>= 1;
         while (num % 5 == 0)
             num /= 5;
         while (num % 3 == 0)
