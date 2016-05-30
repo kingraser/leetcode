ede5d497485c528839c5922d00410fa49ea49675
kinghost <kingraser@gmail.com>
2016-05-30 17:22:10 +0800

    array

--- src/leetcode/SpiralMatrix.java
+++ src/leetcode/SpiralMatrix.java
@@ -5,12 +5,12 @@
  */
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import com.google.common.collect.Lists;
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年10月12日<p>
@@ -32,25 +32,21 @@ public class SpiralMatrix {
     */
 
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res = Lists.newArrayList();
+        List<Integer> res = new ArrayList<>();
         if (matrix == null || matrix.length == 0) return res;
-        for (int rowBegin = 0, rowEnd = matrix.length - 1, colBegin = 0, colEnd = matrix[0].length
+        for (int rowBegin = 0, rowEnd = matrix.length - 1, colBegin = -1, colEnd = matrix[0].length
                 - 1; rowBegin <= rowEnd && colBegin <= colEnd;) {
-            for (int i = colBegin; i <= colEnd; res.add(matrix[rowBegin][i++]));
-            rowBegin++;
-            for (int i = rowBegin; i <= rowEnd; res.add(matrix[i++][colEnd]));
-            colEnd--;
-            for (int i = colEnd; i >= colBegin; res.add(matrix[rowEnd][i--]));
-            rowEnd--;
-            for (int i = rowEnd; i >= rowBegin; res.add(matrix[i--][colBegin]));
-            colBegin++;
+            for (int i = ++colBegin; i <= colEnd; res.add(matrix[rowBegin][i++]));
+            for (int i = ++rowBegin; i <= rowEnd; res.add(matrix[i++][colEnd]));
+            for (int i = --colEnd; i >= colBegin; res.add(matrix[rowEnd][i--]));
+            for (int i = --rowEnd; i >= rowBegin; res.add(matrix[i--][colBegin]));
         }
         return res;
     }
 
     @Test
     public void test() {
-        Assert.assertEquals(Lists.newArrayList(1, 2, 3, 6, 9, 8, 7, 4, 5),
+        Assert.assertEquals(Arrays.asList(1, 2, 3, 6, 9, 8, 7, 4, 5),
                 spiralOrder(new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }));
     }
 }
