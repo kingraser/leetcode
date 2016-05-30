423a34a87b65b1f077dff9a664676e9ed65a78f6
kinghost <kingraser@gmail.com>
2016-05-30 11:36:20 +0800

    hash-table two-pointers string

--- src/leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ src/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -5,8 +5,6 @@
  */
 package leetcode;
 
-import java.util.HashMap;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -16,26 +14,30 @@ import org.junit.Test;
 public class LongestSubstringWithoutRepeatingCharacters {
 
     /*
-    如题
+            如题
     abcabcbb is "abc", which the length is 3.
     bbbbb the longest substring is "b", with the length of 1.
-    
-    思路:维护一个窗口,窗口内无重复字母+水位法.
-    窗口围护方法
-        无重复,后指针向后.
-        有重复,前指针向后,至无重复为止
-     */
+            思路:维护一个窗口,窗口内无重复字母+水位法.
+            窗口围护方法
+            无重复,后指针向后.
+            有重复,前指针向后,至无重复为止
+    */
 
     public int lengthOfLongestSubstring(String s) {
-        int first = 0, size = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); map.put(s.charAt(i), i), size = Math.max(size, i - first + 1), i++)
-            if (map.containsKey(s.charAt(i))) first = Math.max(first, map.get(s.charAt(i)) + 1);
-        return size;
+        int max = 0, len = 0;
+        int[] map = new int[128];
+        for (int i = 0, first = 0, c; i < s.length(); map[c] = ++i)
+            if (map[c = s.charAt(i)] != 0) {
+                if (max < len) max = len;
+                if (first < map[c]) first = map[c];
+                len = i - first + 1;
+            } else len++;
+        return max < len ? len : max;
     }
 
     @Test
     public void test() {
+        Assert.assertEquals(2, lengthOfLongestSubstring("abba"));
         Assert.assertEquals(3, lengthOfLongestSubstring("abcabcbb"));
         Assert.assertEquals(1, lengthOfLongestSubstring("bbbbb"));
     }
