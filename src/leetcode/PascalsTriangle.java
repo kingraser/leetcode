d2c24104e3fffb4de7d4277f3b93343c61965c84
kinghost <kingraser@gmail.com>
2016-05-31 11:04:51 +0800

    array

--- src/leetcode/PascalsTriangle.java
+++ src/leetcode/PascalsTriangle.java
@@ -1,18 +1,16 @@
-package leetcode;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-import com.google.common.collect.Lists;
-
 /*
  * $Id$
  *
  * Copyright (c) 2012 Qunar.com. All Rights Reserved.
  */
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月12日<p>
@@ -35,17 +33,15 @@ public class PascalsTriangle {
         List<List<Integer>> res = new ArrayList<>(numRows);
         for (int i = 0; i < numRows; i++) {
             res.add(new ArrayList<>(i + 1));
-            for (int j = 0; j < i + 1; res.get(i)
-                    .add(j == 0 || j == i ? 1 : res.get(i - 1).get(j) + res.get(i - 1).get(j - 1)), j++);
+            for (int j = 0; j < i + 1; j++)
+                res.get(i).add(j == 0 || j == i ? 1 : res.get(i - 1).get(j) + res.get(i - 1).get(j - 1));
         }
         return res;
     }
 
-    @SuppressWarnings("unchecked")
     @Test
     public void test() {
-        Assert.assertEquals(Lists.newArrayList(Lists.newArrayList(1), Lists.newArrayList(1, 1),
-                Lists.newArrayList(1, 2, 1), Lists.newArrayList(1, 3, 3, 1), Lists.newArrayList(1, 4, 6, 4, 1)),
-                generate(5));
+        Assert.assertEquals(Arrays.asList(Arrays.asList(1), Arrays.asList(1, 1), Arrays.asList(1, 2, 1),
+                Arrays.asList(1, 3, 3, 1), Arrays.asList(1, 4, 6, 4, 1)), generate(5));
     }
 }
--- src/leetcode/PascalsTriangleII.java
+++ src/leetcode/PascalsTriangleII.java
@@ -1,17 +1,17 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
 package leetcode;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
-
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月12日<p>
@@ -27,12 +27,12 @@ public class PascalsTriangleII {
     public List<Integer> getRow(int k) {
         List<Integer> res = Lists.newArrayList(1);
         for (int j = 1; j <= k; j++)
-            res.add((int) ((double) res.get(res.size() - 1) * (k - j + 1) / j));
+            res.add(res.get(res.size() - 1) * (k - j + 1) / j);
         return res;
     }
 
     @Test
     public void test() {
-        Assert.assertEquals(Lists.newArrayList(1, 3, 3, 1), getRow(3));
+        Assert.assertEquals(Arrays.asList(1, 3, 3, 1), getRow(3));
     }
 }
