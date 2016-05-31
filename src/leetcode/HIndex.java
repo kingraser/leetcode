20c94edfc16e0b6418fc12d1607635606df7060b
kinghost <kingraser@gmail.com>
2016-05-31 15:09:04 +0800

    hast-table sort

--- src/leetcode/HIndex.java
+++ src/leetcode/HIndex.java
@@ -15,23 +15,23 @@ import org.junit.Test;
 //-------------------------------------------------------
 public class HIndex {
     /*
-    给定一个非负整数数组,每个元素代表一篇论文的引用数。
-    计算该数组的引用因子
-    所谓引用因子x即有x篇文章的引用数大于等于x，其余文章引用数小于x
-    
+            给定一个非负整数数组,每个元素代表一篇论文的引用数。
+            计算该数组的引用因子
+            所谓引用因子x即有x篇文章的引用数大于等于x，其余文章引用数小于x
+            
     eg{3, 0, 6, 1, 5}的引用因子是3
-    因为3,5,6，有3篇文章的引用数至少为3，其余0,1 引用数小于3 
-    
-    解法
-    
+            因为3,5,6，有3篇文章的引用数至少为3，其余0,1 引用数小于3 
+            
+            解法
+            
     1O(n*log(n))
         1排序
         2顺序查找（优化：二分查找）
      
-     2O(n)
-         1one pass获取各引用数下的文章数
-         2one pass查找
-     */
+    2O(n)
+        1one pass获取各引用数下的文章数
+        2one pass查找
+    */
 
     //1 O(n*log(n))
     public int hIndex(int[] citations) {
@@ -45,8 +45,8 @@ public class HIndex {
     public int hIndexI(int[] citations) {
         Arrays.sort(citations);
         int l = 0, r = citations.length - 1;
-        for (int m = (l + r) / 2; l <= r; m = (l + r) / 2)
-            if (citations[m] < citations.length - m) l = m + 1;
+        for (int m; l <= r;)
+            if (citations[m = (l + r) >> 1] < citations.length - m) l = m + 1;
             else r = m - 1;
         return citations.length - l;
     }
@@ -54,11 +54,9 @@ public class HIndex {
     //2 O(n)
     public int hIndexII(int[] citations) {
         int[] array = new int[citations.length + 1];
-        for (int i : citations)
-            if (i > citations.length) array[citations.length] += 1;
-            else array[i] += 1;
-        for (int i = citations.length, t = array[i]; i >= 0; i--, t += array[i])
-            if (t >= i) return i;
+        Arrays.stream(citations).forEach(i -> array[i > citations.length ? citations.length : i]++);
+        for (int i = citations.length, t = 0; i >= 0; i--)
+            if ((t += array[i]) >= i) return i;
         return 0;
     }
 
@@ -66,5 +64,11 @@ public class HIndex {
     public void test() {
         Assert.assertEquals(3, hIndex(new int[] { 3, 0, 6, 1, 5 }));
         Assert.assertEquals(1, hIndex(new int[] { 100 }));
+
+        Assert.assertEquals(3, hIndexI(new int[] { 3, 0, 6, 1, 5 }));
+        Assert.assertEquals(1, hIndexI(new int[] { 100 }));
+
+        Assert.assertEquals(3, hIndexII(new int[] { 3, 0, 6, 1, 5 }));
+        Assert.assertEquals(1, hIndexII(new int[] { 100 }));
     }
 }
