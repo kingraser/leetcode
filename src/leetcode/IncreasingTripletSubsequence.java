ba43af208674e7a496e79e3888164d3ce196cd92
kinghost <kingraser@gmail.com>
2016-06-01 13:32:07 +0800

    array

---
+++ src/leetcode/IncreasingTripletSubsequence.java
@@ -0,0 +1,61 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2015 Sogou.com. All Rights Reserved.
+ */
+package leetcode;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+//--------------------- Change Logs----------------------
+//@author wangwenlong Initial Created at 2016年2月16日;
+//-------------------------------------------------------
+public class IncreasingTripletSubsequence {
+
+    /*
+    Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array.    
+    Formally the function should:
+    
+    Return true if there exists i, j, k
+    such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false. 
+    
+    Your algorithm should run in O(n) time complexity and O(1) space complexity.
+    
+    Examples:
+    Given [1, 2, 3, 4, 5],
+    return true.
+    
+    Given [5, 4, 3, 2, 1],
+    return false. 
+    */
+
+    @Test
+    public void test() {
+        Assert.assertTrue(increasingTriplet(new int[] { 2, 1, 5, 0, 4, 6 }));
+        Assert.assertTrue(increasingTriplet(new int[] { 1, 2, 3, 4, 5 }));
+        Assert.assertFalse(increasingTriplet(new int[] { 5, 4, 3, 2, 1 }));
+    }
+
+    public boolean increasingTriplet(int[] nums) {
+        return increasing(nums, 3);
+
+    }
+
+    private boolean increasing(int[] nums, int k) {
+        int[] min = new int[k - 1];
+        Arrays.fill(min, Integer.MAX_VALUE);
+        A: for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < min.length; j++)
+                if (nums[i] <= min[j]) {
+                    min[j] = nums[i];
+                    continue A;
+                }
+            return true;
+        }
+        return false;
+
+    }
+}
