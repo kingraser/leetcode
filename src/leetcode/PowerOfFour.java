7d04fc64b6bf89872c555edbf1a6852d066324af
kinghost <kingraser@gmail.com>
2016-06-01 17:40:25 +0800

    bit manipulation

---
+++ src/leetcode/PowerOfFour.java
@@ -0,0 +1,29 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2015 Sogou.com. All Rights Reserved.
+ */
+package leetcode;
+
+//--------------------- Change Logs----------------------
+//@author wangwenlong Initial Created at 2016年4月18日;
+//-------------------------------------------------------
+public class PowerOfFour {
+
+    /*
+    Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+    
+    Example:
+    Given num = 16, return true. Given num = 5, return false.
+    
+    Follow up: Could you solve it without loops/recursion? 
+    */
+
+    //step1 num>0 
+    //step2 1 bit count is 1 
+    //step3 the 1 bit occurs at odd bit
+    public boolean isPowerOfFour(int num) {
+        return num > 0 && (num & (num - 1)) == 0 && 0 != (num & 0x55555555);
+    }
+
+}
