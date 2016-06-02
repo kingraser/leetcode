20465c1f0490f6ae5b15f6d88e6e8b600e02b112
kinghost <kingraser@gmail.com>
2016-06-02 10:45:45 +0800

    linked list

--- src/leetcode/SwapNodesinPairs.java
+++ src/leetcode/SwapNodesinPairs.java
@@ -1,15 +1,14 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
 package leetcode;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import leetcode.common.ListNode;
-
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月10日<p>
@@ -19,29 +18,26 @@ public class SwapNodesinPairs {
     public ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null) return head;
         ListNode result = head.next;
-        for (ListNode first = head, second = first.next, temp = first; first != null
-                && second != null; temp.next = second, first.next = second.next, second.next = first, temp = first, first = first.next, second = first != null
-                        ? first.next : second);
+        for (ListNode first = head, second; first != null && first.next != null;) {
+            first.next = (second = first.next).next;
+            second.next = first;
+            first = first.next;
+        }
         return result;
     }
 
     public ListNode swapPairsII(ListNode head) {
         if ((head == null) || (head.next == null)) return head;
-        ListNode n = head.next;
-        head.next = swapPairsII(head.next.next);
-        n.next = head;
-        return n;
+        ListNode next = head.next;
+        head.next = swapPairsII(next.next);
+        next.next = head;
+        return next;
     }
 
     @Test
     public void test() {
-        ListNode l1 = new ListNode(1), l2 = new ListNode(2);
-        ListNode L1 = new ListNode(1), L2 = new ListNode(2);
-
-        l1.next = l2;
-        L2.next = L1;
-
-        Assert.assertEquals(L2, swapPairsII(l1));
+        Assert.assertEquals(ListNode.generateNodes(1, 2), swapPairs(ListNode.generateNodes(2, 1)));
+        Assert.assertEquals(ListNode.generateNodes(1, 2), swapPairsII(ListNode.generateNodes(2, 1)));
     }
 
 }
