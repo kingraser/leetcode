74eaf77aa701a819621cc44e2a6744b48ce243ba
kinghost <kingraser@gmail.com>
2016-06-03 19:18:29 +0800

    two-pointers

--- src/leetcode/FourSum.java
+++ src/leetcode/FourSum.java
@@ -5,6 +5,7 @@
  */
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -20,7 +21,7 @@ import com.google.common.collect.Lists;
 public class FourSum {
 
     /*
-    转为3Sum(k Sum可递归为k-1 Sum)   
+            转为3Sum(k Sum可递归为k-1 Sum)   
     */
 
     public List<List<Integer>> fourSum(int[] nums, int target) {
@@ -35,34 +36,35 @@ public class FourSum {
      * @param start 数组的起始位置
      * @return
      */
-    private List<List<Integer>> kSum(int[] nums, int target, int k, int start) {
-        List<List<Integer>> result = Lists.newArrayList();
-        if (k > 2) {//turn to k-1 sum
-            for (int i = start; i < nums.length - k; i++) {
-                if (i > 0 && nums[i - 1] == nums[i]) continue;
-                for (List<Integer> list : kSum(nums, target - nums[i], k - 1, i + 1)) {
-                    List<Integer> temp = Lists.newArrayList(nums[i]);
-                    temp.addAll(list);
-                    result.add(temp);
-                }
+    public static List<List<Integer>> kSum(int[] nums, int target, int k, int start) {
+        if (k == 2) return TwoSum(nums, target, start);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = start; i < nums.length - k; i++)
+            if (i > start && nums[i - 1] == nums[i]) continue;
+            else for (List<Integer> list : kSum(nums, target - nums[i], k - 1, i + 1)) {
+                list.add(0, nums[i]);
+                result.add(list);
             }
-        } else {//2 sum
-            HashSet<Integer> set = new HashSet<>();
-            for (int i = start; i < nums.length; i++)
-                if (set.contains(target - nums[i])) {
-                    result.add(Lists.newArrayList(target - nums[i], nums[i]));
-                    for (; i + 1 < nums.length && nums[i + 1] == nums[i]; i++);
-                } else set.add(nums[i]);
-        }
         return result;
     }
 
-    @SuppressWarnings("unchecked")
+    private static List<List<Integer>> TwoSum(int[] nums, int target, int start) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int l = start, r = nums.length - 1, v; l < r;)
+            if (l > start && nums[l - 1] == nums[l]) l++;
+            else if ((v = nums[l] + nums[r]) == target) result.add(Lists.newArrayList(nums[l++], nums[r]));
+            else if (v < target) l++;
+            else r--;
+        return result;
+    }
+
     @Test
     public void test() {
-        List<List<Integer>> expected = Lists.newArrayList(Lists.newArrayList(-2, -1, 1, 2),
-                Lists.newArrayList(-2, 0, 0, 2), Lists.newArrayList(-1, 0, 0, 1));
-        Assert.assertEquals(expected, fourSum(new int[] { 1, 0, -1, 0, -2, 2 }, 0));
+        List<List<Integer>> expected = Arrays.asList(Arrays.asList(-2, -1, 1, 2), Arrays.asList(-2, 0, 0, 2),
+                Arrays.asList(-1, 0, 0, 1));
+        Assert.assertEquals(new HashSet<>(expected), new HashSet<>(fourSum(new int[] { 1, 0, -1, 0, -2, 2 }, 0)));
+        expected = Arrays.asList(Arrays.asList(0, 0, 0, 0));
+        Assert.assertEquals(expected, fourSum(new int[] { 0, 0, 0, 0, 0, 0 }, 0));
     }
 
 }
