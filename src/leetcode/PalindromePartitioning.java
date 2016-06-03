769d403f94cded811277f9f82f838db3e835e04f
kinghost <kingraser@gmail.com>
2016-06-03 15:21:40 +0800

    backtracking dp

--- src/leetcode/PalindromePartitioning.java
+++ src/leetcode/PalindromePartitioning.java
@@ -6,13 +6,12 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import com.google.common.collect.Lists;
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年10月16日<p>
@@ -30,35 +29,40 @@ public class PalindromePartitioning {
     */
 
     /*
-    在每一步都可以判断中间结果是否为合法结果,用回溯法。
-    一个长度为 n 的字符串,有 n − 1 个地方可以砍断,每个地方可断可不断,因此复杂度为O(2^n−1)
+            在每一步都可以判断中间结果是否为合法结果,用回溯法。
+            一个长度为 n 的字符串,有 n − 1 个地方可以砍断,每个地方可断可不断,因此复杂度为O(2^n−1)
     */
 
-    @SuppressWarnings("unchecked")
     @Test
     public void test() {
-        Assert.assertEquals(Lists.newArrayList(Lists.newArrayList("a", "a", "b"), Lists.newArrayList("aa", "b")),
-                partition("aab"));
+        List<List<String>> expected = Arrays.asList(Arrays.asList("a", "a", "b"), Arrays.asList("aa", "b"));
+        Assert.assertEquals(new HashSet<>(expected), new HashSet<>(partition("aab")));
     }
 
     public List<List<String>> partition(String s) {
-        List<List<String>> result = new LinkedList<>();
-        char[] c = s.toCharArray();
-        for (int end = s.length() - 1, i = end; i >= 0; i--)
-            if (isPalindrome(c, i, s.length() - 1)) {//可以用PalindromePartitioningII的dp法判断
-                List<List<String>> resultTemp = partition(s.substring(0, i));
-                if (resultTemp.isEmpty()) resultTemp.add(new ArrayList<>(s.length()));
-                for (List<String> list : resultTemp) {
-                    list.add(s.substring(i));
-                    result.add(list);
-                }
+        return partition(s, getPalindromeMap(s.toCharArray()));
+    }
+
+    public List<List<String>> partition(String s, boolean[][] map) {
+        if (s.length() == 0) return Arrays.asList(new ArrayList<>());
+        List<List<String>> result = new ArrayList<>();
+        for (int i = s.length() - 1; i >= 0; i--)
+            if (isPalindrome(i, s.length() - 1, map)) for (List<String> l : partition(s.substring(0, i))) {
+                l.add(s.substring(i));
+                result.add(l);
             }
         return result;
     }
 
-    private boolean isPalindrome(char[] c, int start, int end) {
-        while (start < end)
-            if (c[start++] != c[end--]) return false;
-        return true;
+    private boolean isPalindrome(int i, int j, boolean[][] map) {
+        return i < j ? map[i][j] : true;
+    }
+
+    private boolean[][] getPalindromeMap(char[] s) {
+        boolean[][] map = new boolean[s.length][s.length];
+        for (int j = 1; j < s.length; j++)
+            for (int i = 0; i + j < s.length; i++)
+                map[i][i + j] = s[i] == s[i + j] && isPalindrome(i + 1, i + j - 1, map);
+        return map;
     }
 }
--- src/leetcode/PalindromePartitioningII.java
+++ src/leetcode/PalindromePartitioningII.java
@@ -7,6 +7,9 @@ package leetcode;
 
 import java.util.Arrays;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年10月16日<p>
 //-------------------------------------------------------
@@ -20,51 +23,44 @@ public class PalindromePartitioningII {
     */
 
     /*
-    定义状态 f(i,j) 表示区间 [i,j] 之间最小的 cut 数,则状态转移方程为
+            定义状态 f(i,j) 表示区间 [i,j] 之间最小的 cut 数,则状态转移方程为
     f(i,j)=min{f(i,k)+f(k+1,j)},i≤k≤j,0≤i≤j<n
-    这是一个二维函数,实际写代码比较麻烦。
-    所以要转换成一维DP。如果每次,从i往右扫描,每找到一个回文就算一次DP的话,就可以
-    转换为f(i)=区间[i,n-1]之间最小的cut数,n为字符串长度,则状态转移方程为
+            这是一个二维函数,实际写代码比较麻烦。
+            所以要转换成一维DP。如果每次,从i往右扫描,每找到一个回文就算一次DP的话,就可以
+            转换为f(i)=区间[i,n-1]之间最小的cut数,n为字符串长度,则状态转移方程为
     f(i)=min{f(j+1)+1},i≤j<n
-    一个问题出现了,就是如何判断[i,j]是否是回文?每次都从i到j比较一遍?
-    太浪费了,这里也是一个DP问题。
-    定义状态P[i][j]=true if S[i,j]为回文,那么
-    P[i][j]=str[i]==str[j]&&P[i+1][j-1]
+            一个问题出现了,就是如何判断[i,j]是否是回文?每次都从i到j比较一遍?
+            太浪费了,这里也是一个DP问题。
+            定义状态P[i][j]=true if S[i,j]为回文,那么
+    P[i][j]=S[i]==S[j]&&P[i+1][j-1]
     */
 
-    private static boolean[][] palindromeArray;
-
-    private static int[] dp;
+    @Test
+    public void test() {
+        Assert.assertEquals(1, minCut("aab"));
+        Assert.assertEquals(2, minCut("aabccbd"));
+    }
 
     public int minCut(String s) {
-        char[] sArray = s.toCharArray();
-        buildPalindromeArray(sArray);
-        dp = new int[s.length()];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        for (int i = s.length() - 1; i > -1; i--) {
-            if (isPalindrome(i, s.length() - 1)) {
-                dp[i] = 0;
-                continue;
-            }
-            for (int j = i; j < s.length() - 1; j++)
-                if (isPalindrome(i, j)) dp[i] = Math.min(dp[i], 1 + dp[j + 1]);
-        }
-        return dp[0];
+        boolean[][] map = getPalindromeMap(s.toCharArray());
+        int[] minCut = new int[s.length()];
+        Arrays.fill(minCut, Integer.MAX_VALUE);
+        for (int i = s.length() - 1; i > -1; i--)
+            if (isPalindrome(i, s.length() - 1, map)) minCut[i] = 0;
+            else for (int j = i; j < s.length() - 1; j++)
+                if (isPalindrome(i, j, map)) minCut[i] = Math.min(minCut[i], 1 + minCut[j + 1]);
+        return minCut[0];
     }
 
-    private boolean isPalindrome(int i, int j) {
-        return palindromeArray[i][j];
+    private boolean isPalindrome(int i, int j, boolean[][] map) {
+        return i < j ? map[i][j] : true;
     }
 
-    private void buildPalindromeArray(char[] s) {
-        palindromeArray = new boolean[s.length][s.length];
-        for (int i = 0; i < s.length - 1; i++) {
-            palindromeArray[i][i] = true;
-            palindromeArray[i][i + 1] = s[i] == s[i + 1];
-        }
-        palindromeArray[s.length - 1][s.length - 1] = true;
-        for (int j = 2; j < s.length; j++)
-            for (int i = 0; i < s.length - j; i++)
-                palindromeArray[i][i + j] = palindromeArray[i + 1][i + j - 1] && s[i] == s[i + j];
+    private boolean[][] getPalindromeMap(char[] s) {
+        boolean[][] map = new boolean[s.length][s.length];
+        for (int j = 1; j < s.length; j++)
+            for (int i = 0; i + j < s.length; i++)
+                map[i][i + j] = s[i] == s[i + j] && isPalindrome(i + 1, i + j - 1, map);
+        return map;
     }
 }
