ffaca5765ed0e9b433a9eecd63bb7b7b04dc1900
kinghost <kingraser@gmail.com>
2016-06-13 11:23:14 +0800

    tree dfs/bfs

--- src/leetcode/BinaryTreeRightSideView.java
+++ src/leetcode/BinaryTreeRightSideView.java
@@ -8,7 +8,6 @@ package leetcode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -40,14 +39,15 @@ public class BinaryTreeRightSideView {
 
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        if (Objects.nonNull(root)) rightSideView(root, list, 0);
+        rightSideView(root, list, 0);
         return list;
     }
 
     private void rightSideView(TreeNode root, List<Integer> list, int level) {
+        if (root == null) return;
         if (level == list.size()) list.add(root.val);
-        if (Objects.nonNull(root.right)) rightSideView(root.right, list, ++level);
-        if (Objects.nonNull(root.left)) rightSideView(root.left, list, level);
+        rightSideView(root.right, list, level + 1);
+        rightSideView(root.left, list, level + 1);
     }
 
 }
