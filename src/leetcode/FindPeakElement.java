f51c2f12d6e362e2445c041b01b34846cbfb850d
kinghost <kingraser@gmail.com>
2016-06-13 18:06:00 +0800

    binary-search

--- src/leetcode/FindPeakElement.java
+++ src/leetcode/FindPeakElement.java
@@ -5,6 +5,9 @@
  */
 package leetcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月14日<p>
 //-------------------------------------------------------
@@ -14,10 +17,15 @@ public class FindPeakElement {
     For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
     */
 
+    @Test
+    public void test() {
+        Assert.assertEquals(2, findPeakElement(new int[] { 1, 2, 3, 1 }));
+    }
+
     public int findPeakElement(int[] num) {
-        int l = 0, r = num.length - 1, mid = r / 2;
-        for (; l < r; mid = (l + r) / 2)
-            if (num[mid] < num[mid + 1]) l = mid + 1;
+        int l = 0, r = num.length - 1, mid;
+        while (l < r)
+            if (num[mid = (l + r) >> 1] < num[mid + 1]) l = mid + 1;
             else r = mid;
         return l;
     }
