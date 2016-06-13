b46629799f565af2d47e1ddd23770c137f854ad0
kinghost <kingraser@gmail.com>
2016-06-13 15:31:19 +0800

    hash-table math

--- src/leetcode/CountPrimes.java
+++ src/leetcode/CountPrimes.java
@@ -18,22 +18,20 @@ public class CountPrimes {
     //Count the number of prime numbers less than a non-negative number, n.
     public int countPrimes(int n) {
         BitSet bs = new BitSet(n);
-        bs.set(0);
-        bs.set(1);
-        int ind = 0, count = 0;
-        while (ind < n) {
-            ind = bs.nextClearBit(ind + 1);
-            if (ind >= n) return count;
+        bs.set(0, 2);
+        int idx = 0, count = 0;
+        while (idx < n) {
+            idx = bs.nextClearBit(idx + 1);
+            if (idx >= n) return count;
             count++;
-            for (int i = 2 * ind; i < n; bs.set(i), i += ind);
+            for (int i = idx << 1; i < n; bs.set(i), i += idx);
         }
         return count;
     }
 
     @Test
     public void test() {
-        int expected = 4, actual = countPrimes(10);
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(4, countPrimes(10));
     }
 
 }
