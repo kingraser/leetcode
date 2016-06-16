a7936807c46242fb941d6393e232330bc5b5ad66
kinghost <kingraser@gmail.com>
2016-06-16 11:27:16 +0800

    string

--- src/leetcode/LongestCommonPrefix.java
+++ src/leetcode/LongestCommonPrefix.java
@@ -13,9 +13,9 @@ public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
         if (null == strs || strs.length == 0) return "";
         StringBuilder sb = new StringBuilder();
-        A: for (int i = 0; i < strs[0].length(); sb.append(strs[0].charAt(i)), i++)
+        A: for (int i = 0; i < strs[0].length(); sb.append(strs[0].charAt(i++)))
             for (int j = 1; j < strs.length; j++)
-                if (i > strs[j].length() - 1 || strs[j].charAt(i) != strs[0].charAt(i)) break A;
+                if (i == strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i)) break A;
         return sb.toString();
     }
 
