d5b782795cacaeb903e1c69bfa044fbf7b0f9189
kinghost <kingraser@gmail.com>
2016-06-16 11:48:15 +0800

    array bit_manipulation divide-conquer

--- src/leetcode/MajorityElement.java
+++ src/leetcode/MajorityElement.java
@@ -11,12 +11,12 @@ package leetcode;
 public class MajorityElement {
     /*    
     Given an array of size n, find the majority element. 
-    The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+    The majority element is the element that appears more than n/2 times.
     */
 
     public int majorityElement(int[] num) {
         int a = num[0];
-        for (int i = 1, sum = 1; sum <= num.length / 2 && i < num.length; i++)
+        for (int i = 1, sum = 1; sum <= num.length >> 1 && i < num.length; i++)
             if (num[i] != a) {
                 sum--;
                 if (sum == 0) {
