c7350bfad2bc985ace279738f02777343d858997
kinghost <kingraser@gmail.com>
2016-06-20 17:22:24 +0800

    math string

--- src/leetcode/MultiplyStrings.java
+++ src/leetcode/MultiplyStrings.java
@@ -6,6 +6,10 @@
 package leetcode;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年10月10日<p>
@@ -17,7 +21,31 @@ public class MultiplyStrings {
     Note: The numbers can be arbitrarily large and are non-negative.
     */
 
+    @Test
+    public void test() {
+        Assert.assertEquals("998001", multiply("999", "999"));
+        Assert.assertEquals("56088", multiply("123", "456"));
+        Assert.assertEquals("998001", multiplyII("999", "999"));
+        Assert.assertEquals("56088", multiplyII("123", "456"));
+        Assert.assertEquals("0", multiplyII("999", "0"));
+    }
+
     public String multiply(String num1, String num2) {
         return new BigDecimal(num1).multiply(new BigDecimal(num2)).toString();
     }
+
+    public String multiplyII(String A1, String A2) {
+        int[] r = new int[A1.length() + A2.length()];
+        for (int l1 = A1.length(), l2 = A2.length(), i = l1 - 1; i >= 0; i--)
+            for (int j = l2 - 1, c = 0; j >= 0 || c > 0; j--, c /= 10)
+                r[i + j + 1] = (c += r[i + j + 1] + (A1.charAt(i) - '0') * (j < 0 ? 0 : A2.charAt(j) - '0')) % 10;
+        return new String(Arrays.stream(r).map(i -> '0' + i).toArray(), get1stNot0Idx(r), r.length - get1stNot0Idx(r));
+    }
+
+    private int get1stNot0Idx(int[] A) {
+        for (int i = 0; i < A.length - 1; i++)
+            if (A[i] != 0) return i;
+        return A.length - 1;
+    }
+
 }
