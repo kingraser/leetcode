54493787dd6d4fb9d5e9ca033aaf8d6f84fa59b7
kinghost <kingraser@gmail.com>
2016-11-15 14:33:17 +0800

    math

--- src/leetcode/ReverseInteger.java
+++ src/leetcode/ReverseInteger.java
@@ -13,34 +13,32 @@ import org.junit.Test;
 //-------------------------------------------------------
 public class ReverseInteger {
 
-    /*
-    Example1: x = 123, return 321
-    Example2: x = -123, return -321 
-    
-    1cheat 
-    
-    2模拟
-    */
+  /*
+  Reverse digits of an integer.
+  
+  Example1: x = 123, return 321
+  Example2: x = -123, return -321 
+  */
 
-    public int reverse(int x) {
-        Long reverse = Long.valueOf(new StringBuilder().append(Math.abs((long) x)).reverse().toString());
-        return x < 0 ? reverse + Integer.MIN_VALUE > 0 ? 0 : -reverse.intValue()
-                : reverse > Integer.MAX_VALUE ? 0 : reverse.intValue();
-    }
+  public int reverse(int x) {
+    Long reverse = Long.valueOf(new StringBuilder().append(Math.abs((long) x)).reverse().toString());
+    return x < 0 ? reverse + Integer.MIN_VALUE > 0 ? 0 : -reverse.intValue()
+        : reverse > Integer.MAX_VALUE ? 0 : reverse.intValue();
+  }
 
-    public int reverseII(int x) {
-        if (Math.abs((long) x) < 10) return x;
-        long y = 0, n = 1 + (int) Math.log10(Math.abs((long) x));
-        for (int i = 0; i < n; y = y * 10 + (int) (x / Math.pow(10, i) % 10), i++);
-        return y < Integer.MIN_VALUE || y > Integer.MAX_VALUE ? 0 : (int) y;
-    }
+  public int reverseII(int x) {
+    if (Math.abs((long) x) < 10) return x;
+    long y = 0, n = 1 + (int) Math.log10(Math.abs((long) x));
+    for (int i = 0; i < n; y = y * 10 + (int) (x / Math.pow(10, i) % 10), i++);
+    return y < Integer.MIN_VALUE || y > Integer.MAX_VALUE ? 0 : (int) y;
+  }
 
-    @Test
-    public void test() {
-        Assert.assertEquals(321, reverse(123));
-        Assert.assertEquals(-321, reverse(-123));
-        Assert.assertEquals(321, reverseII(123));
-        Assert.assertEquals(-321, reverseII(-123));
-    }
+  @Test
+  public void test() {
+    Assert.assertEquals(321, reverse(123));
+    Assert.assertEquals(-321, reverse(-123));
+    Assert.assertEquals(321, reverseII(123));
+    Assert.assertEquals(-321, reverseII(-123));
+  }
 
 }
