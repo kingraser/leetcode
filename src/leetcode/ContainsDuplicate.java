27f529a59987e4ea79bfb5b31e8cf059cc28378c
kinghost <kingraser@gmail.com>
2016-11-17 12:26:37 +0800

    array hash table

--- src/leetcode/ContainsDuplicate.java
+++ src/leetcode/ContainsDuplicate.java
@@ -5,8 +5,13 @@
  */
 package leetcode;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月12日<p>
@@ -19,10 +24,18 @@ public class ContainsDuplicate {
     */
 
     public boolean containsDuplicate(int[] nums) {
+        return Arrays.stream(nums).boxed().collect(Collectors.toSet()).size() < nums.length;
+    }
+
+    public boolean containsDuplicateII(int[] nums) {
         Set<Integer> set = new HashSet<>();
-        for (int i : nums)
-            if (!set.add(i)) return true;
-        return false;
+        return Arrays.stream(nums).filter(i -> !set.add(i)).findFirst().isPresent();
+    }
+
+    @Test
+    public void test() {
+        Assert.assertTrue(containsDuplicate(new int[] { 1, 2, 3, 1 }));
+        Assert.assertFalse(containsDuplicate(new int[] { 1, 2, 3 }));
     }
 
 }
