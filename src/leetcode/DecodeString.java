f4542104132d3b0f66cf22b076762d1c4edc9026
kinghost <kingraser@gmail.com>
2016-11-22 11:39:13 +0800

    dfs stack

---
+++ src/leetcode/DecodeString.java
@@ -0,0 +1,62 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2015 Sogou.com. All Rights Reserved.
+ */
+package leetcode;
+
+import java.util.Stack;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+//--------------------- Change Logs----------------------
+//@author wangwenlong Initial Created at 2016年9月5日;
+//-------------------------------------------------------
+public class DecodeString {
+
+  /*
+  Given an encoded string, return it's decoded string.
+  The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. 
+  Note that k is guaranteed to be a positive integer.
+  You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+  Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. 
+  For example, there won't be input like 3a or 2[4].
+  
+  Examples:
+  
+  s = "3[a]2[bc]", return "aaabcbc".
+  s = "3[a2[c]]", return "accaccacc".
+  s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
+  */
+
+  @Test
+  public void test() {
+    Assert.assertEquals("aaabcbc", decodeString("3[a]2[bc]"));
+    Assert.assertEquals("accaccacc", decodeString("3[a2[c]]"));
+    Assert.assertEquals("abcabccdcdcdef", decodeString("2[abc]3[cd]ef"));
+  }
+
+  public String decodeString(String s) {
+    Stack<Integer> times = new Stack<>();
+    Stack<StringBuilder> stack = new Stack<>();
+    stack.push(new StringBuilder());
+    for (int i = 0, num = 0, c; i < s.length(); i++)
+      if ((c = s.charAt(i)) >= '0' && c <= '9') num = num * 10 + c - '0';
+      else if (c == '[') {
+        stack.push(new StringBuilder());
+        times.push(num);
+        num = 0;
+      } else if (c == ']') {
+        StringBuilder top = repeat(stack.pop(), times.pop());
+        stack.peek().append(top);
+      } else stack.peek().append((char) c);
+    return stack.peek().toString();
+  }
+
+  private StringBuilder repeat(StringBuilder sb, int times) {
+    for (int len = sb.length(); --times > 0;)
+      for (int i = 0; i < len; sb.append(sb.charAt(i++)));
+    return sb;
+  }
+}
