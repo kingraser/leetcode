8dd7a2b000ce471f9b873a20211e85d087e33196
kinghost <kingraser@gmail.com>
2016-12-21 17:56:35 +0800

    graph

--- src/leetcode/EvaluateDivision.java
+++ src/leetcode/EvaluateDivision.java
@@ -5,30 +5,49 @@
  */
 package leetcode;
 
-import java.util.ArrayList;
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
 
-import leetcode.common.Pair;
+import leetcode.util.Pair;
 
 //--------------------- Change Logs----------------------
 //@author wangwenlong Initial Created at 2016年9月12日;
 //-------------------------------------------------------
 public class EvaluateDivision {
+  /*
+  Equations are given in the format A / B = k, where A and B are variables represented as strings, and k is a real number (floating point number). Given some queries, return the answers. If the answer does not exist, return -1.0.
+  
+  Example:
+  Given a / b = 2.0, b / c = 3.0.
+  queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ? .
+  return [6.0, 0.5, -1.0, 1.0, -1.0 ].
+  
+  The input is: vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries , where equations.size() == values.size(), and the values are positive. This represents the equations. Return vector<double>.
+  
+  According to the example above:
+  
+  equations = [ ["a", "b"], ["b", "c"] ],
+  values = [2.0, 3.0],
+  queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ]. 
+  
+  The input is always valid. You may assume that evaluating the queries will result in no division by zero and there is no contradiction. 
+  */
 
   @Test
   public void test() {
     String[][] equations = { { "a", "b" }, { "b", "c" } },
         queries = { { "a", "c" }, { "b", "a" }, { "a", "e" }, { "a", "a" }, { "x", "x" } };
     double[] values = { 2.0, 3.0 };
-    Assert.assertArrayEquals(new double[] { 6.0, 0.5, -1.0, 1.0, -1.0 }, calcEquation(equations, values, queries),
+    assertArrayEquals(new double[] { 6.0, 0.5, -1.0, 1.0, -1.0 }, calcEquation(equations, values, queries),
         Double.MIN_NORMAL);
   }
 
@@ -58,16 +77,11 @@ public class EvaluateDivision {
         bases.get(a.key).add(bString);
       }
     }
-    return getQueryResults(queries, map);
-  }
-
-  private double[] getQueryResults(String[][] queries, Map<String, Pair<Integer, Double>> map) {
-    List<Double> list = new ArrayList<>(queries.length);
-    Pair<Integer, Double> a, b;
-    for (String[] q : queries)
-      list.add(Objects.isNull(a = map.get(q[0])) || Objects.isNull(b = map.get(q[1])) || a.key != b.key ? -1d
-          : a.value / b.value);
-    return list.stream().mapToDouble(d -> d.doubleValue()).toArray();
+    return Arrays.stream(queries).mapToDouble(q -> {
+      Pair<Integer, Double> a, b;
+      if (Objects.isNull(a = map.get(q[0])) || Objects.isNull(b = map.get(q[1])) || a.key != b.key) return -1d;
+      return a.value / b.value;
+    }).toArray();
   }
 
 }
