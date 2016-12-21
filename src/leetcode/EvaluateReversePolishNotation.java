a0cc947648c493cfd14e619f7f976bc44f03909a
kinghost <kingraser@gmail.com>
2016-12-21 19:34:26 +0800

    stack

--- src/leetcode/EvaluateReversePolishNotation.java
+++ src/leetcode/EvaluateReversePolishNotation.java
@@ -5,45 +5,42 @@
  */
 package leetcode;
 
-import java.util.Set;
-import java.util.Stack;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Assert;
+import java.util.Map;
+import java.util.Stack;
+import java.util.function.BiFunction;
+
 import org.junit.Test;
 
-import com.google.common.collect.Sets;
+import com.google.common.collect.ImmutableMap;
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年10月16日<p>
 //-------------------------------------------------------
 public class EvaluateReversePolishNotation {
-    /*
-    Evaluate the value of an arithmetic expression in Reverse Polish Notation.    
-    Valid operators are +, -, *, /. Each operand may be an integer or another expression.    
-    Some examples:    
-      ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
-      ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6    
-    */
+  /*
+  Evaluate the value of an arithmetic expression in Reverse Polish Notation.    
+  Valid operators are +, -, *, /. Each operand may be an integer or another expression.    
+  Some examples:    
+    ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+    ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6    
+  */
 
-    @Test
-    public void test() {
-        Assert.assertEquals(9, evalRPN(new String[] { "2", "1", "+", "3", "*" }));
-        Assert.assertEquals(6, evalRPN(new String[] { "4", "13", "5", "/", "+" }));
-    }
+  @Test
+  public void test() {
+    assertEquals(9, evalRPN(new String[] { "2", "1", "+", "3", "*" }));
+    assertEquals(6, evalRPN(new String[] { "4", "13", "5", "/", "+" }));
+  }
 
-    Set<String> operators = Sets.newHashSet("+", "-", "*", "/");
+  Map<String, BiFunction<Integer, Integer, Integer>> operators = ImmutableMap.of("+", (a, b) -> b + a, "-",
+      (a, b) -> b - a, "*", (a, b) -> b * a, "/", (a, b) -> b / a);
 
-    public int evalRPN(String[] tokens) {
-        Stack<Integer> integers = new Stack<>();
-        for (String s : tokens)
-            if (operators.contains(s)) {
-                int right = integers.pop(), left = integers.pop();
-                if (s.equals("+")) left += right;
-                else if (s.equals("-")) left -= right;
-                else if (s.equals("*")) left *= right;
-                else left /= right;
-                integers.push(left);
-            } else integers.push(Integer.parseInt(s));
-        return integers.pop();
-    }
+  public int evalRPN(String[] tokens) {
+    Stack<Integer> integers = new Stack<>();
+    for (String s : tokens)
+      if (operators.containsKey(s)) integers.push(operators.get(s).apply(integers.pop(), integers.pop()));
+      else integers.push(Integer.parseInt(s));
+    return integers.pop();
+  }
 }
