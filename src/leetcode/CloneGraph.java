c3946096c3fa110e9edf89f4dbc5c9dd4db5b8fa
kinghost <kingraser@gmail.com>
2016-12-21 11:54:04 +0800

    dynamic programming

--- src/leetcode/CloneGraph.java
+++ src/leetcode/CloneGraph.java
@@ -14,45 +14,45 @@ import leetcode.common.UndirectedGraphNode;
 // <p>@author wit Initial Created at 2015年10月16日<p>
 //-------------------------------------------------------
 public class CloneGraph {
-    /*
-    Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
-    
-    OJ's undirected graph serialization:
-    
-    Nodes are labeled uniquely.
-    We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
-    
-    As an example, consider the serialized graph {0,1,2#1,2#2,2}.
-    
-    The graph has a total of three nodes, and therefore contains three parts as separated by #.
-    
-    First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
-    Second node is labeled as 1. Connect node 1 to node 2.
-    Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
-    
-    Visually, the graph looks like the following:
-    
-       1
-      / \
-     /   \
-    0 --- 2
-         / \
-         \_/
-    */
+  /*
+  Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+  
+  OJ's undirected graph serialization:
+  
+  Nodes are labeled uniquely.
+  We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
+  
+  As an example, consider the serialized graph {0,1,2#1,2#2,2}.
+  
+  The graph has a total of three nodes, and therefore contains three parts as separated by #.
+  
+  First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
+  Second node is labeled as 1. Connect node 1 to node 2.
+  Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
+  
+  Visually, the graph looks like the following:
+  
+     1
+    / \
+   /   \
+  0 --- 2
+       / \
+       \_/
+  */
 
-    //dfs or bfs
-    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
-        Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
-        dfs(map, node);
-        return map.get(node);
-    }
+  //dfs or bfs
+  public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+    Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+    dfs(map, node);
+    return map.get(node);
+  }
 
-    public void dfs(Map<UndirectedGraphNode, UndirectedGraphNode> map, UndirectedGraphNode node) {
-        if (map.containsKey(node)) return;
-        map.put(node, new UndirectedGraphNode(node.label));
-        node.neighbors.forEach(neighbor -> {
-            dfs(map, neighbor);
-            map.get(node).neighbors.add(map.get(neighbor));
-        });
-    }
+  public void dfs(Map<UndirectedGraphNode, UndirectedGraphNode> map, UndirectedGraphNode node) {
+    if (map.containsKey(node)) return;
+    map.put(node, new UndirectedGraphNode(node.label));
+    node.neighbors.forEach(neighbor -> {
+      dfs(map, neighbor);
+      map.get(node).neighbors.add(map.get(neighbor));
+    });
+  }
 }
--- src/leetcode/CoinChange.java
+++ src/leetcode/CoinChange.java
@@ -5,9 +5,10 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Arrays;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 //--------------------- Change Logs----------------------
@@ -15,36 +16,36 @@ import org.junit.Test;
 //-------------------------------------------------------
 public class CoinChange {
 
-    /*
-    You are given coins of different denominations and a total amount of money amount. 
-    Write a function to compute the fewest number of coins that you need to make up that amount. 
-    If that amount of money cannot be made up by any combination of the coins, return -1.
-    
-    Example 1:
-    coins = [1, 2, 5], amount = 11
-    return 3 (11 = 5 + 5 + 1)
-    
-    Example 2:
-    coins = [2], amount = 3
-    return -1.
-    
-    Note:
-    You may assume that you have an infinite number of each kind of coin. 
-    */
+  /*
+  You are given coins of different denominations and a total amount of money amount. 
+  Write a function to compute the fewest number of coins that you need to make up that amount. 
+  If that amount of money cannot be made up by any combination of the coins, return -1.
+  
+  Example 1:
+  coins = [1, 2, 5], amount = 11
+  return 3 (11 = 5 + 5 + 1)
+  
+  Example 2:
+  coins = [2], amount = 3
+  return -1.
+  
+  Note:
+  You may assume that you have an infinite number of each kind of coin. 
+  */
 
-    @Test
-    public void test() {
-        Assert.assertEquals(3, coinChange(new int[] { 1, 2, 5 }, 11));
-        Assert.assertEquals(-1, coinChange(new int[] { 2 }, 3));
-    }
+  @Test
+  public void test() {
+    assertEquals(3, coinChange(new int[] { 1, 2, 5 }, 11));
+    assertEquals(-1, coinChange(new int[] { 2 }, 3));
+  }
 
-    public int coinChange(int[] coins, int amount) {
-        long[] result = new long[amount + 1];
-        Arrays.fill(result, 1, result.length, Integer.MAX_VALUE);
-        for (int i = 1; i < result.length; i++)
-            for (int j = 0; j < coins.length; j++)
-                if (i - coins[j] >= 0) result[i] = Math.min(result[i], result[i - coins[j]] + 1);
-        return Integer.MAX_VALUE == result[amount] ? -1 : (int) result[amount];
-    }
+  public int coinChange(int[] coins, int amount) {
+    long[] result = new long[amount + 1];
+    Arrays.fill(result, 1, result.length, Integer.MAX_VALUE);
+    for (int i = 1; i < result.length; i++)
+      for (int j = 0; j < coins.length; j++)
+        if (i - coins[j] >= 0) result[i] = Math.min(result[i], result[i - coins[j]] + 1);
+    return Integer.MAX_VALUE == result[amount] ? -1 : (int) result[amount];
+  }
 
 }
