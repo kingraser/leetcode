5be5911b64ec28e59aa5be3427355bc4083553e5
kinghost <kingraser@gmail.com>
2016-12-21 17:38:32 +0800

    dynamic programming

--- src/leetcode/DungeonGame.java
+++ src/leetcode/DungeonGame.java
@@ -9,44 +9,44 @@ package leetcode;
 // <p>@author wit Initial Created at 2015年10月16日<p>
 //-------------------------------------------------------
 public class DungeonGame {
-    /*
-    The demons had captured the princess (P) and imprisoned her in the bottom-right corner of a dungeon. 
-    The dungeon consists of M x N rooms laid out in a 2D grid. 
-    Our valiant knight (K) was initially positioned in the top-left room and must fight his way through the dungeon to rescue the princess.
-    The knight has an initial health point represented by a positive integer. 
-    If at any point his health point drops to 0 or below, he dies immediately.    
-    Some of the rooms are guarded by demons, so the knight loses health (negative integers) upon entering these rooms; 
-    other rooms are either empty (0's) or contain magic orbs that increase the knight's health (positive integers).    
-    In order to reach the princess as quickly as possible, the knight decides to move only rightward or downward in each step.    
-    Write a function to determine the knight's minimum initial health so that he is able to rescue the princess.    
-    For example, given the dungeon below, 
-    the initial health of the knight must be at least 7 if he follows the optimal path RIGHT-> RIGHT -> DOWN -> DOWN.
-    -2 (K)      -3          3
-    -5          -10         1
-    10          30          -5 (P)
-    
-    Notes:    
-    The knight's health has no upper bound.
-    Any room can contain threats or power-ups, 
-    even the first room the knight enters and the bottom-right room where the princess is imprisoned.
-    */
+  /*
+  The demons had captured the princess (P) and imprisoned her in the bottom-right corner of a dungeon. 
+  The dungeon consists of M x N rooms laid out in a 2D grid. 
+  Our valiant knight (K) was initially positioned in the top-left room and must fight his way through the dungeon to rescue the princess.
+  The knight has an initial health point represented by a positive integer. 
+  If at any point his health point drops to 0 or below, he dies immediately.    
+  Some of the rooms are guarded by demons, so the knight loses health (negative integers) upon entering these rooms; 
+  other rooms are either empty (0's) or contain magic orbs that increase the knight's health (positive integers).    
+  In order to reach the princess as quickly as possible, the knight decides to move only rightward or downward in each step.    
+  Write a function to determine the knight's minimum initial health so that he is able to rescue the princess.    
+  For example, given the dungeon below, 
+  the initial health of the knight must be at least 7 if he follows the optimal path RIGHT-> RIGHT -> DOWN -> DOWN.
+  -2 (K)      -3          3
+  -5          -10         1
+  10          30          -5 (P)
+  
+  Notes:    
+  The knight's health has no upper bound.
+  Any room can contain threats or power-ups, 
+  even the first room the knight enters and the bottom-right room where the princess is imprisoned.
+  */
 
-    /*
-    dp[i][j]表示从i,j开始到达右下需要的最小值
-            状态转移方程dp[i][j] = Math.max(1, Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j]);
-    */
+  /*
+  Let dp[i][j] represents the minimum value from i,j to bottom-right
+  dp[i][j] = Math.max(1, Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j]);
+  */
 
-    public int calculateMinimumHP(int[][] dungeon) {
-        int width = dungeon[0].length, height = dungeon.length;
-        int[][] dp = new int[height][width];
-        dp[height - 1][width - 1] = Math.max(1, 1 - dungeon[height - 1][width - 1]);
-        for (int i = height - 2; i > -1; i--)
-            dp[i][width - 1] = Math.max(1, dp[i + 1][width - 1] - dungeon[i][width - 1]);
-        for (int i = width - 2; i > -1; i--)
-            dp[height - 1][i] = Math.max(1, dp[height - 1][i + 1] - dungeon[height - 1][i]);
-        for (int i = height - 2; i > -1; i--)
-            for (int j = width - 2; j > -1; j--)
-                dp[i][j] = Math.max(1, Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j]);
-        return dp[0][0];
-    }
+  public int calculateMinimumHP(int[][] dungeon) {
+    int width = dungeon[0].length, height = dungeon.length;
+    int[][] dp = new int[height][width];
+    dp[height - 1][width - 1] = Math.max(1, 1 - dungeon[height - 1][width - 1]);
+    for (int i = height - 2; i > -1; i--)
+      dp[i][width - 1] = Math.max(1, dp[i + 1][width - 1] - dungeon[i][width - 1]);
+    for (int i = width - 2; i > -1; i--)
+      dp[height - 1][i] = Math.max(1, dp[height - 1][i + 1] - dungeon[height - 1][i]);
+    for (int i = height - 2; i > -1; i--)
+      for (int j = width - 2; j > -1; j--)
+        dp[i][j] = Math.max(1, Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j]);
+    return dp[0][0];
+  }
 }
--- src/leetcode/EditDistance.java
+++ src/leetcode/EditDistance.java
@@ -10,37 +10,35 @@ package leetcode;
 //-------------------------------------------------------
 public class EditDistance {
 
-    /*
-    Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. 
-    (each operation is counted as 1 step.)
-    
-    You have the following 3 operations permitted on a word:
-    
-    a) Insert a character
-    b) Delete a character
-    c) Replace a character
-    */
+  /*
+  Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. 
+  (each operation is counted as 1 step.)
+  
+  You have the following 3 operations permitted on a word:
+  
+  a) Insert a character
+  b) Delete a character
+  c) Replace a character
+  */
 
-    /*
-            分析
-            设状态为f[i][j],表示 A[0,i]和B[0,j]之间的最小编辑距离。
-            设A[0,i]的形式是 str1c,
-    B[0,j]的形式是 str2d,
-    1. 如果 c==d,则 f[i][j]=f[i-1][j-1];
-    2. 如果 c!=d,
-    (a) 如果将c替换成d,则 f[i][j]=f[i-1][j-1]+1;
-    (b) 如果在c后面添加一个d,则 f[i][j]=f[i][j-1]+1;
-    (c) 如果将c删除,则 f[i][j]=f[i-1][j]+1;
-    */
+  /*
+  Let f[i][j] represents the minimum edit distance between A[0,i] and B[0,j]
+  Let A[0,i] is str1c, B[0,j] is str2d,
+  1. if c==d, then f[i][j]=f[i-1][j-1];
+  2. else c!=d,
+  (a) replace c with d, then f[i][j]=f[i-1][j-1]+1;
+  (b) append c with d, then f[i][j]=f[i][j-1]+1;
+  (c) delete c,则 f[i][j]=f[i-1][j]+1;
+  */
 
-    public int minDistance(String word1, String word2) {
-        int[][] A = new int[word1.length() + 1][word2.length() + 1];
-        for (int i = 0; i < word1.length() + 1; A[i][0] = i, i++);
-        for (int i = 0; i < word2.length() + 1; A[0][i] = i, i++);
-        for (int i = 1; i < word1.length() + 1; i++)
-            for (int j = 1; j < word2.length() + 1; j++)
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) A[i][j] = A[i - 1][j - 1];
-                else A[i][j] = Math.min(Math.min(A[i - 1][j - 1], A[i][j - 1]), A[i - 1][j]) + 1;
-        return A[word1.length()][word2.length()];
-    }
+  public int minDistance(String word1, String word2) {
+    int[][] A = new int[word1.length() + 1][word2.length() + 1];
+    for (int i = 0; i < word1.length() + 1; A[i][0] = i, i++);
+    for (int i = 0; i < word2.length() + 1; A[0][i] = i, i++);
+    for (int i = 1; i < word1.length() + 1; i++)
+      for (int j = 1; j < word2.length() + 1; j++)
+        if (word1.charAt(i - 1) == word2.charAt(j - 1)) A[i][j] = A[i - 1][j - 1];
+        else A[i][j] = Math.min(Math.min(A[i - 1][j - 1], A[i][j - 1]), A[i - 1][j]) + 1;
+    return A[word1.length()][word2.length()];
+  }
 }
