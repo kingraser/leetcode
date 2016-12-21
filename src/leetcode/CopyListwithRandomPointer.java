c83a32650571a05139cc5bd832ebe6ce6d27a915
kinghost <kingraser@gmail.com>
2016-12-21 15:48:50 +0800

    linked list

--- src/leetcode/CopyListwithRandomPointer.java
+++ src/leetcode/CopyListwithRandomPointer.java
@@ -15,19 +15,19 @@ import leetcode.common.RandomListNode;
 //-------------------------------------------------------
 public class CopyListwithRandomPointer {
 
-    /*
-    A linked list is given such that each node contains an additional random pointer 
-    which could point to any node in the list or null.
-    
-    Return a deep copy of the list. 
-    */
+  /*
+  A linked list is given such that each node contains an additional random pointer 
+  which could point to any node in the list or null.
+  
+  Return a deep copy of the list. 
+  */
 
-    public RandomListNode copyRandomList(RandomListNode head) {
-        Map<RandomListNode, RandomListNode> map = new HashMap<>();
-        for (RandomListNode n = head; n != null; map.put(n, new RandomListNode(n.label)), n = n.next);
-        for (RandomListNode n = head; n != null; map.get(n).next = map.get(n.next), map.get(n).random = map
-                .get(n.random), n = n.next);
-        return map.get(head);
-    }
+  public RandomListNode copyRandomList(RandomListNode head) {
+    Map<RandomListNode, RandomListNode> map = new HashMap<>();
+    for (RandomListNode n = head; n != null; map.put(n, new RandomListNode(n.label)), n = n.next);
+    for (RandomListNode n = head; n != null; map.get(n).next = map.get(n.next), map.get(n).random = map
+        .get(n.random), n = n.next);
+    return map.get(head);
+  }
 
 }
