5c6cd2ff5d7fa180ac0867f6b356478c0e2ae7c1
kinghost <kingraser@gmail.com>
2016-12-21 17:17:52 +0800

    linked list

--- src/leetcode/DeleteNodeinaLinkedList.java
+++ src/leetcode/DeleteNodeinaLinkedList.java
@@ -18,6 +18,7 @@ public class DeleteNodeinaLinkedList {
   Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, 
   the linked list should become 1 -> 2 -> 4 after calling your function. 
   */
+
   public void deleteNode(ListNode node) {
     node.val = node.next.val;
     node.next = node.next.next;
