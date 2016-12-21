1921983805d3326b7273fd1c7f3fe840350be82b
kinghost <kingraser@gmail.com>
2016-12-21 14:45:02 +0800

    2 pointers

--- src/leetcode/ContainerWithMostWater.java
+++ src/leetcode/ContainerWithMostWater.java
@@ -9,24 +9,27 @@ package leetcode;
 // <p>@author wit Initial Created at 2015年9月9日<p>
 //-------------------------------------------------------
 public class ContainerWithMostWater {
-    /*
-            给定 int[] 求两个板能存住的最大水量
-            思路:两个指针头,尾.移动矮的一方至比起始移动时高或不能移动为止.水位法保存水量
-    */
+  /*
+  Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). 
+  n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). 
+  Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+  
+  Note: You may not slant the container. 
+  */
 
-    public int maxArea(int[] height) {
-        int l = 0, r = height.length - 1, max = Integer.MIN_VALUE, lastL = max, lastR = max;
-        while (l < r)
-            if (height[l] < lastL) l++;
-            else if (height[r] < lastR) r--;
-            else {
-                max = Math.max(max, Math.min(height[l], height[r]) * (r - l));
-                lastL = height[l];
-                lastR = height[r];
-                if (lastL < lastR) l++;
-                else r--;
-            }
-        return max;
-    }
+  public int maxArea(int[] height) {
+    int max = Integer.MIN_VALUE;
+    for (int l = 0, r = height.length - 1, lastL = max, lastR = max; l < r;)
+      if (height[l] < lastL) l++;
+      else if (height[r] < lastR) r--;
+      else {
+        max = Math.max(max, Math.min(height[l], height[r]) * (r - l));
+        lastL = height[l];
+        lastR = height[r];
+        if (lastL < lastR) l++;
+        else r--;
+      }
+    return max;
+  }
 
 }
