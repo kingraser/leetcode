48313a808d5567459adff2f478b1f2c405edad30
kinghost <kingraser@gmail.com>
2016-12-21 11:24:07 +0800

    bit manipulation

--- src/leetcode/BinaryWatch.java
+++ src/leetcode/BinaryWatch.java
@@ -5,13 +5,14 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.common.collect.Sets;
@@ -21,39 +22,38 @@ import com.google.common.collect.Sets;
 //-------------------------------------------------------
 public class BinaryWatch {
 
-    /*
-    A binary watch has 4 LEDs on the top which represent the hours (0-11), and the 6 LEDs on the bottom represent the minutes (0-59).    
-    Each LED represents a zero or one, with the least significant bit on the right.
-    ●●○○
-    ●○○●●○
-    For example, the above binary watch reads "3:25".    
-    Given a non-negative integer n which represents the number of LEDs that are currently on, return all possible times the watch could represent.    
-    Example:    
-    Input: n = 1
-    Return: ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"] 
-    Note:    
-    The order of output does not matter.
-    The hour must not contain a leading zero, for example "01:00" is not valid, it should be "1:00".
-    The minute must be consist of two digits and may contain a leading zero, for example "10:2" is not valid, it should be "10:02".
-    */
+  /*
+  A binary watch has 4 LEDs on the top which represent the hours (0-11), and the 6 LEDs on the bottom represent the minutes (0-59).    
+  Each LED represents a zero or one, with the least significant bit on the right.
+  ●●○○
+  ●○○●●○
+  For example, the above binary watch reads "3:25".    
+  Given a non-negative integer n which represents the number of LEDs that are currently on, return all possible times the watch could represent.    
+  Example:    
+  Input: n = 1
+  Return: ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"] 
+  Note:    
+  The order of output does not matter.
+  The hour must not contain a leading zero, for example "01:00" is not valid, it should be "1:00".
+  The minute must be consist of two digits and may contain a leading zero, for example "10:2" is not valid, it should be "10:02".
+  */
 
-    private static final Map<Integer, List<String>> RESULT = new HashMap<>();
+  private static final Map<Integer, List<String>> RESULT = new HashMap<>();
 
-    static {
-        for (int i = 0; i < 12; i++)
-            for (int j = 0; j < 60; j++)
-                RESULT.computeIfAbsent(Integer.bitCount(j | i << 6), k -> new ArrayList<>())
-                        .add(String.format("%d:%02d", i, j));
-    }
+  static {
+    for (int i = 0; i < 12; i++)
+      for (int j = 0; j < 60; j++)
+        RESULT.computeIfAbsent(Integer.bitCount(j | (i << 6)), k -> new ArrayList<>())
+            .add(String.format("%d:%02d", i, j));
+  }
 
-    public List<String> readBinaryWatch(int num) {
-        return RESULT.getOrDefault(num, new ArrayList<>());
-    }
+  public List<String> readBinaryWatch(int num) {
+    return RESULT.getOrDefault(num, new ArrayList<>());
+  }
 
-    @Test
-    public void test() {
-        Assert.assertEquals(
-                Sets.newHashSet("1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"),
-                new HashSet<>(readBinaryWatch(1)));
-    }
+  @Test
+  public void test() {
+    assertEquals(Sets.newHashSet("1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"),
+        new HashSet<>(readBinaryWatch(1)));
+  }
 }
--- src/leetcode/BitwiseANDofNumbersRange.java
+++ src/leetcode/BitwiseANDofNumbersRange.java
@@ -5,7 +5,8 @@
  */
 package leetcode;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 //--------------------- Change Logs----------------------
@@ -13,26 +14,22 @@ import org.junit.Test;
 //-------------------------------------------------------
 public class BitwiseANDofNumbersRange {
 
-    /*
-    Given a range [m, n] where 0 <= m <= n <= 2147483647, 
-    return the bitwise AND of all numbers in this range, inclusive.    
-    For example, given the range [5, 7], you should return 4.
-    
-            等价于求m与n二进制编码中同为1的前缀.
-    */
+  /*
+  Given a range [m, n] where 0 <= m <= n <= 2147483647, 
+  return the bitwise AND of all numbers in this range, inclusive.    
+  For example, given the range [5, 7], you should return 4.
+  
+  get m, n common 1 prefix
+  */
 
-    @Test
-    public void test() {
-        Assert.assertEquals(4, rangeBitwiseAnd(5, 7));
-    }
+  @Test
+  public void test() {
+    assertEquals(4, rangeBitwiseAnd(5, 7));
+  }
 
-    public int rangeBitwiseAnd(int m, int n) {
-        int bit = 0;
-        while (m != n) {
-            m >>= 1;
-            n >>= 1;
-            bit++;
-        }
-        return m << bit;
-    }
+  public int rangeBitwiseAnd(int m, int n) {
+    int bit = 0;
+    for (; m != n; m >>= 1, n >>= 1, bit++);
+    return m << bit;
+  }
 }
