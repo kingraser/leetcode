7844df6bdd261b9c400b14fa52c516909678bc76
kinghost <kingraser@gmail.com>
2016-12-21 19:39:58 +0800

    design

--- src/leetcode/LRUCache.java
+++ src/leetcode/LRUCache.java
@@ -13,23 +13,28 @@ import java.util.Map;
 //-------------------------------------------------------
 public class LRUCache {
 
-    //least recently used
-    private Map<Integer, Integer> map;
+  /*
+  Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and set.
+  
+  get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+  set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item. 
+  */
 
-    private int capacity;
+  private Map<Integer, Integer> map;
 
-    public LRUCache(int capacity) {
-        this.capacity = capacity;
-        map = new LinkedHashMap<>(capacity, 1, true);
-    }
+  private int capacity;
 
-    public int get(int key) {
-        if (!map.containsKey(key)) return -1;
-        else return map.get(key);
-    }
+  public LRUCache(int capacity) {
+    this.capacity = capacity;
+    map = new LinkedHashMap<>(capacity, 1, true);
+  }
 
-    public void set(int key, int value) {
-        if (!map.containsKey(key) && map.size() == capacity) map.remove(map.keySet().iterator().next());
-        map.put(key, value);
-    }
+  public int get(int key) {
+    return map.getOrDefault(key, -1);
+  }
+
+  public void set(int key, int value) {
+    if (!map.containsKey(key) && map.size() == capacity) map.remove(map.keySet().iterator().next());
+    map.put(key, value);
+  }
 }
