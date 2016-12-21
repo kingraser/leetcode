855692934c39123a4fad4de5771e943aec8281ed
kinghost <kingraser@gmail.com>
2016-12-21 12:14:26 +0800

    string

--- src/leetcode/ZigZagConversion.java
+++ src/leetcode/ZigZagConversion.java
@@ -5,7 +5,8 @@
  */
 package leetcode;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 //--------------------- Change Logs----------------------
@@ -13,36 +14,36 @@ import org.junit.Test;
 //-------------------------------------------------------
 public class ZigZagConversion {
 
-    /*
-            给定字符串PAYPALISHIRING 和行数3
-    P       A       H       N
-    A   P   L   S   I   I   G
-    Y       I       R
-    And then read line by line: "PAHNAPLSIIGYIR"
-                               
-    Δ=2n-2    1                           2n-1                         4n-3
-    Δ=        2                     2n-2  2n                    4n-4   4n-2
-    Δ=        3               2n-3        2n+1              4n-5       .
-    Δ=        .           .               .               .            .
-    Δ=        .       n+2                 .           3n               .
-    Δ=        n-1 n+1                     3n-3    3n-1                 5n-5
-    Δ=2n-2    n                           3n-2                         5n-4
-    */
+  /*
+  Given string PAYPALISHIRING and rows 3
+  P       A       H       N
+  A   P   L   S   I   I   G
+  Y       I       R
+  And then read line by line: "PAHNAPLSIIGYIR"
+                             
+  Δ=2n-2    1                           2n-1                         4n-3
+  Δ=        2                     2n-2  2n                    4n-4   4n-2
+  Δ=        3               2n-3        2n+1              4n-5       .
+  Δ=        .           .               .               .            .
+  Δ=        .       n+2                 .           3n               .
+  Δ=        n-1 n+1                     3n-3    3n-1                 5n-5
+  Δ=2n-2    n                           3n-2                         5n-4
+  */
 
-    public String convert(String s, int row) {
-        if (row == 1) return s;
-        char[] result = new char[s.length()];
-        for (int i = 0, k = 0, step = (row - 1) << 1; i < row; i++)
-            for (int j = i, i2 = i << 1; j < s.length();) {
-                result[k++] = s.charAt(j);
-                if ((j += step) - i2 < s.length() && i > 0 && i < row - 1) result[k++] = s.charAt(j - i2);
-            }
-        return new String(result);
-    }
+  public String convert(String s, int row) {
+    if (row == 1) return s;
+    char[] result = new char[s.length()];
+    for (int i = 0, k = 0, step = (row - 1) << 1; i < row; i++)
+      for (int j = i, i2 = i << 1; j < s.length();) {
+        result[k++] = s.charAt(j);
+        if ((j += step) - i2 < s.length() && i > 0 && i < row - 1) result[k++] = s.charAt(j - i2);
+      }
+    return new String(result);
+  }
 
-    @Test
-    public void test() {
-        Assert.assertEquals("PAHNAPLSIIGYIR", convert("PAYPALISHIRING", 3));
-    }
+  @Test
+  public void test() {
+    assertEquals("PAHNAPLSIIGYIR", convert("PAYPALISHIRING", 3));
+  }
 
 }
