f479ef1cdb4ab368da52f8b9d14dcdb570a4d8be
kinghost <kingraser@gmail.com>
2016-12-21 16:04:57 +0800

    math

--- src/leetcode/CountPrimes.java
+++ src/leetcode/CountPrimes.java
@@ -5,9 +5,10 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.BitSet;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 //--------------------- Change Logs----------------------
@@ -15,19 +16,19 @@ import org.junit.Test;
 //-------------------------------------------------------
 public class CountPrimes {
 
-    //Count the number of prime numbers less than a non-negative number, n.
-    public int countPrimes(int n) {
-        BitSet bs = new BitSet(n);
-        bs.set(0, 2);
-        int idx = 0, count = 0;
-        for (; (idx = bs.nextClearBit(idx + 1)) < n; count++)
-            for (int i = idx << 1; i < n; bs.set(i), i += idx);
-        return count;
-    }
+  //Count the number of prime numbers less than a non-negative number, n.
+  public int countPrimes(int n) {
+    BitSet bs = new BitSet(n);
+    bs.set(0, 2);
+    int idx = 0, count = 0;
+    for (; (idx = bs.nextClearBit(idx + 1)) < n; count++)
+      for (int i = idx << 1; i < n; bs.set(i), i += idx);
+    return count;
+  }
 
-    @Test
-    public void test() {
-        Assert.assertEquals(4, countPrimes(10));
-    }
+  @Test
+  public void test() {
+    assertEquals(4, countPrimes(10));
+  }
 
 }
