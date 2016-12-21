993ae71b580147ba585fee0191ed9be52e4552d1
kinghost <kingraser@gmail.com>
2016-12-21 19:35:20 +0800

    string

--- src/leetcode/ExcelSheetColumnNumber.java
+++ src/leetcode/ExcelSheetColumnNumber.java
@@ -5,7 +5,8 @@
  */
 package leetcode;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 //--------------------- Change Logs----------------------
@@ -13,25 +14,25 @@ import org.junit.Test;
 //-------------------------------------------------------
 public class ExcelSheetColumnNumber {
 
-    /*
-    A -> 1
-    B -> 2
-    C -> 3
-    ...
-    Z -> 26
-    AA -> 27
-    AB -> 28 
-    */
+  /*
+  A -> 1
+  B -> 2
+  C -> 3
+  ...
+  Z -> 26
+  AA -> 27
+  AB -> 28 
+  */
 
-    public int titleToNumber(String s) {
-        int n = 0;
-        for (int i = s.length() - 1, radix = 1; i >= 0; n += (s.charAt(i--) - '@') * radix, radix *= 26);
-        return n;
-    }
+  public int titleToNumber(String s) {
+    int n = 0;
+    for (int i = s.length() - 1, radix = 1; i >= 0; n += (s.charAt(i--) - '@') * radix, radix *= 26);
+    return n;
+  }
 
-    @Test
-    public void test() {
-        Assert.assertEquals(28, titleToNumber("AB"));
-    }
+  @Test
+  public void test() {
+    assertEquals(28, titleToNumber("AB"));
+  }
 
 }
--- src/leetcode/ExcelSheetColumnTitle.java
+++ src/leetcode/ExcelSheetColumnTitle.java
@@ -5,33 +5,34 @@
  */
 package leetcode;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年10月16日<p>
 //-------------------------------------------------------
 public class ExcelSheetColumnTitle {
-    /*
-    Given a positive integer, return its corresponding column title as appear in an Excel sheet.    
-    For example:
-    1 -> A
-    2 -> B
-    3 -> C
-    ...
-    26 -> Z
-    27 -> AA
-    28 -> AB 
-    */
+  /*
+  Given a positive integer, return its corresponding column title as appear in an Excel sheet.    
+  For example:
+  1 -> A
+  2 -> B
+  3 -> C
+  ...
+  26 -> Z
+  27 -> AA
+  28 -> AB 
+  */
 
-    @Test
-    public void test() {
-        Assert.assertEquals("AB", convertToTitle(28));
-    }
+  @Test
+  public void test() {
+    assertEquals("AB", convertToTitle(28));
+  }
 
-    public String convertToTitle(int n) {
-        StringBuilder s = new StringBuilder("");
-        for (; n-- > 0; s.append((char) ('A' + n % 26)), n /= 26);
-        return s.reverse().toString();
-    }
+  public String convertToTitle(int n) {
+    StringBuilder s = new StringBuilder("");
+    for (; n-- > 0; s.append((char) ('A' + n % 26)), n /= 26);
+    return s.reverse().toString();
+  }
 }
