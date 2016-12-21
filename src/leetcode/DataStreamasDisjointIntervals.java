959f829abc4fc817d0cf127680919cbb3ab89bf7
kinghost <kingraser@gmail.com>
2016-12-21 17:13:36 +0800

    design

--- src/leetcode/DataStreamasDisjointIntervals.java
+++ src/leetcode/DataStreamasDisjointIntervals.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import leetcode.common.Interval;
@@ -21,42 +21,57 @@ import leetcode.common.Interval;
 //-------------------------------------------------------
 public class DataStreamasDisjointIntervals {
 
-    @Test
-    public void test() {
-        SummaryRanges summaryRanges = new SummaryRanges();
-        summaryRanges.addNum(1);
-        Assert.assertEquals(Arrays.asList(new Interval(1, 1)), summaryRanges.getIntervals());
-        summaryRanges.addNum(3);
-        Assert.assertEquals(Arrays.asList(new Interval(1, 1), new Interval(3, 3)), summaryRanges.getIntervals());
-        summaryRanges.addNum(7);
-        Assert.assertEquals(Arrays.asList(new Interval(1, 1), new Interval(3, 3), new Interval(7, 7)),
-                summaryRanges.getIntervals());
-        summaryRanges.addNum(2);
-        Assert.assertEquals(Arrays.asList(new Interval(1, 3), new Interval(7, 7)), summaryRanges.getIntervals());
-        summaryRanges.addNum(6);
-        Assert.assertEquals(Arrays.asList(new Interval(1, 3), new Interval(6, 7)), summaryRanges.getIntervals());
+  /*
+  Given a data stream input of non-negative integers a1, a2, ..., an, ..., 
+  summarize the numbers seen so far as a list of disjoint intervals.
+  
+  For example, suppose the integers from the data stream are 1, 3, 7, 2, 6, ..., then the summary will be:
+  
+  [1, 1]
+  [1, 1], [3, 3]
+  [1, 1], [3, 3], [7, 7]
+  [1, 3], [7, 7]
+  [1, 3], [6, 7]
+  
+  Follow up:
+  What if there are lots of merges and the number of disjoint intervals are small compared to the data stream's size? 
+  */
+
+  @Test
+  public void test() {
+    SummaryRanges summaryRanges = new SummaryRanges();
+    summaryRanges.addNum(1);
+    assertEquals(Arrays.asList(new Interval(1, 1)), summaryRanges.getIntervals());
+    summaryRanges.addNum(3);
+    assertEquals(Arrays.asList(new Interval(1, 1), new Interval(3, 3)), summaryRanges.getIntervals());
+    summaryRanges.addNum(7);
+    assertEquals(Arrays.asList(new Interval(1, 1), new Interval(3, 3), new Interval(7, 7)),
+        summaryRanges.getIntervals());
+    summaryRanges.addNum(2);
+    assertEquals(Arrays.asList(new Interval(1, 3), new Interval(7, 7)), summaryRanges.getIntervals());
+    summaryRanges.addNum(6);
+    assertEquals(Arrays.asList(new Interval(1, 3), new Interval(6, 7)), summaryRanges.getIntervals());
+  }
+
+  public class SummaryRanges {
+    TreeMap<Integer, Interval> map = new TreeMap<>();
+
+    public void addNum(int val) {
+      if (map.containsKey(val)) return;
+      Entry<Integer, Interval> l = map.lowerEntry(val), h = map.higherEntry(val);
+      if (l != null && h != null && l.getValue().end + 1 == val && h.getKey() == val + 1) {
+        l.getValue().end = h.getValue().end;
+        map.remove(h.getKey());
+      } else if (l != null && l.getValue().end + 1 >= val) l.getValue().end = Math.max(l.getValue().end, val);
+      else if (h != null && h.getKey() == val + 1) {
+        h.getValue().start = val;
+        map.put(val, h.getValue());
+        map.remove(h.getKey());
+      } else map.put(val, new Interval(val, val));
     }
 
-    public class SummaryRanges {
-
-        TreeMap<Integer, Interval> map = new TreeMap<>();
-
-        public void addNum(int val) {
-            if (map.containsKey(val)) return;
-            Entry<Integer, Interval> l = map.lowerEntry(val), h = map.higherEntry(val);
-            if (l != null && h != null && l.getValue().end + 1 == val && h.getKey() == val + 1) {
-                l.getValue().end = h.getValue().end;
-                map.remove(h.getKey());
-            } else if (l != null && l.getValue().end + 1 >= val) l.getValue().end = Math.max(l.getValue().end, val);
-            else if (h != null && h.getKey() == val + 1) {
-                h.getValue().start = val;
-                map.put(val, h.getValue());
-                map.remove(h.getKey());
-            } else map.put(val, new Interval(val, val));
-        }
-
-        public List<Interval> getIntervals() {
-            return new ArrayList<>(map.values());
-        }
+    public List<Interval> getIntervals() {
+      return new ArrayList<>(map.values());
     }
+  }
 }
