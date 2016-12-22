db5781c04ebcfb25c8ce8dc044033af91c20d262
kinghost <kingraser@gmail.com>
2016-12-22 11:34:04 +0800

    binary search

--- src/leetcode/FindMinimuminRotatedSortedArray.java
+++ src/leetcode/FindMinimuminRotatedSortedArray.java
@@ -5,7 +5,8 @@
  */
 package leetcode;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 //--------------------- Change Logs----------------------
@@ -13,23 +14,23 @@ import org.junit.Test;
 //-------------------------------------------------------
 public class FindMinimuminRotatedSortedArray {
 
-    /*
-    Suppose a sorted array is rotated at some pivot unknown to you beforehand.    
-    (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).    
-    Find the minimum element.    
-    You may assume no duplicate exists in the array.
-    */
+  /*
+  Suppose a sorted array is rotated at some pivot unknown to you beforehand.    
+  (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).    
+  Find the minimum element.    
+  You may assume no duplicate exists in the array.
+  */
 
-    @Test
-    public void test() {
-        Assert.assertEquals(0, findMin(new int[] { 4, 5, 6, 7, 0, 1, 2 }));
-    }
+  @Test
+  public void test() {
+    assertEquals(0, findMin(new int[] { 4, 5, 6, 7, 0, 1, 2 }));
+  }
 
-    public int findMin(int[] A) {
-        int beg = 0, end = A.length - 1, mid;
-        while (beg < end && A[end] <= A[beg])
-            if (A[mid = (beg + end) >> 1] < A[beg]) end = mid;
-            else beg = mid + 1;
-        return A[beg];
-    }
+  public int findMin(int[] A) {
+    int start = 0, end = A.length - 1, mid;
+    while (start < end && A[end] <= A[start])
+      if (A[mid = (start + end) >> 1] < A[start]) end = mid;
+      else start = mid + 1;
+    return A[start];
+  }
 }
--- src/leetcode/FindPeakElement.java
+++ src/leetcode/FindPeakElement.java
@@ -5,7 +5,8 @@
  */
 package leetcode;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 //--------------------- Change Logs----------------------
@@ -13,20 +14,32 @@ import org.junit.Test;
 //-------------------------------------------------------
 public class FindPeakElement {
 
-    /*
-    For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
-    */
+  /*
+  A peak element is an element that is greater than its neighbors.
+  
+  Given an input array where num[i] ≠ num[i+1], find a peak element and return its index.
+  
+  The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+  
+  You may imagine that num[-1] = num[n] = -∞.
+  
+  For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+  
+  Note:
+  
+  Your solution should be in logarithmic complexity.  
+  */
 
-    @Test
-    public void test() {
-        Assert.assertEquals(2, findPeakElement(new int[] { 1, 2, 3, 1 }));
-    }
+  @Test
+  public void test() {
+    assertEquals(2, findPeakElement(new int[] { 1, 2, 3, 1 }));
+  }
 
-    public int findPeakElement(int[] num) {
-        int l = 0, r = num.length - 1, mid;
-        while (l < r)
-            if (num[mid = (l + r) >> 1] < num[mid + 1]) l = mid + 1;
-            else r = mid;
-        return l;
-    }
+  public int findPeakElement(int[] num) {
+    int l = 0, r = num.length - 1, mid;
+    while (l < r)
+      if (num[mid = (l + r) >> 1] < num[mid + 1]) l = mid + 1;
+      else r = mid;
+    return l;
+  }
 }
