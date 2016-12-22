202cb09b59eb08b1fd6f0a935e46bb854a63dc01
kinghost <kingraser@gmail.com>
2016-12-22 11:26:21 +0800

    heap

--- src/leetcode/FindMedianfromDataStream.java
+++ src/leetcode/FindMedianfromDataStream.java
@@ -5,10 +5,11 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Collections;
 import java.util.PriorityQueue;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 //--------------------- Change Logs----------------------
@@ -16,54 +17,54 @@ import org.junit.Test;
 //-------------------------------------------------------
 public class FindMedianfromDataStream {
 
-    /*
-    Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. So the median is the mean of the two middle value.
-    Examples:
-    
-    [2,3,4] , the median is 3
-    
-    [2,3], the median is (2 + 3) / 2 = 2.5
-    
-    Design a data structure that supports the following two operations:
-    
-    void addNum(int num) - Add a integer number from the data stream to the data structure.
-    double findMedian() - Return the median of all elements so far.
-    
-    For example:
-    
-    add(1)
-    add(2)
-    findMedian() -> 1.5
-    add(3) 
-    findMedian() -> 2
-    */
+  /*
+  Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. So the median is the mean of the two middle value.
+  Examples:
+  
+  [2,3,4] , the median is 3
+  
+  [2,3], the median is (2 + 3) / 2 = 2.5
+  
+  Design a data structure that supports the following two operations:
+  
+  void addNum(int num) - Add a integer number from the data stream to the data structure.
+  double findMedian() - Return the median of all elements so far.
+  
+  For example:
+  
+  add(1)
+  add(2)
+  findMedian() -> 1.5
+  add(3) 
+  findMedian() -> 2
+  */
 
-    @Test
-    public void test() {
-        addNum(1);
-        addNum(2);
-        Assert.assertEquals(1.5, findMedian(), 0);
-        addNum(3);
-        Assert.assertEquals(2, findMedian(), 0);
-    }
+  @Test
+  public void test() {
+    addNum(1);
+    addNum(2);
+    assertEquals(1.5, findMedian(), 0);
+    addNum(3);
+    assertEquals(2, findMedian(), 0);
+  }
 
-    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+  PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
-    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+  PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
 
-    // Adds a number into the data structure.
-    public void addNum(int num) {
-        if (maxHeap.isEmpty()) maxHeap.offer(num);
-        else if (num <= maxHeap.peek()) maxHeap.offer(num);
-        else minHeap.offer(num);
-        for (; maxHeap.size() < minHeap.size(); maxHeap.offer(minHeap.poll()));
-        for (; minHeap.size() < maxHeap.size() - 1; minHeap.offer(maxHeap.poll()));
-    }
+  // Adds a number into the data structure.
+  public void addNum(int num) {
+    if (maxHeap.isEmpty()) maxHeap.offer(num);
+    else if (num <= maxHeap.peek()) maxHeap.offer(num);
+    else minHeap.offer(num);
+    for (; maxHeap.size() < minHeap.size(); maxHeap.offer(minHeap.poll()));
+    for (; minHeap.size() < maxHeap.size() - 1; minHeap.offer(maxHeap.poll()));
+  }
 
-    // Returns the median of current data stream
-    public double findMedian() {
-        if (maxHeap.size() == minHeap.size()) return (double) (maxHeap.peek() + minHeap.peek()) / 2;
-        return maxHeap.peek();
-    }
+  // Returns the median of current data stream
+  public double findMedian() {
+    if (maxHeap.size() == minHeap.size()) return (double) (maxHeap.peek() + minHeap.peek()) / 2;
+    return maxHeap.peek();
+  }
 
 }
