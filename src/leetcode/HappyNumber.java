f552b1072b2ed3b2311b2f743e7227874b4b2fb9
kinghost <kingraser@gmail.com>
2016-12-22 12:20:21 +0800

    hash-table

--- src/leetcode/HappyNumber.java
+++ src/leetcode/HappyNumber.java
@@ -5,9 +5,10 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.Set;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.common.collect.Sets;
@@ -17,26 +18,26 @@ import com.google.common.collect.Sets;
 //-------------------------------------------------------
 public class HappyNumber {
 
-    /*
-    Example: 19 is a happy number
-    
-    pow(1,2) + pow(9,2) = 82
-    pow(8,2) + pow(2,2) = 68
-    pow(6,2) + pow(8,2) = 100
-    pow(1,2) + pow(0,2) + pow(0,2) = 1
-    */
+  /*
+  Example: 19 is a happy number
+  
+  pow(1,2) + pow(9,2) = 82
+  pow(8,2) + pow(2,2) = 68
+  pow(6,2) + pow(8,2) = 100
+  pow(1,2) + pow(0,2) + pow(0,2) = 1
+  */
 
-    public boolean isHappy(int n) {
-        Set<Integer> set = Sets.newHashSet(n);
-        for (int result = 0; n != 1; n = result, result = 0) {
-            for (int radix; n != 0; result += (radix = n % 10) * radix, n /= 10);
-            if (!set.add(result)) return false;
-        }
-        return true;
+  public boolean isHappy(int n) {
+    Set<Integer> set = Sets.newHashSet(n);
+    for (int result = 0; n != 1; n = result, result = 0) {
+      for (int radix; n != 0; result += (radix = n % 10) * radix, n /= 10);
+      if (!set.add(result)) return false;
     }
+    return true;
+  }
 
-    @Test
-    public void test() {
-        Assert.assertTrue(isHappy(19));
-    }
+  @Test
+  public void test() {
+    assertTrue(isHappy(19));
+  }
 }
