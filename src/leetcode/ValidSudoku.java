ec15039b55ed053bafac5ea0ee402c5ff361ae34
kinghost <kingraser@gmail.com>
2016-12-22 14:53:17 +0800

    array

--- src/leetcode/ValidSudoku.java
+++ src/leetcode/ValidSudoku.java
@@ -5,7 +5,8 @@
  */
 package leetcode;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 //--------------------- Change Logs----------------------
@@ -13,28 +14,27 @@ import org.junit.Test;
 //-------------------------------------------------------
 public class ValidSudoku {
 
-    public boolean isValidSudoku(char[][] board) {
-        boolean[][] row = new boolean[9][9], col = new boolean[9][9], box = new boolean[9][9];
-        for (int i = 0, num; i < 9; i++)
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') continue;
-                num = board[i][j] - '1';
-                if (row[i][num] || col[j][num] || box[(i / 3) * 3 + j / 3][num]) return false;
-                row[i][num] = true;
-                col[j][num] = true;
-                box[(i / 3) * 3 + j / 3][num] = true;
-            }
-        return true;
-    }
+  public boolean isValidSudoku(char[][] board) {
+    boolean[][] row = new boolean[9][9], col = new boolean[9][9], box = new boolean[9][9];
+    for (int i = 0, num; i < 9; i++)
+      for (int j = 0; j < 9; j++) {
+        if (board[i][j] == '.') continue;
+        if (row[i][num = board[i][j] - '1'] || col[j][num] || box[(i / 3) * 3 + j / 3][num]) return false;
+        row[i][num] = true;
+        col[j][num] = true;
+        box[(i / 3) * 3 + j / 3][num] = true;
+      }
+    return true;
+  }
 
-    @Test
-    public void test() {
-        char[][] board = new char[][] { { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
-                { '6', '.', '.', '1', '9', '5', '.', '.', '.' }, { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
-                { '8', '.', '.', '.', '6', '.', '.', '.', '3' }, { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
-                { '7', '.', '.', '.', '2', '.', '.', '.', '6' }, { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
-                { '.', '.', '.', '4', '1', '9', '.', '.', '5' }, { '.', '.', '.', '.', '8', '.', '.', '7', '9' } };
-        Assert.assertTrue(isValidSudoku(board));;
-    }
+  @Test
+  public void test() {
+    char[][] board = new char[][] { { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+        { '6', '.', '.', '1', '9', '5', '.', '.', '.' }, { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+        { '8', '.', '.', '.', '6', '.', '.', '.', '3' }, { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+        { '7', '.', '.', '.', '2', '.', '.', '.', '6' }, { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+        { '.', '.', '.', '4', '1', '9', '.', '.', '5' }, { '.', '.', '.', '.', '8', '.', '.', '7', '9' } };
+    assertTrue(isValidSudoku(board));;
+  }
 
 }
