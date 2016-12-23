f01da3694c97047ab6fef78761802fbe6bdb754c
kinghost <kingraser@gmail.com>
2016-12-23 11:25:22 +0800

    Reservoir Sampling

--- src/leetcode/LinkedListRandomNode.java
+++ src/leetcode/LinkedListRandomNode.java
@@ -5,8 +5,6 @@
  */
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 import leetcode.common.ListNode;
@@ -16,73 +14,36 @@ import leetcode.common.ListNode;
 //-------------------------------------------------------
 public class LinkedListRandomNode {
 
-  class Solution1 {
+  /*
+  Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+  
+  Follow up:
+  What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
+  
+  Example:
+  
+  // Init a singly linked list [1,2,3].
+  ListNode head = new ListNode(1);
+  head.next = new ListNode(2);
+  head.next.next = new ListNode(3);
+  Solution solution = new Solution(head);
+  
+  // getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+  solution.getRandom(); 
+  */
 
-    ListNode root;
+  ListNode root;
+  Random random = new Random();
 
-    Random random = new Random();
-
-    public Solution1(ListNode root) {
-      this.root = root;
-    }
-
-    public int getRandom() {
-      ListNode result = null, current = root;
-      for (int n = 1; current != null; current = current.next)
-        if (random.nextInt(n++) == 0) result = current;
-      return result.val;
-    }
+  public LinkedListRandomNode(ListNode root) {
+    this.root = root;
   }
 
-  class Solution2 {
-
-    ListNode root;
-
-    int count = -1;
-
-    Random random = new Random();
-
-    public Solution2(ListNode root) {
-      this.root = root;
-    }
-
-    public int getRandom() {
-      if (count == -1) count = getLength(root);
-      return get(root, random.nextInt(count)).val;
-    }
-
-    private ListNode get(ListNode node, int count) {
-      for (; count-- > 0; node = node.next);
-      return node;
-    }
-
-    public int getLength(ListNode node) {
-      int count = 0;
-      for (; node != null; count++, node = node.next);
-      return count;
-    }
+  public int getRandom() {
+    ListNode result = null, current = root;
+    for (int n = 1; current != null; current = current.next)
+      if (random.nextInt(n++) == 0) result = current;
+    return result.val;
   }
 
-  class Solution3 {
-
-    ListNode root;
-
-    List<Integer> list;
-
-    Random random = new Random();
-
-    public Solution3(ListNode root) {
-      this.root = root;
-    }
-
-    public int getRandom() {
-      if (list == null) copy(root);
-      return list.get(random.nextInt(list.size()));
-    }
-
-    public void copy(ListNode node) {
-      list = new ArrayList<>();
-      for (; node != null; list.add(node.val), node = node.next);
-    }
-  }
 }
