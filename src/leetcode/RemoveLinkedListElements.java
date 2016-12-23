170cd17bd5a834c3ae0f1778ff0042012d32d3c0
kinghost <kingraser@gmail.com>
2016-12-23 16:32:55 +0800

    linked list

--- src/leetcode/RemoveLinkedListElements.java
+++ src/leetcode/RemoveLinkedListElements.java
@@ -5,6 +5,11 @@
  */
 package leetcode;
 
+import static leetcode.common.ListNode.list;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import leetcode.common.ListNode;
 
 //--------------------- Change Logs----------------------
@@ -12,25 +17,22 @@ import leetcode.common.ListNode;
 //-------------------------------------------------------
 public class RemoveLinkedListElements {
 
-    /*
-    Example
-    Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
-    Return: 1 --> 2 --> 3 --> 4 --> 5 
-    */
+  /*
+  Example
+  Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+  Return: 1 --> 2 --> 3 --> 4 --> 5 
+  */
 
-    public ListNode removeElements(ListNode head, int val) {
-        if (head == null) return null;
-        for (ListNode p = head; p.next != null;)
-            if (p.next.val == val) p.next = p.next.next;
-            else p = p.next;
-        return head.val == val ? head.next : head;
-    }
+  @Test
+  public void test() {
+    assertEquals(list(1, 2, 3, 4, 5), removeElements(list(1, 2, 6, 3, 4, 5), 6));
+  }
 
-    public ListNode removeElementsII(ListNode head, int val) {
-        if (head == null) return null;
-        if (head.val == val) return removeElements(head.next, val);
-        head.next = removeElements(head.next, val);
-        return head;
-    }
+  public ListNode removeElements(ListNode head, int val) {
+    if (head == null) return null;
+    if (head.val == val) return removeElements(head.next, val);
+    head.next = removeElements(head.next, val);
+    return head;
+  }
 
 }
