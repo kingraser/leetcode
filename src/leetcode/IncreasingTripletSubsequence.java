c8b598d08641045c366b77feedbe9f9a082a11e9
kinghost <kingraser@gmail.com>
2016-12-23 14:06:46 +0800

    array

--- src/leetcode/IncreasingTripletSubsequence.java
+++ src/leetcode/IncreasingTripletSubsequence.java
@@ -5,9 +5,11 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Arrays;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 //--------------------- Change Logs----------------------
@@ -15,47 +17,45 @@ import org.junit.Test;
 //-------------------------------------------------------
 public class IncreasingTripletSubsequence {
 
-    /*
-    Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array.    
-    Formally the function should:
-    
-    Return true if there exists i, j, k
-    such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false. 
-    
-    Your algorithm should run in O(n) time complexity and O(1) space complexity.
-    
-    Examples:
-    Given [1, 2, 3, 4, 5],
-    return true.
-    
-    Given [5, 4, 3, 2, 1],
-    return false. 
-    */
+  /*
+  Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array.    
+  Formally the function should:
+  
+  Return true if there exists i, j, k
+  such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false. 
+  
+  Your algorithm should run in O(n) time complexity and O(1) space complexity.
+  
+  Examples:
+  Given [1, 2, 3, 4, 5],
+  return true.
+  
+  Given [5, 4, 3, 2, 1],
+  return false. 
+  */
 
-    @Test
-    public void test() {
-        Assert.assertTrue(increasingTriplet(new int[] { 2, 1, 5, 0, 4, 6 }));
-        Assert.assertTrue(increasingTriplet(new int[] { 1, 2, 3, 4, 5 }));
-        Assert.assertFalse(increasingTriplet(new int[] { 5, 4, 3, 2, 1 }));
-    }
+  @Test
+  public void test() {
+    assertTrue(increasingTriplet(new int[] { 2, 1, 5, 0, 4, 6 }));
+    assertTrue(increasingTriplet(new int[] { 1, 2, 3, 4, 5 }));
+    assertFalse(increasingTriplet(new int[] { 5, 4, 3, 2, 1 }));
+  }
 
-    public boolean increasingTriplet(int[] nums) {
-        return increasing(nums, 3);
+  public boolean increasingTriplet(int[] nums) {
+    return increasing(nums, 3);
+  }
 
-    }
-
-    private boolean increasing(int[] nums, int k) {
-        int[] min = new int[k - 1];
-        Arrays.fill(min, Integer.MAX_VALUE);
-        A: for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < min.length; j++)
-                if (nums[i] < min[j]) {
-                    min[j] = nums[i];
-                    continue A;
-                }
-            return true;
+  private boolean increasing(int[] nums, int k) {
+    int[] min = new int[--k];
+    Arrays.fill(min, Integer.MAX_VALUE);
+    A: for (int i = 0; i < nums.length; i++) {
+      for (int j = 0; j < min.length; j++)
+        if (nums[i] < min[j]) {
+          min[j] = nums[i];
+          continue A;
         }
-        return false;
-
+      return true;
     }
+    return false;
+  }
 }
