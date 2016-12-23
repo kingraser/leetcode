23c53d7e0e20e642ec744a9625cabcb0228ad35f
kinghost <kingraser@gmail.com>
2016-12-23 14:04:47 +0800

    trie

--- src/leetcode/ImplementTriePrefixTree.java
+++ src/leetcode/ImplementTriePrefixTree.java
@@ -9,14 +9,15 @@ package leetcode;
 // <p>@author wit Initial Created at 2015年10月16日<p>
 //-------------------------------------------------------
 public class ImplementTriePrefixTree {
-    /*
-    Implement a trie with insert, search, and startsWith methods.    
-    Note:
-    You may assume that all inputs are consist of lowercase letters a-z. 
-    */
 
-    /*
-    @see leetcode.common.Trie
-    */
+  /*
+  Implement a trie with insert, search, and startsWith methods.    
+  Note:
+  You may assume that all inputs are consist of lowercase letters a-z. 
+  */
+
+  /*
+  @see leetcode.common.Trie
+  */
 
 }
