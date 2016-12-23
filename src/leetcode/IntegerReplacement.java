85922bd7a3cb5718d7cc1481f85e5c2036ef8276
kinghost <kingraser@gmail.com>
2016-12-23 14:52:51 +0800

    math

---
+++ src/leetcode/IntegerReplacement.java
@@ -0,0 +1,56 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2015 Sogou.com. All Rights Reserved.
+ */
+package leetcode;
+
+import org.junit.Test;
+
+//--------------------- Change Logs----------------------
+//@author wangwenlong Initial Created at 2016年9月12日;
+//-------------------------------------------------------
+public class IntegerReplacement {
+
+  /*
+  Given a positive integer n and you can do operations as follow:
+    If n is even, replace n with n/2.
+    If n is odd, you can replace n with either n + 1 or n - 1.
+  What is the minimum number of replacements needed for n to become 1?
+  
+  Example 1:  
+  Input:
+  8  
+  Output:
+  3  
+  Explanation:
+  8 -> 4 -> 2 -> 1
+  
+  Example 2:  
+  Input:
+  7  
+  Output:
+  4  
+  Explanation:
+  7 -> 8 -> 4 -> 2 -> 1
+  or
+  7 -> 6 -> 3 -> 2 -> 1
+  */
+
+  @Test
+  public void test() {
+    assertEquals(3, integerReplacement(8));
+    assertEquals(4, integerReplacement(7));
+    assertEquals(32, integerReplacement(2147483647));
+  }
+
+  public int integerReplacement(int n) {
+    int count = 0;
+    for (long m = n; m != 1; count++)
+      if (0 == (m & 1)) m >>= 1;
+      else if (m == 3 || (m & 2) == 0) m--;
+      else m++;
+    return count;
+  }
+
+}
