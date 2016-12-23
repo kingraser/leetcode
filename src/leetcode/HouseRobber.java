5ef62d29b9777eccb5af441df5a9d7859e6b7440
kinghost <kingraser@gmail.com>
2016-12-23 11:32:23 +0800

    greedy

--- src/leetcode/HouseRobber.java
+++ src/leetcode/HouseRobber.java
@@ -5,7 +5,8 @@
  */
 package leetcode;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 //--------------------- Change Logs----------------------
@@ -13,28 +14,28 @@ import org.junit.Test;
 //-------------------------------------------------------
 public class HouseRobber {
 
-    /*
-    You are a professional robber planning to rob houses along a street. 
-    Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them 
-    is that adjacent houses have security system connected 
-    and it will automatically contact the police if two adjacent houses were broken into on the same night.
-    Given a list of non-negative integers representing the amount of money of each house, 
-    determine the maximum amount of money you can rob tonight without alerting the police.
-    */
+  /*
+  You are a professional robber planning to rob houses along a street. 
+  Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them 
+  is that adjacent houses have security system connected 
+  and it will automatically contact the police if two adjacent houses were broken into on the same night.
+  Given a list of non-negative integers representing the amount of money of each house, 
+  determine the maximum amount of money you can rob tonight without alerting the police.
+  */
 
-    public int rob(int[] nums) {
-        int current = 0, previous = 0;//current for rob current, previous for rob previous
-        for (int i = 0; i < nums.length;) {
-            int newCurrent = previous + nums[i++];
-            previous = Math.max(previous, current);
-            current = newCurrent;
-        }
-        return Math.max(current, previous);
+  public int rob(int[] nums) {
+    int current = 0, previous = 0;//current for rob current, previous for rob previous
+    for (int i = 0; i < nums.length;) {
+      int newCurrent = previous + nums[i++];
+      previous = Math.max(previous, current);
+      current = newCurrent;
     }
+    return Math.max(current, previous);
+  }
 
-    @Test
-    public void test() {
-        int[] input = new int[] { 1, 1, 3, 4, 5, 7, 8, 9, 10, 11, 31, 32, 47 };
-        Assert.assertEquals(105, rob(input));
-    }
+  @Test
+  public void test() {
+    int[] input = new int[] { 1, 1, 3, 4, 5, 7, 8, 9, 10, 11, 31, 32, 47 };
+    assertEquals(105, rob(input));
+  }
 }
--- src/leetcode/HouseRobberII.java
+++ src/leetcode/HouseRobberII.java
@@ -12,34 +12,34 @@ import java.util.Arrays;
 //-------------------------------------------------------
 public class HouseRobberII {
 
-    /*
-    Note: This is an extension of House Robber.    
-    After robbing those houses on that street, 
-    the thief has found himself a new place for his thievery 
-    so that he will not get too much attention. 
-    This time, all houses at this place are arranged in a circle. 
-    That means the first house is the neighbor of the last one. 
-    Meanwhile, the security system for these houses remain the same as for those in the previous street.
-    
-    Given a list of non-negative integers representing the amount of money of each house, 
-    determine the maximum amount of money you can rob tonight without alerting the police.
-    */
+  /*
+  Note: This is an extension of House Robber.    
+  After robbing those houses on that street, 
+  the thief has found himself a new place for his thievery 
+  so that he will not get too much attention. 
+  This time, all houses at this place are arranged in a circle. 
+  That means the first house is the neighbor of the last one. 
+  Meanwhile, the security system for these houses remain the same as for those in the previous street.
+  
+  Given a list of non-negative integers representing the amount of money of each house, 
+  determine the maximum amount of money you can rob tonight without alerting the police.
+  */
 
-    //环
-    //两种情况:1抢第一家到倒数第二家,2抢第二家到最后一家,取最大值
+  //circle
+  //2 cases:1 rob first to the last but on,2 rob second to last,find the max
 
-    public int rob(int[] nums) {
-        if (nums.length == 0) return 0;
-        if (nums.length == 1) return nums[0];
-        if (nums.length == 2) return Math.max(nums[0], nums[1]);
+  public int rob(int[] nums) {
+    if (nums.length == 0) return 0;
+    if (nums.length == 1) return nums[0];
+    if (nums.length == 2) return Math.max(nums[0], nums[1]);
 
-        int[] a = Arrays.copyOf(nums, nums.length);
-        a[2] += a[0];//case1
-        for (int i = 3; i < a.length - 1; i++)
-            a[i] += Math.max(a[i - 2], a[i - 3]);
-        nums[nums.length - 3] += nums[nums.length - 1];//case2
-        for (int i = nums.length - 4; i > 0; i--)
-            nums[i] += Math.max(nums[i + 2], nums[i + 3]);
-        return Math.max(Math.max(a[a.length - 3], a[a.length - 2]), Math.max(nums[1], nums[2]));
-    }
+    int[] a = Arrays.copyOf(nums, nums.length);
+    a[2] += a[0];//case1
+    for (int i = 3; i < a.length - 1; i++)
+      a[i] += Math.max(a[i - 2], a[i - 3]);
+    nums[nums.length - 3] += nums[nums.length - 1];//case2
+    for (int i = nums.length - 4; i > 0; i--)
+      nums[i] += Math.max(nums[i + 2], nums[i + 3]);
+    return Math.max(Math.max(a[a.length - 3], a[a.length - 2]), Math.max(nums[1], nums[2]));
+  }
 }
