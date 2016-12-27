a9b155cbf06f9d7a07432441a33d0805eda877ad
kinghost <kingraser@gmail.com>
2016-12-27 17:01:11 +0800

    dfs

--- src/leetcode/IslandPerimeter.java
+++ src/leetcode/IslandPerimeter.java
@@ -1,9 +1,30 @@
 package leetcode;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class IslandPerimeter {
+
+  /*
+  You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water. Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells). The island doesn't have "lakes" (water inside that isn't connected to the water around the island). One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+  
+  Example:
+  
+  [
+  [0,1,0,0],
+  [1,1,1,0],
+  [0,1,0,0],
+  [1,1,0,0]]
+  
+  Answer: 16
+  Explanation: The perimeter is the 16 @ stripes in the image below: 
+   __ __ __ __
+  |__|_@|__|__|
+  |_@|_@|_@|__|
+  |__|_@|__|__|
+  |_@|_@|__|__|  
+  */
 
   static int[] dRow = new int[] { 1, -1, 0, 0 }, dCol = new int[] { 0, 0, 1, -1 };
 
@@ -25,8 +46,7 @@ public class IslandPerimeter {
 
   @Test
   public void test() {
-    Assert.assertEquals(16,
-        islandPerimeter(new int[][] { { 0, 1, 0, 0 }, { 1, 1, 1, 0 }, { 0, 1, 0, 0 }, { 1, 1, 0, 0 } }));
-    Assert.assertEquals(8, islandPerimeter(new int[][] { { 1, 1 }, { 1, 1 } }));
+    assertEquals(16, islandPerimeter(new int[][] { { 0, 1, 0, 0 }, { 1, 1, 1, 0 }, { 0, 1, 0, 0 }, { 1, 1, 0, 0 } }));
+    assertEquals(8, islandPerimeter(new int[][] { { 1, 1 }, { 1, 1 } }));
   }
 }
