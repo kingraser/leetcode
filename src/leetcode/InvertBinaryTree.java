5b4da66660c501b1644b5c624c98885bd82788bc
kinghost <kingraser@gmail.com>
2016-12-27 17:00:33 +0800

    binary tree

--- src/leetcode/InvertBinaryTree.java
+++ src/leetcode/InvertBinaryTree.java
@@ -5,12 +5,22 @@
  */
 package leetcode;
 
+import static leetcode.common.TreeNode.tree;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import leetcode.common.TreeNode;
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月13日<p>
 //-------------------------------------------------------
 public class InvertBinaryTree {
+
+  @Test
+  public void test() {
+    assertEquals(tree("1,3,5,n,n,n,2,n,4,n,n"), invertTree(tree("1,2,4,n,n,n,3,n,5,n,n")));
+  }
 
   public TreeNode invertTree(TreeNode root) {
     if (root == null) return root;
