d4544fc7e808eaefc7be6415bf9932f204292e6f
kinghost <kingraser@gmail.com>
2016-12-27 14:59:11 +0800

    hash table

--- src/leetcode/ContainsDuplicateII.java
+++ src/leetcode/ContainsDuplicateII.java
@@ -5,8 +5,13 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.util.HashMap;
 import java.util.Map;
+
+import org.junit.Test;
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月12日<p>
@@ -18,6 +23,12 @@ public class ContainsDuplicateII {
   find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] 
   and the difference between i and j is at most k. 
   */
+
+  @Test
+  public void test() {
+    assertTrue(containsNearbyDuplicate(new int[] { 1, 2, 1 }, 2));
+    assertFalse(containsNearbyDuplicate(new int[] { 1, 2, 3, 1 }, 2));
+  }
 
   public boolean containsNearbyDuplicate(int[] nums, int k) {
     Map<Integer, Integer> map = new HashMap<>();
