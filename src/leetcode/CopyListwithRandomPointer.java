6f39377c6fb7bdd2150e5564b7eb69d479c6d94d
kinghost <kingraser@gmail.com>
2016-12-27 15:18:00 +0800

    linked list

--- src/leetcode/CopyListwithRandomPointer.java
+++ src/leetcode/CopyListwithRandomPointer.java
@@ -5,8 +5,12 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.HashMap;
 import java.util.Map;
+
+import org.junit.Test;
 
 import leetcode.common.RandomListNode;
 
@@ -21,6 +25,18 @@ public class CopyListwithRandomPointer {
   
   Return a deep copy of the list. 
   */
+
+  @Test
+  public void test() {
+    RandomListNode zero = new RandomListNode(0), one = new RandomListNode(1), two = new RandomListNode(2);
+    zero.next = one;
+    one.next = two;
+    zero.random = two;
+    one.random = zero;
+    two.random = one;
+
+    assertEquals(zero, copyRandomList(zero));
+  }
 
   public RandomListNode copyRandomList(RandomListNode head) {
     Map<RandomListNode, RandomListNode> map = new HashMap<>();
