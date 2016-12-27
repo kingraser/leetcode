d431909f07f5bdbbdcdf239a02d66ffb8cc69134
kinghost <kingraser@gmail.com>
2016-12-27 16:24:13 +0800

    stack

--- src/leetcode/ImplementQueueusingStacks.java
+++ src/leetcode/ImplementQueueusingStacks.java
@@ -5,34 +5,59 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Stack;
+
+import org.junit.Test;
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月13日<p>
 //-------------------------------------------------------
 public class ImplementQueueusingStacks {
 
-  private Stack<Integer> in = new Stack<Integer>(), out = new Stack<Integer>();
-
-  // Push element x to the back of queue.
-  public void push(int x) {
-    in.push(x);
+  @Test
+  public void test() {
+    Queue queue = new Queue();
+    assertTrue(queue.empty());
+    queue.push(1);
+    assertFalse(queue.empty());
+    assertEquals(queue.peek(), 1);
+    queue.push(2);
+    assertEquals(queue.peek(), 1);
+    queue.pop();
+    assertEquals(queue.peek(), 2);
+    queue.pop();
+    assertTrue(queue.empty());
+    queue.push(3);
+    assertEquals(queue.peek(), 3);
   }
 
-  // Removes the element from in front of queue.
-  public void pop() {
-    peek();
-    out.pop();
-  }
+  class Queue {
+    private Stack<Integer> in = new Stack<Integer>(), out = new Stack<Integer>();
 
-  // Get the front element.
-  public int peek() {
-    if (out.isEmpty()) for (; !in.isEmpty(); out.push(in.pop()));
-    return out.peek();
-  }
+    // Push element x to the back of queue.
+    public void push(int x) {
+      in.push(x);
+    }
 
-  // Return whether the queue is empty.
-  public boolean empty() {
-    return in.isEmpty() && out.isEmpty();
+    // Removes the element from in front of queue.
+    public void pop() {
+      peek();
+      out.pop();
+    }
+
+    // Get the front element.
+    public int peek() {
+      if (out.isEmpty()) for (; !in.isEmpty(); out.push(in.pop()));
+      return out.peek();
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+      return in.isEmpty() && out.isEmpty();
+    }
   }
 }
--- src/leetcode/ImplementStackusingQueues.java
+++ src/leetcode/ImplementStackusingQueues.java
@@ -5,33 +5,56 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayDeque;
 import java.util.Queue;
+
+import org.junit.Test;
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月13日<p>
 //-------------------------------------------------------
 public class ImplementStackusingQueues {
 
-  Queue<Integer> queue = new ArrayDeque<>();
-
-  int top;
-
-  public void push(int x) {
-    queue.add(top = x);
+  @Test
+  public void test() {
+    Stack stack = new Stack();
+    assertTrue(stack.empty());
+    stack.push(1);
+    assertFalse(stack.empty());
+    assertEquals(1, stack.top());
+    stack.push(2);
+    assertEquals(2, stack.top());
+    stack.pop();
+    assertEquals(1, stack.top());
+    stack.pop();
+    assertTrue(stack.empty());
   }
 
-  public void pop() {
-    for (int i = 0; i < queue.size() - 1; i++)
-      queue.add((top = queue.poll()));
-    queue.poll();
-  }
+  class Stack {
+    Queue<Integer> queue = new ArrayDeque<>();
 
-  public int top() {
-    return top;
-  }
+    int top;
 
-  public boolean empty() {
-    return queue.isEmpty();
+    public void push(int x) {
+      queue.add(top = x);
+    }
+
+    public void pop() {
+      for (int i = 0; i < queue.size() - 1; i++)
+        queue.add((top = queue.poll()));
+      queue.poll();
+    }
+
+    public int top() {
+      return top;
+    }
+
+    public boolean empty() {
+      return queue.isEmpty();
+    }
   }
 }
