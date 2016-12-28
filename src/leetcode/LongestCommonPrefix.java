ef2cacee869a7788fcc5cea638dcb786901584af
kinghost <kingraser@gmail.com>
2016-12-28 14:17:19 +0800

    string

--- src/leetcode/LongestCommonPrefix.java
+++ src/leetcode/LongestCommonPrefix.java
@@ -5,18 +5,27 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月9日<p>
 //-------------------------------------------------------
 public class LongestCommonPrefix {
 
-    public String longestCommonPrefix(String[] strs) {
-        if (null == strs || strs.length == 0) return "";
-        StringBuilder sb = new StringBuilder();
-        A: for (int i = 0; i < strs[0].length(); sb.append(strs[0].charAt(i++)))
-            for (int j = 1; j < strs.length; j++)
-                if (i == strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i)) break A;
-        return sb.toString();
-    }
+  @Test
+  public void test() {
+    assertEquals("ab", longestCommonPrefix(new String[] { "abc", "abcd", "ab", "abde" }));
+  }
+
+  public String longestCommonPrefix(String[] strs) {
+    if (null == strs || strs.length == 0) return "";
+    StringBuilder sb = new StringBuilder();
+    A: for (int i = 0; i < strs[0].length(); sb.append(strs[0].charAt(i++)))
+      for (int j = 1; j < strs.length; j++)
+        if (i == strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i)) break A;
+    return sb.toString();
+  }
 
 }
