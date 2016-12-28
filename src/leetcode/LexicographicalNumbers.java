2a54bd055517a678da480255c14d0d7f2294bd64
kinghost <kingraser@gmail.com>
2016-12-28 11:23:03 +0800

    dfs

---
+++ src/leetcode/LexicographicalNumbers.java
@@ -0,0 +1,46 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2015 Sogou.com. All Rights Reserved.
+ */
+package leetcode;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+//--------------------- Change Logs----------------------
+//@author wangwenlong Initial Created at 2016年8月23日;
+//-------------------------------------------------------
+public class LexicographicalNumbers {
+
+  /*
+  Given an integer n, return 1 - n in lexicographical order.
+  
+  For example, given 13, return: [1,10,11,12,13,2,3,4,5,6,7,8,9].
+  
+  Please optimize your algorithm to use less time and space. 
+  The input size may be as large as 5,000,000. 
+  */
+
+  @Test
+  public void test() {
+    assertEquals(Arrays.asList(1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9), lexicalOrder(13));
+  }
+
+  public List<Integer> lexicalOrder(int n) {
+    List<Integer> result = new ArrayList<>();
+    for (int i = 1; i < 10; lexicalOrder(result, n, i++));
+    return result;
+  }
+
+  private void lexicalOrder(List<Integer> result, int n, int last) {
+    if (last > n) return;
+    result.add(last);
+    for (int i = 0; i < 10; lexicalOrder(result, n, last * 10 + i++));
+  }
+}
