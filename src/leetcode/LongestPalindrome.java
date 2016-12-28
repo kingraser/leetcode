92f1cd646d562c9575c0f002c540e5ede2893d1f
kinghost <kingraser@gmail.com>
2016-12-28 14:25:28 +0800

    bit set

---
+++ src/leetcode/LongestPalindrome.java
@@ -0,0 +1,53 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2015 Sogou.com. All Rights Reserved.
+ */
+package leetcode;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.BitSet;
+
+import org.junit.Test;
+
+//--------------------- Change Logs----------------------
+//@author wangwenlong Initial Created at 2016年10月8日;
+//-------------------------------------------------------
+public class LongestPalindrome {
+
+  /*
+  Given a string which consists of lowercase or uppercase letters, find the length of the longest palindromes that can be built with those letters.
+  
+  This is case sensitive, for example "Aa" is not considered a palindrome here.
+  
+  Note:
+  Assume the length of given string will not exceed 1,010.
+  
+  Example:
+  
+  Input:
+  "abccccdd"
+  
+  Output:
+  7
+  
+  Explanation:
+  One longest palindrome that can be built is "dccaccd", whose length is 7. 
+  */
+
+  @Test
+  public void test() {
+    assertEquals(7, longestPalindrome("abccccdd"));
+  }
+
+  public int longestPalindrome(String s) {
+    BitSet bitSet = new BitSet(128);
+    int len = 0;
+    for (char c : s.toCharArray()) {
+      bitSet.flip(c);
+      if (!bitSet.get(c)) len += 2;
+    }
+    return len < s.length() ? ++len : len;
+  }
+}
