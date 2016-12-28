0700ce12b789c256c70b5722e472fa1b690c0b5b
kinghost <kingraser@gmail.com>
2016-12-28 14:41:39 +0800

    array

--- src/leetcode/MajorityElement.java
+++ src/leetcode/MajorityElement.java
@@ -5,25 +5,33 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月13日<p>
 //-------------------------------------------------------
 public class MajorityElement {
-    /*    
-    Given an array of size n, find the majority element. 
-    The majority element is the element that appears more than n/2 times.
-    */
 
-    public int majorityElement(int[] num) {
-        int a = num[0];
-        for (int i = 1, sum = 1; sum <= num.length >> 1 && i < num.length; i++)
-            if (num[i] != a) {
-                sum--;
-                if (sum == 0) {
-                    a = num[++i];
-                    sum++;
-                }
-            } else sum++;
-        return a;
-    }
+  /*    
+  Given an array of size n, find the majority element. 
+  The majority element is the element that appears more than n/2 times.
+  */
+
+  @Test
+  public void test() {
+    assertEquals(1, majorityElement(new int[] { 2, 3, 4, 1, 1, 1, 1 }));
+  }
+
+  public int majorityElement(int[] num) {
+    int major = num[0];
+    for (int i = 1, count = 1; i < num.length; i++)
+      if (major == num[i]) count++;
+      else if (count-- == 0) {
+        count = 1;
+        major = num[i];
+      }
+    return major;
+  }
 }
