325941f3b43292c1197f35f9101908505ee8fcc0
kinghost <kingraser@gmail.com>
2016-12-28 14:43:39 +0800

    binary tree

--- src/leetcode/LowestCommonAncestorofaBinarySearchTree.java
+++ src/leetcode/LowestCommonAncestorofaBinarySearchTree.java
@@ -5,7 +5,9 @@
  */
 package leetcode;
 
-import org.junit.Assert;
+import static leetcode.common.TreeNode.tree;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import leetcode.common.TreeNode;
@@ -15,29 +17,29 @@ import leetcode.common.TreeNode;
 //-------------------------------------------------------
 public class LowestCommonAncestorofaBinarySearchTree {
 
-    /*
-         6
-        / \
-       2   8
-      / \ / \
-     0  4 7  9
-       / \
-      3   5
-    For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. 
-    Another example is LCA of nodes 2 and 4 is 2
-    */
+  /*
+       6
+      / \
+     2   8
+    / \ / \
+   0  4 7  9
+     / \
+    3   5
+  For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. 
+  Another example is LCA of nodes 2 and 4 is 2
+  */
 
-    @Test
-    public void test() {
-        TreeNode four = TreeNode.generateTree("4,3,n,n,5,n,n"), two = new TreeNode(2, new TreeNode(0), four),
-                eight = TreeNode.generateTree("8,7,n,n,9,n,n"), six = new TreeNode(6, two, eight), root = six;
-        Assert.assertEquals(six, lowestCommonAncestor(root, two, eight));
-        Assert.assertEquals(two, lowestCommonAncestor(root, two, four));
-    }
+  @Test
+  public void test() {
+    TreeNode four = tree("4,3,n,n,5,n,n"), two = new TreeNode(2, new TreeNode(0), four), eight = tree("8,7,n,n,9,n,n"),
+        six = new TreeNode(6, two, eight), root = six;
+    assertEquals(six, lowestCommonAncestor(root, two, eight));
+    assertEquals(two, lowestCommonAncestor(root, two, four));
+  }
 
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
-        if (root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
-        return root;
-    }
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+    if (root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+    return root;
+  }
 }
