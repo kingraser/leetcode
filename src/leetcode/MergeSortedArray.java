6fe2eecc76fdd54dc8845bde762af4222bad0dad
kinghost <kingraser@gmail.com>
2017-01-04 16:37:06 +0800

    array

--- src/leetcode/MergeSortedArray.java
+++ src/leetcode/MergeSortedArray.java
@@ -5,6 +5,10 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月13日<p>
 //-------------------------------------------------------
@@ -19,9 +23,16 @@ public class MergeSortedArray {
   The number of elements initialized in nums1 and nums2 are m and n respectively.
   */
 
+  @Test
+  public void test() {
+    int[] A = new int[] { 1, 3, 5, 0, 0, 0 }, B = new int[] { 2, 4, 6 };
+    merge(A, 3, B, 3);
+    assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6 }, A);
+  }
+
   public void merge(int A[], int m, int B[], int n) {
-    int a = m - 1, b = n - 1, c = m + n - 1;
-    for (; a >= 0 && b >= 0; A[c--] = A[a] >= B[b] ? A[a--] : B[b--]);
-    for (; b >= 0; A[c--] = B[b--]);
+    int idx1 = m - 1, idx2 = n - 1, idx = m + n - 1;
+    for (; idx1 >= 0 && idx2 >= 0; A[idx--] = A[idx1] >= B[idx2] ? A[idx1--] : B[idx2--]);
+    for (; idx2 >= 0; A[idx--] = B[idx2--]);
   }
 }
