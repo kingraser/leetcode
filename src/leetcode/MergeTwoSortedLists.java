4d925b9dd1b1abb355b0f6e1d629ccf5ea41a244
kinghost <kingraser@gmail.com>
2017-01-04 16:44:56 +0800

    linked list

--- src/leetcode/MergeTwoSortedLists.java
+++ src/leetcode/MergeTwoSortedLists.java
@@ -5,12 +5,23 @@
  */
 package leetcode;
 
+import static leetcode.common.ListNode.list;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import leetcode.common.ListNode;
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月10日<p>
 //-------------------------------------------------------
 public class MergeTwoSortedLists {
+
+  @Test
+  public void test() {
+    ListNode l1 = list(1, 3, 5), l2 = list(2, 4, 6);
+    assertEquals(list(1, 2, 3, 4, 5, 6), mergeTwoLists(l1, l2));
+  }
 
   public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
     if (l1 == null) return l2;
