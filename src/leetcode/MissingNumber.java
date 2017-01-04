4550f6f2c4b4a36e426a2f7484d5544236779ef8
kinghost <kingraser@gmail.com>
2017-01-04 17:23:49 +0800

    binary search

--- src/leetcode/MissingNumber.java
+++ src/leetcode/MissingNumber.java
@@ -5,6 +5,8 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Arrays;
 
 import org.junit.Test;
@@ -15,29 +17,12 @@ import org.junit.Test;
 public class MissingNumber {
 
   /*
-          给定一数组0, 1, 2, ..., n，从中取一个数，找出这个数
+  Give an array 0, 1, 2, ..., n, remove a number, find it. 
   For example
   Given [0, 1, 3] return 2. 
-  
-          解法一：异或    
-          解法二：取和
-          解法三：二分
   */
 
   public int missingNumber(int[] nums) {
-    int check = 0;
-    for (int i = 0; i < nums.length; i++)
-      check ^= nums[i] ^ (i + 1);
-    return check;
-  }
-
-  public int missingNumberII(int[] nums) {
-    int sum = (nums.length * (nums.length + 1)) >> 1;
-    for (int i = 0; i < nums.length; sum -= nums[i++]);
-    return sum;
-  }
-
-  public int missingNumberIII(int[] nums) {
     Arrays.sort(nums);
     int left = 0, right = nums.length, mid;
     while (left < right)
@@ -49,7 +34,5 @@ public class MissingNumber {
   @Test
   public void test() {
     assertEquals(2, missingNumber(new int[] { 0, 1, 3 }));
-    assertEquals(2, missingNumberII(new int[] { 0, 1, 3 }));
-    assertEquals(2, missingNumberIII(new int[] { 0, 1, 3 }));
   }
 }
