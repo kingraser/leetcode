d10a797f18e5399800977e945997aec064d9ea19
kinghost <kingraser@gmail.com>
2017-01-04 15:19:19 +0800

    bucketing

--- src/leetcode/MaximumGap.java
+++ src/leetcode/MaximumGap.java
@@ -5,7 +5,12 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Arrays;
+import java.util.Objects;
+
+import org.junit.Test;
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年10月16日<p>
@@ -18,25 +23,26 @@ public class MaximumGap {
   Return 0 if the array contains less than 2 elements.
   You may assume all elements in the array are non-negative integers and fit in the 32-bit signed integer range.
   */
+
+  @Test
+  public void test() {
+    assertEquals(499, maximumGap(new int[] { 1, 10, 5, 2, 1000, 509 }));
+  }
+
   public int maximumGap(int[] nums) {
-    if (nums == null || nums.length < 2) return 0;
-    int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
-    for (int i = 0; i < nums.length; i++) {//get max and min
-      max = Math.max(max, nums[i]);
-      min = Math.min(min, nums[i]);
-    }
-    int bucketLen = (int) Math.ceil(((double) max - min) / (nums.length - 1)),
-        bucketCount = (int) ((max - min) / bucketLen + 1);
+    if (Objects.isNull(nums) || nums.length < 2) return 0;
+    int max = Arrays.stream(nums).max().getAsInt(), min = Arrays.stream(nums).min().getAsInt(),
+        bucketLen = (int) Math.ceil(((double) max - min) / (nums.length - 1)),
+        bucketCount = (max - min) / bucketLen + 1;
     int[] minBuckets = new int[bucketCount], maxBuckets = new int[bucketCount];
     Arrays.fill(minBuckets, Integer.MAX_VALUE);
     Arrays.fill(maxBuckets, Integer.MIN_VALUE);
-    for (int i = 0; i < nums.length; i++) {
-      int b = (int) ((nums[i] - min) / bucketLen);
-      minBuckets[b] = Math.min(minBuckets[b], nums[i]);
-      maxBuckets[b] = Math.max(maxBuckets[b], nums[i]);
+    for (int i = 0, idx; i < nums.length; i++) {
+      minBuckets[idx = (nums[i] - min) / bucketLen] = Math.min(minBuckets[idx], nums[i]);
+      maxBuckets[idx] = Math.max(maxBuckets[idx], nums[i]);
     }
-    int maxGap = 0, prev = maxBuckets[0];
-    for (int i = 1; i < bucketCount; i++) {
+    int maxGap = 0;
+    for (int i = 1, prev = maxBuckets[0]; i < bucketCount; i++) {
       if (minBuckets[i] == Integer.MAX_VALUE && maxBuckets[i] == Integer.MIN_VALUE) continue;
       maxGap = Math.max(maxGap, minBuckets[i] - prev);
       prev = maxBuckets[i];
