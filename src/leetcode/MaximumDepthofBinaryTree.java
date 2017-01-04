56c70edcbc97d406d2328bbc2fa94a172e17dc07
kinghost <kingraser@gmail.com>
2017-01-04 15:02:38 +0800

    binary tree

--- src/leetcode/MaximumDepthofBinaryTree.java
+++ src/leetcode/MaximumDepthofBinaryTree.java
@@ -5,12 +5,22 @@
  */
 package leetcode;
 
+import static leetcode.common.TreeNode.tree;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import leetcode.common.TreeNode;
 
 //--------------------- Change Logs----------------------
 // <p>@author wit Initial Created at 2015年9月13日<p>
 //-------------------------------------------------------
 public class MaximumDepthofBinaryTree {
+
+  @Test
+  public void test() {
+    assertEquals(4, maxDepth(tree("1,2,n,n,3,4,5,n,n,n,6,n,n")));
+  }
 
   public int maxDepth(TreeNode root) {
     return root == null ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
