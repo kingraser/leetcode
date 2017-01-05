851c57b766e1d0213e16aed9e364d62c4f0382c5
kinghost <kingraser@gmail.com>
2017-01-05 12:00:58 +0800

    dfs

--- src/leetcode/NQueens.java
+++ src/leetcode/NQueens.java
@@ -5,6 +5,8 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,76 +19,38 @@ import org.junit.Test;
 public class NQueens {
 
   public List<List<String>> solveNQueens(int n) {
-    List<List<String>> result = new ArrayList<>(n);
-    List<Integer> queenList = new ArrayList<>(n);
-    for (int i = 0; i != -1;) {//-1表示尝试过1-9，结束
-      queenList.add(i);//放到盘上(校验已经完成)
-      if (queenList.size() == n) {//找到一个解
-        result.add(parse(n, queenList));//解记录
-        queenList.remove(n - 1);//去掉刚刚加入皇后,开始回溯
-        i = back(n, queenList);//如果可以回溯则回溯
-      } else i = getNext(0, n, queenList);//找到下一行的皇后列位置
+    List<List<String>> result = new ArrayList<>();
+    dfs(result, new ArrayList<>(), n);
+    return result;
+  }
+
+  private void dfs(List<List<String>> result, List<Integer> queens, int size) {
+    if (queens.size() == size) {
+      result.add(parse(size, queens));
+      return;
+    }
+    for (int col = 0; col < size; col++)
+      if (isOk(col, queens)) {
+        queens.add(col);
+        dfs(result, queens, size);
+        queens.remove(queens.size() - 1);
+      }
+  }
+
+  private List<String> parse(int size, List<Integer> queens) {
+    List<String> result = new ArrayList<>(queens.size());
+    for (Integer queen : queens) {
+      char[] array = new char[size];
+      Arrays.fill(array, '.');
+      array[queen] = 'Q';
+      result.add(new String(array));
     }
     return result;
   }
 
-  /**
-   * @param start
-   *          起始位置
-   * @param n
-   *          棋盘大小
-   * @param queenList
-   *          当前部分结果集
-   * @return 下一皇后位置
-   */
-  private int getNext(int start, int n, List<Integer> queenList) {
-    for (int i = start; i < n; i++)
-      if (isOk(i, queenList)) return i;//当前位置与前面皇后都不矛盾,返回当前位置
-    return back(n, queenList);//矛盾，回溯
-  }
-
-  /**
-   * @param n
-   *          棋盘大小
-   * @param queenList
-   *          当前部分结果集
-   * @return 回溯之后需要尝试的列数
-   */
-  private int back(int n, List<Integer> queenList) {
-    int next = -1;
-    while (next == -1 && !queenList.isEmpty())
-      next = getNext(1 + queenList.remove(queenList.size() - 1), n, queenList);
-    return next;//返回
-  }
-
-  /**
-   * @param n
-   *          棋盘大小
-   * @param queenList
-   *          当前部分结果集
-   * @return OJ规定的格式
-   */
-  private List<String> parse(int n, List<Integer> queenList) {
-    List<String> result = new ArrayList<>();
-    queenList.forEach(i -> {
-      char[] array = new char[n];
-      Arrays.fill(array, '.');
-      array[i] = 'Q';
-      result.add(new String(array));
-    });
-    return result;
-  }
-
-  /**
-   * @param col
-   *          列
-   * @param queenList
-   *          之前的结果集（行数即结果集size）
-   * @return 该列数是否矛盾
-   */
-  public boolean isOk(int col, List<Integer> queenList) {
-    for (int i = 0; i < queenList.size(); i++)
-      if (queenList.get(i) == col || Math.abs(i - queenList.size()) == Math.abs(queenList.get(i) - col)) return false;//列相同且在同一斜线上,矛盾
+  public boolean isOk(int col, List<Integer> queens) {
+    for (int i = 0, size = queens.size(); i < size; i++)
+      if (queens.get(i) == col || Math.abs(i - size) == Math.abs(queens.get(i) - col)) return false;
     return true;
   }
 
--- src/leetcode/NQueensII.java
+++ src/leetcode/NQueensII.java
@@ -5,6 +5,8 @@
  */
 package leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,58 +18,27 @@ import org.junit.Test;
 public class NQueensII {
 
   public int totalNQueens(int n) {
-    int sum = 0;
-    List<Integer> queenList = new ArrayList<>();
-    for (int i = 0; i != -1;) {//-1表示尝试过1-9，结束 
-      queenList.add(i);
-      if (queenList.size() == n) {//找到一个解
-        sum++;//解记录
-        queenList.remove(n - 1);//去掉刚刚加入皇后,开始回溯
-        i = back(n, queenList);//如果可以回溯则回溯
-      } else i = getNext(0, n, queenList);//找到下一行的皇后列位置
+    int[] sum = new int[1];
+    dfs(sum, new ArrayList<>(), n);
+    return sum[0];
+  }
+
+  private void dfs(int[] sum, List<Integer> queens, int size) {
+    if (queens.size() == size) {
+      sum[0]++;
+      return;
     }
-    return sum;
+    for (int col = 0; col < size; col++)
+      if (isOk(col, queens)) {
+        queens.add(col);
+        dfs(sum, queens, size);
+        queens.remove(queens.size() - 1);
+      }
   }
 
-  /**
-   * @param start
-   *          起始位置
-   * @param n
-   *          棋盘大小
-   * @param queenList
-   *          当前部分结果集
-   * @return 下一皇后位置
-   */
-  private int getNext(int start, int n, List<Integer> queenList) {
-    for (int i = start; i < n; i++)
-      if (isOk(i, queenList)) return i;//当前位置与前面皇后都不矛盾,返回当前位置
-    return back(n, queenList);//矛盾，回溯
-  }
-
-  /**
-   * @param n
-   *          棋盘大小
-   * @param queenList
-   *          当前部分结果集
-   * @return 回溯之后需要尝试的列数
-   */
-  private int back(int n, List<Integer> queenList) {
-    int next = -1;
-    while (next == -1 && queenList.size() > 0)
-      next = getNext(1 + queenList.remove(queenList.size() - 1), n, queenList);
-    return next;//返回
-  }
-
-  /**
-   * @param col
-   *          列
-   * @param queenList
-   *          之前的结果集（行数即结果集size）
-   * @return 该列数是否矛盾
-   */
-  public boolean isOk(int col, List<Integer> queenList) {
-    for (int i = 0; i < queenList.size(); i++)
-      if (queenList.get(i) == col || Math.abs(i - queenList.size()) == Math.abs(queenList.get(i) - col)) return false;//列相同且在同一斜线上,矛盾
+  public boolean isOk(int col, List<Integer> queens) {
+    for (int i = 0, size = queens.size(); i < size; i++)
+      if (queens.get(i) == col || Math.abs(i - size) == Math.abs(queens.get(i) - col)) return false;
     return true;
   }
 
@@ -75,5 +46,4 @@ public class NQueensII {
   public void test() {
     assertEquals(92, totalNQueens(8));
   }
-
 }
