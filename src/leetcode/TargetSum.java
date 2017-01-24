6f3cb7c71d6cba148f987efe72dea7929bc7d330
kinghost <kingraser@gmail.com>
2017-01-24 19:29:21 +0800

    update

---
+++ src/leetcode/TargetSum.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class TargetSum {
+
+  /*
+  You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -. For each integer, you should choose one from + and - as its new symbol.
+  Find out how many ways to assign symbols to make sum of integers equal to target S.
+  
+  Example 1:
+  
+  Input: nums is [1, 1, 1, 1, 1], S is 3. 
+  Output: 5
+  Explanation: 
+  
+  -1+1+1+1+1 = 3
+  +1-1+1+1+1 = 3
+  +1+1-1+1+1 = 3
+  +1+1+1-1+1 = 3
+  +1+1+1+1-1 = 3
+  
+  There are 5 ways to assign symbols to make the sum of nums be target 3.
+  
+  Note:
+    The length of the given array is positive and will not exceed 20.
+    The sum of elements in the given array will not exceed 1000.
+    Your output answer is guaranteed to be fitted in a 32-bit integer.
+  */
+
+  @Test
+  public void test() {
+    assertEquals(5, findTargetSumWays(new int[] { 1, 1, 1, 1, 1 }, 3));
+  }
+
+  private int dfs(int[] nums, int sum, int i) {
+    if (i == nums.length) return sum == 0 ? 1 : 0;
+    return dfs(nums, sum - nums[i], i + 1) + dfs(nums, sum + nums[i], i + 1);
+  }
+
+  public int findTargetSumWays(int[] nums, int S) {
+    return dfs(nums, S, 0);
+  }
+
+}
