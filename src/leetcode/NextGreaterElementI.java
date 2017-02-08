840b5a163514f1a8cfedfbe3d0e3a73aa7d752d1
kinghost <kingraser@gmail.com>
2017-02-08 10:58:08 +0800

    stack

---
+++ src/leetcode/NextGreaterElementI.java
@@ -0,0 +1,57 @@
+package leetcode;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+import org.junit.Test;
+
+public class NextGreaterElementI {
+
+  /*
+  You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+  
+  The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
+  
+  Example 1:
+  
+  Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
+  Output: [-1,3,-1]
+  Explanation:
+    For number 4 in the first array, you cannot find the next greater number for it in the second array, so output -1.
+    For number 1 in the first array, the next greater number for it in the second array is 3.
+    For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
+  
+  Example 2:
+  
+  Input: nums1 = [2,4], nums2 = [1,2,3,4].
+  Output: [3,-1]
+  Explanation:
+    For number 2 in the first array, the next greater number for it in the second array is 3.
+    For number 4 in the first array, there is no next greater number for it in the second array, so output -1.
+  
+  Note:  
+    All elements in nums1 and nums2 are unique.
+    The length of both nums1 and nums2 would not exceed 1000.
+  */
+
+  @Test
+  public void test() {
+    assertArrayEquals(new int[] { -1, 3, -1 }, nextGreaterElement(new int[] { 4, 1, 2 }, new int[] { 1, 3, 4, 2 }));
+    assertArrayEquals(new int[] { 3, -1 }, nextGreaterElement(new int[] { 2, 4 }, new int[] { 1, 2, 3, 4 }));
+  }
+
+  public int[] nextGreaterElement(int[] findNums, int[] nums) {
+    Map<Integer, Integer> map = new HashMap<>();
+    Stack<Integer> stack = new Stack<>();
+    for (int num : nums) {
+      while (!stack.isEmpty() && stack.peek() < num)
+        map.put(stack.pop(), num);
+      stack.push(num);
+    }
+    return Arrays.stream(findNums).map(i -> map.getOrDefault(i, -1)).toArray();
+  }
+}
---
+++ src/leetcode/NextGreaterElementII.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+import org.junit.Test;
+
+public class NextGreaterElementII {
+
+  /*
+  Given a circular array (the next element of the last element is the first element of the array), print the Next Greater Number for every element. The Next Greater Number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, output -1 for this number.
+  
+  Example 1: 
+  Input: [1,2,1]
+  Output: [2,-1,2]
+  Explanation: The first 1's next greater number is 2; 
+  The number 2 can't find next greater number; 
+  The second 1's next greater number needs to search circularly, which is also 2.
+  
+  Note: The length of given array won't exceed 10000. 
+  */
+
+  @Test
+  public void test() {
+    assertArrayEquals(new int[] { 120, 11, 120, 120, 123, 123, -1, 100, 100, 100 },
+        nextGreaterElements(new int[] { 100, 1, 11, 1, 120, 111, 123, 1, -1, -100 }));
+    assertArrayEquals(new int[] { 2, 3, -1, 3, 2 }, nextGreaterElements(new int[] { 1, 2, 3, 2, 1 }));
+    assertArrayEquals(new int[] { 2, -1, 2 }, nextGreaterElements(new int[] { 1, 2, 1 }));
+  }
+
+  public int[] nextGreaterElements(int[] nums) {
+    int[] result = new int[nums.length];
+    Arrays.fill(result, -1);
+    Stack<Integer> stack = new Stack<>();
+    for (int i = 0, len = nums.length << 1, num; i < len; i++) {
+      while (!stack.isEmpty() && nums[stack.peek()] < (num = nums[i % nums.length]))
+        result[stack.pop()] = num;
+      if (i < nums.length) stack.push(i);
+    }
+    return result;
+  }
+}
