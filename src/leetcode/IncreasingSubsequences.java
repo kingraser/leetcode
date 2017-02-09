c7e74413bdc8b79d25311f38f4ebf1536a77f84a
kinghost <kingraser@gmail.com>
2017-02-09 10:42:58 +0800

    dfs

--- src/leetcode/IncreasingSubsequences.java
+++ src/leetcode/IncreasingSubsequences.java
@@ -1,7 +1,18 @@
 package leetcode;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.Test;
 
 public class IncreasingSubsequences {
 
@@ -20,8 +31,28 @@ public class IncreasingSubsequences {
     The given array may contain duplicates, and two equal integers should also be considered as a special case of increasing sequence.
   */
 
+  @Test
+  public void test() {
+    assertEquals(Stream
+        .of(Arrays.asList(4, 6), Arrays.asList(4, 6, 7), Arrays.asList(4, 6, 7, 7), Arrays.asList(4, 7),
+            Arrays.asList(4, 7, 7), Arrays.asList(6, 7), Arrays.asList(6, 7, 7), Arrays.asList(7, 7))
+        .collect(Collectors.toSet()), new HashSet<>(findSubsequences(new int[] { 4, 6, 7, 7 })));
+  }
+
   public List<List<Integer>> findSubsequences(int[] nums) {
     List<List<Integer>> result = new ArrayList<>();
+    dfs(result, new ArrayDeque<>(), 0, nums);
     return result;
+  }
+
+  public void dfs(List<List<Integer>> result, Deque<Integer> deque, int start, int[] nums) {
+    if (deque.size() > 1) result.add(new ArrayList<>(deque));
+    Set<Integer> set = new HashSet<>();
+    for (int i = start; i < nums.length; i++)
+      if ((deque.isEmpty() || deque.peekLast() <= nums[i]) && set.add(nums[i])) {
+        deque.addLast(nums[i]);
+        dfs(result, deque, i + 1, nums);
+        deque.removeLast();
+      }
   }
 }
