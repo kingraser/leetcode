0c5c48167397e40ef20ab4d739c5ce9c9e09760b
kinghost <kingraser@gmail.com>
2017-03-06 18:18:54 +0800

    2 pointers

--- src/leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ src/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,17 +1,9 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月6日<p>
-//-------------------------------------------------------
 public class LongestSubstringWithoutRepeatingCharacters {
 
   /*
@@ -22,18 +14,18 @@ public class LongestSubstringWithoutRepeatingCharacters {
   Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
   */
 
-  public int lengthOfLongestSubstring(String s) {
-    int result = 0, map[] = new int[128];
-    for (int i = 0, left = 0, c, len = 0; i < s.length(); map[c] = i, result = Math.max(result, len))
-      len = map[c = s.charAt(i++)] == 0 ? ++len : i - (left = Math.max(left, map[c]));
-    return result;
-  }
-
   @Test
   public void test() {
     assertEquals(2, lengthOfLongestSubstring("abba"));
     assertEquals(3, lengthOfLongestSubstring("abcabcbb"));
     assertEquals(1, lengthOfLongestSubstring("bbbbb"));
+  }
+
+  public int lengthOfLongestSubstring(String s) {
+    int result = 0, map[] = new int[128];
+    for (int idx = 0, left = 0, c, len = 0; idx < s.length(); map[c] = idx, result = Math.max(result, len))
+      len = map[c = s.charAt(idx++)] == 0 ? ++len : idx - (left = Math.max(left, map[c]));
+    return result;
   }
 
 }
