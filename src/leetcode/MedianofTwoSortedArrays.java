da71e09ce01acf142053cb440b17c31eb93bf332
kinghost <kingraser@gmail.com>
2017-03-06 18:31:44 +0800

    binary search

--- src/leetcode/MedianofTwoSortedArrays.java
+++ src/leetcode/MedianofTwoSortedArrays.java
@@ -1,17 +1,9 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月6日<p>
-//-------------------------------------------------------
 public class MedianofTwoSortedArrays {
 
   /*
@@ -29,23 +21,25 @@ public class MedianofTwoSortedArrays {
   The median is (2 + 3)/2 = 2.5
   */
 
+  @Test
+  public void test() {
+    assertEquals(2d, findMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2 }), 0);
+    assertEquals(2.5d, findMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 }), 0);
+
+    assertEquals(2d, findMedianSortedArrays(new int[] { 1, 2 }, new int[] { 1, 2, 3 }), 0);
+    assertEquals(1d, findMedianSortedArrays(new int[] { 1 }, new int[] { 1 }), 0);
+  }
+
   public double findMedianSortedArrays(int[] A, int[] B) {
-    if (A.length > B.length) return findMedianSortedArrays(B, A);//make sure A is shorter than B
+    if (A.length > B.length) return findMedianSortedArrays(B, A); // make sure A is shorter than B
     int lA = A.length, lB = B.length, half = (lA + lB - 1) >> 1, left = 0, right = Math.min(half, lA);
     for (int mid; left < right;)
       if (A[mid = (left + right) >> 1] < B[half - mid]) left = mid + 1;
       else right = mid;
     double a = left == 0 ? B[half] : Math.max(A[left - 1], B[half - left]);
-    if (((lA + lB) & 1) == 1) return a;//odd
+    if (((lA + lB) & 1) == 1) return a; // odd
     double b = left == lA ? B[half - lA + 1] : half - left + 1 < lB ? Math.min(A[left], B[half - left + 1]) : A[left];
     return (a + b) / 2;
   }
 
-  @Test
-  public void test() {
-    assertEquals(2d, findMedianSortedArrays(new int[] { 1, 2 }, new int[] { 1, 2, 3 }), 0);
-    assertEquals(1d, findMedianSortedArrays(new int[] { 1 }, new int[] { 1 }), 0);
-    assertEquals(2d, findMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2 }), 0);
-    assertEquals(2.5d, findMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 }), 0);
-  }
 }
