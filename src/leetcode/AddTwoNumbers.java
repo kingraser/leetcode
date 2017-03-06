70f39deec844762ec09d80dadb13ce7fc9786205
kinghost <kingraser@gmail.com>
2017-03-06 18:11:05 +0800

    linked list

--- src/leetcode/AddTwoNumbers.java
+++ src/leetcode/AddTwoNumbers.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static leetcode.common.ListNode.list;
@@ -14,9 +9,6 @@ import org.junit.Test;
 
 import leetcode.common.ListNode;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月6日<p>
-//-------------------------------------------------------
 public class AddTwoNumbers {
 
   /*
@@ -28,22 +20,22 @@ public class AddTwoNumbers {
   Output: 7 -> 0 -> 8
   */
 
+  @Test
+  public void test() {
+    assertEquals(list(7, 0, 8), addTwoNumbers(list(2, 4, 3), list(5, 6, 4)));
+  }
+
   public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
     return addTwoNumbers(l1, l2, 0);
   }
 
   public static ListNode addTwoNumbers(ListNode l1, ListNode l2, int c) {
     ListNode node = Objects.isNull(l1) ? Objects.isNull(l2) ? c == 0 ? null : new ListNode(c) : l2 : l1;
-    if (Objects.isNull(node)) return null;
+    if (Objects.isNull(node)) return node;
     int val = (Objects.isNull(l1) ? 0 : l1.val) + (Objects.isNull(l2) ? 0 : l2.val) + c;
     node.val = val % 10;
     node.next = addTwoNumbers(Objects.isNull(l1) ? null : l1.next, Objects.isNull(l2) ? null : l2.next, val / 10);
     return node;
-  }
-
-  @Test
-  public void test() {
-    assertEquals(list(7, 0, 8), addTwoNumbers(list(2, 4, 3), list(5, 6, 4)));
   }
 
 }
