2d339fb9f0293f7fe601ad445caf9db88ca11f5a
kinghost <kingraser@gmail.com>
2017-03-07 15:18:32 +0800

    stack

--- src/leetcode/LongestValidParentheses.java
+++ src/leetcode/LongestValidParentheses.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -11,24 +6,27 @@ import java.util.Stack;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月12日<p>
-//-------------------------------------------------------
 public class LongestValidParentheses {
 
   /*
-  Given a string containing just the characters ’(’ and ’)’, 
+  Given a string containing just the characters '(' and ')', 
   find the length of the longest valid (well-formed) parentheses substring.
-  For ”(()”, the longest valid parentheses substring is ”()”, which has length = 2.
-  Another example is ”)()())”, where the longest valid parentheses substring is ”()()”, 
+  For "(()", the longest valid parentheses substring is "()", which has length = 2.
+  Another example is ")()())", where the longest valid parentheses substring is "()()", 
   which has length = 4. 
   1stack O(n) time O(n) space
   2dp O(n) time O(n) space One pass
   3 O(n) time O(1) space Two pass
   */
 
+  @Test
+  public void test() {
+    assertEquals(2, longestValidParentheses("(()"));
+    assertEquals(4, longestValidParentheses(")()())"));
+  }
+
   public int longestValidParentheses(String s) {
-    Stack<Integer> stack = new Stack<Integer>();//only add left
+    Stack<Integer> stack = new Stack<Integer>(); // only add left
     int lastleft = 0, longest = 0;
     for (int i = 0; i < s.length(); i++)
       if (s.charAt(i) == '(') stack.push(i);
@@ -40,40 +38,4 @@ public class LongestValidParentheses {
     return longest;
   }
 
-  public int longestValidParenthesesII(String s) {
-    int[] f = new int[s.length()];
-    int result = 0;
-    for (int i = s.length() - 2; i >= 0; --i) {
-      int match = i + f[i + 1] + 1;
-      if (s.charAt(i) == '(' && match < s.length() && s.charAt(match) == ')') {// case: "((...))"
-        f[i] = f[i + 1] + 2;
-        if (match + 1 < s.length()) f[i] += f[match + 1];// if a valid sequence exist afterwards "((...))()"
-      }
-      result = Math.max(result, f[i]);
-    }
-    return result;
-  }
-
-  public int longestValidParenthesesIII(String s) {
-    int answer = 0, depth = 0, start = -1, i = 0;
-    for (; i < s.length(); i++)
-      if (s.charAt(i) == '(') depth++;
-      else if (--depth < 0) {
-        start = i;
-        depth = 0;
-      } else if (depth == 0) answer = Math.max(answer, i - start);
-    for (depth = 0, start = s.length(), i = s.length() - 1; i >= 0; --i)
-      if (s.charAt(i) == ')') depth++;
-      else if (--depth < 0) {
-        start = i;
-        depth = 0;
-      } else if (depth == 0) answer = Math.max(answer, start - i);
-    return answer;
-  }
-
-  @Test
-  public void test() {
-    assertEquals(2, longestValidParenthesesIII("(()"));
-    assertEquals(4, longestValidParenthesesIII(")()())"));
-  }
 }
