f3d49c362ab55957885da4bcfacaa252d0c79385
kinghost <kingraser@gmail.com>
2017-03-07 16:05:23 +0800

    binary search

--- src/leetcode/SearchInsertPosition.java
+++ src/leetcode/SearchInsertPosition.java
@@ -1,33 +1,24 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月15日<p>
-//-------------------------------------------------------
 public class SearchInsertPosition {
 
   /*
+  Given a sorted array and a target value, return the index if the target is found. 
+  If not, return the index where it would be if it were inserted in order.  
+  You may assume no duplicates in the array.
+  
+  Here are few examples.
   [1,3,5,6], 5 → 2
   [1,3,5,6], 2 → 1
   [1,3,5,6], 7 → 4
   [1,3,5,6], 0 → 0 
   */
-
-  public int searchInsert(int[] A, int target) {
-    int l = 0;
-    for (int r = A.length, mid = l + (r - l) / 2; l != r; mid = l + (r - l) / 2)
-      if (target > A[mid]) l = ++mid;
-      else r = mid;
-    return l;
-  }
 
   @Test
   public void test() {
@@ -36,6 +27,24 @@ public class SearchInsertPosition {
     assertEquals(1, searchInsert(A, 2));
     assertEquals(4, searchInsert(A, 7));
     assertEquals(0, searchInsert(A, 0));
+    
+    assertEquals(2, searchInsertZero(A, 5));
+    assertEquals(1, searchInsertZero(A, 2));
+    assertEquals(4, searchInsertZero(A, 7));
+    assertEquals(0, searchInsertZero(A, 0));
+  }
+
+  public int searchInsertZero(int[] A, int target) {
+    int idx = Arrays.binarySearch(A, target);
+    return idx >= 0 ? idx : ~idx;
+  }
+
+  public int searchInsert(int[] A, int target) {
+    int left = 0;
+    for (int right = A.length, mid; left < right;)
+      if (target > A[mid = (left + right) >> 1]) left = mid + 1;
+      else right = mid;
+    return left;
   }
 
 }
--- src/leetcode/SearchforaRange.java
+++ src/leetcode/SearchforaRange.java
@@ -1,18 +1,11 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月11日<p>
-//-------------------------------------------------------
 public class SearchforaRange {
+
   /*
   Given a sorted array of integers, find the starting and ending position of a given target value.    
   Your algorithm's runtime complexity must be in the order of O(log n).    
@@ -22,6 +15,12 @@ public class SearchforaRange {
   return [3, 4]. 
   */
 
+  @Test
+  public void test() {
+    int[] a = { 5, 7, 7, 8, 8, 10 };
+    assertArrayEquals(new int[] { 3, 4 }, searchRange(a, 8));;
+  }
+
   public int[] searchRange(int[] nums, int target) {
     int start = firstGreaterEqual(nums, target);
     if (start == nums.length || nums[start] != target) return new int[] { -1, -1 };
@@ -29,17 +28,11 @@ public class SearchforaRange {
   }
 
   public int firstGreaterEqual(int[] A, int target) {
-    int l = 0, r = A.length, mid = l + ((r - l) >> 1);
-    for (; l < r; mid = l + ((r - l) >> 1))
-      if (A[mid] < target) l = mid + 1;
-      else r = mid;
-    return l;
-  }
-
-  @Test
-  public void test() {
-    int[] a = { 5, 7, 7, 8, 8, 10 };
-    assertArrayEquals(new int[] { 3, 4 }, searchRange(a, 8));;
+    int left = 0, right = A.length, mid;
+    while (left < right)
+      if (A[mid = (left + right) >> 1] < target) left = mid + 1;
+      else right = mid;
+    return left;
   }
 
 }
