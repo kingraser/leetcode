ad2fb802053d4ed3271ead4ca17ee67b510ced91
kinghost <kingraser@gmail.com>
2017-03-07 18:45:35 +0800

    array

--- src/leetcode/RotateArray.java
+++ src/leetcode/RotateArray.java
@@ -1,22 +1,14 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
+import static leetcode.util.ArrayUtil.reverse;
 import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月14日<p>
-//-------------------------------------------------------
 public class RotateArray {
 
   /*    
-  Rotate an array of n elements to the right by k steps.
-  
+  Rotate an array of n elements to the right by k steps.  
   For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4]. 
   */
 
@@ -27,21 +19,11 @@ public class RotateArray {
     reverse(nums, 0, nums.length - 1);
   }
 
-  private void reverse(int[] nums, int i, int j) {
-    for (; i < j; swap(nums, i++, j--));
-  }
-
-  private void swap(int[] nums, int i, int j) {
-    int swap = nums[i];
-    nums[i] = nums[j];
-    nums[j] = swap;
-  }
-
   @Test
   public void test() {
-    int[] expecteds = new int[] { 5, 6, 7, 1, 2, 3, 4 }, actuals = new int[] { 1, 2, 3, 4, 5, 6, 7 };
-    rotate(actuals, 3);
-    assertArrayEquals(expecteds, actuals);
+    int[] input = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+    rotate(input, 3);
+    assertArrayEquals(new int[] { 5, 6, 7, 1, 2, 3, 4 }, input);
   }
 
 }
--- src/leetcode/RotateImage.java
+++ src/leetcode/RotateImage.java
@@ -1,18 +1,12 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月14日<p>
-//-------------------------------------------------------
 public class RotateImage {
+
+  //Rotate the image by 90 degrees (clockwise).
 
   @Test
   public void test() {
@@ -21,9 +15,8 @@ public class RotateImage {
     assertArrayEquals(new int[][] { { 3, 1 }, { 4, 2 } }, input);
   }
 
-  //Rotate the image by 90 degrees (clockwise).
   public void rotate(int[][] matrix) {
-    for (int i = 0, n = matrix.length, limit = (n - 1) / 2; i <= limit; i++) {
+    for (int i = 0, n = matrix.length, limit = (n - 1) / 2; i <= limit; i++)
       for (int j = i; j < n - 1 - i; j++) {
         int temp = matrix[i][j];
         matrix[i][j] = matrix[n - 1 - j][i];
@@ -31,7 +24,5 @@ public class RotateImage {
         matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
         matrix[j][n - 1 - i] = temp;
       }
-    }
   }
-
 }
