cc140f694e1323e811441541e1894c0982ce65b7
kinghost <kingraser@gmail.com>
2017-03-07 14:51:44 +0800

    string hash table

--- src/leetcode/SubstringwithConcatenationofAllWords.java
+++ src/leetcode/SubstringwithConcatenationofAllWords.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -12,12 +7,10 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月12日<p>
-//-------------------------------------------------------
 public class SubstringwithConcatenationofAllWords {
 
   /*
@@ -30,26 +23,26 @@ public class SubstringwithConcatenationofAllWords {
   You should return the indices: [0,9].(order does not matter).
   */
 
-  public List<Integer> findSubstring(String s, String[] words) {
-    List<Integer> result = new ArrayList<>();
-    if (words == null || words.length == 0 || words[0].length() * words.length > s.length()) return result;
-    Map<String, Integer> dictionary = new HashMap<>();
-    Arrays.stream(words).forEach(str -> dictionary.compute(str, (k, v) -> v == null ? 1 : v + 1));
-    A: for (int i = 0, size = words[0].length(), end = s.length() - size * words.length; i <= end; i++) {
-      Map<String, Integer> current = new HashMap<>(dictionary);
-      for (Integer j = 0, v; j < words.length; j++) {
-        String word = s.substring(i + j * size, i + (j + 1) * size);
-        if ((v = current.get(word)) == null) continue A;
-        else if (v == 1) current.remove(word);
-        else current.put(word, v - 1);
-      }
-      if (current.isEmpty()) result.add(i);
-    }
-    return result;
-  }
-
   @Test
   public void test() {
     assertEquals(Arrays.asList(0, 9), findSubstring("barfoothefoobarman", new String[] { "foo", "bar" }));
   }
+
+  public List<Integer> findSubstring(String s, String[] words) {
+    List<Integer> result = new ArrayList<>();
+    Map<String, Integer> dictionary = new HashMap<>();
+    Arrays.stream(words).forEach(str -> dictionary.compute(str, (k, v) -> v == null ? 1 : v + 1));
+    for (int i = 0, size = words[0].length(), length = size * words.length; i <= s.length() - length; i++)
+      if (isConcatenationofAllWords(s.substring(i, i + length), new HashMap<>(dictionary), size)) result.add(i);
+    return result;
+  }
+
+  public boolean isConcatenationofAllWords(String s, Map<String, Integer> map, int wordLength) {
+    String word;
+    for (int idx = 0; idx < s.length(); idx += wordLength, map.remove(word, 0))
+      if (Objects.isNull(map.computeIfPresent(word = s.substring(idx, idx + wordLength), (k, v) -> v - 1)))
+        return false;
+    return map.isEmpty();
+  }
+
 }
