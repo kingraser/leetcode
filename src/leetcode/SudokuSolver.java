b4aaf345b8ebdfc16cd5a59d8d8d3faf800f9ad5
kinghost <kingraser@gmail.com>
2017-03-07 16:25:50 +0800

    backtracking

--- src/leetcode/SudokuSolver.java
+++ src/leetcode/SudokuSolver.java
@@ -1,55 +1,48 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertArrayEquals;
 
 import java.util.Stack;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月11日<p>
-//-------------------------------------------------------
 public class SudokuSolver {
+
+  /*
+  Write a program to solve a Sudoku puzzle by filling the empty cells.  
+  Empty cells are indicated by the character '.'.  
+  You may assume that there will be only one unique solution. 
+  */
+
+  @Test
+  public void test() {
+    char[][] board = Stream.of("53..7....", "6..195...", ".98....6.", "8...6...3", "4..8.3..1", "7...2...6",
+        ".6....28.", "...419..5", "....8..79").map(s -> s.toCharArray()).toArray(l -> new char[l][]);
+    solveSudoku(board);
+    assertArrayEquals(Stream.of("534678912", "672195348", "198342567", "859761423", "426853791", "713924856",
+        "961537284", "287419635", "345286179").map(s -> s.toCharArray()).toArray(l -> new char[l][]), board);
+  }
 
   static int row, col, top;
 
   public void solveSudoku(char[][] board) {
-    Stack<Integer> log = new Stack<>();
+    Stack<Integer> records = new Stack<>();
     for (row = 0; row < 9; row++)
       for (col = 0; col < 9; col++)
-        if (board[row][col] == '.') put(board, '1', log);
+        if (board[row][col] == '.') put(board, '1', records);
   }
 
-  private void put(char[][] A, char start, Stack<Integer> log) {
+  private void put(char[][] A, char start, Stack<Integer> records) {
     A[row][col] = '.';
     A: for (char a = start; a <= '9'; a++) {
       for (int i = 0; i < 9; i++)
-        if (A[row][i] == a || A[i][col] == a || A[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == a) continue A;//有重复
-      A[row][col] = a;//放置
-      log.push(row * 10 + col);//历史记录
+        if (A[row][i] == a || A[i][col] == a || A[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == a) continue A; // conflict
+      A[row][col] = a; // place
+      records.push(row * 10 + col); // record
       return;
     }
-    put(A, ++A[row = (top = log.pop()) / 10][col = (top % 10)], log);
+    put(A, ++A[row = (top = records.pop()) / 10][col = (top % 10)], records);
   }
 
-  @Test
-  public void test() {
-    char[][] board = new char[][] { { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
-        { '6', '.', '.', '1', '9', '5', '.', '.', '.' }, { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
-        { '8', '.', '.', '.', '6', '.', '.', '.', '3' }, { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
-        { '7', '.', '.', '.', '2', '.', '.', '.', '6' }, { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
-        { '.', '.', '.', '4', '1', '9', '.', '.', '5' }, { '.', '.', '.', '.', '8', '.', '.', '7', '9' } },
-        expected = new char[][] { { '5', '3', '4', '6', '7', '8', '9', '1', '2' },
-            { '6', '7', '2', '1', '9', '5', '3', '4', '8' }, { '1', '9', '8', '3', '4', '2', '5', '6', '7' },
-            { '8', '5', '9', '7', '6', '1', '4', '2', '3' }, { '4', '2', '6', '8', '5', '3', '7', '9', '1' },
-            { '7', '1', '3', '9', '2', '4', '8', '5', '6' }, { '9', '6', '1', '5', '3', '7', '2', '8', '4' },
-            { '2', '8', '7', '4', '1', '9', '6', '3', '5' }, { '3', '4', '5', '2', '8', '6', '1', '7', '9' } };
-    solveSudoku(board);
-    assertArrayEquals(expected, board);
-  }
 }
