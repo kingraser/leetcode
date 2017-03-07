db1a63f7154b0af0f84877287465eaedcf80617e
kinghost <kingraser@gmail.com>
2017-03-07 18:58:33 +0800

    hash-table

--- src/leetcode/GroupAnagrams.java
+++ src/leetcode/GroupAnagrams.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -19,9 +14,6 @@ import java.util.stream.Stream;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年10月12日<p>
-//-------------------------------------------------------
 public class GroupAnagrams {
 
   /*
