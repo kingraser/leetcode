02a5b67dc3307d0aea2e87f0f32a8ae124742140
kinghost <kingraser@gmail.com>
2017-03-07 14:10:20 +0800

    linked list

--- src/leetcode/RemoveDuplicatesfromSortedList.java
+++ src/leetcode/RemoveDuplicatesfromSortedList.java
@@ -1,26 +1,31 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
+
+import static leetcode.common.ListNode.list;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 import leetcode.common.ListNode;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月13日<p>
-//-------------------------------------------------------
 public class RemoveDuplicatesfromSortedList {
 
   /*
+  Given a sorted linked list, delete all duplicates such that each element appear only once.  
+  For example,
   Given 1->1->2, return 1->2.
   Given 1->1->2->3->3, return 1->2->3
   */
 
+  @Test
+  public void test() {
+    assertEquals(list(1, 2), deleteDuplicates(list(1, 1, 2)));
+    assertEquals(list(1, 2, 3), deleteDuplicates(list(1, 1, 2, 3, 3)));
+  }
+
   public ListNode deleteDuplicates(ListNode head) {
-    for (ListNode march = head; march != null && march.next != null;)
-      if (march.val == march.next.val) march.next = march.next.next;
-      else march = march.next;
+    for (ListNode current = head; current != null && current.next != null;)
+      if (current.val == current.next.val) current.next = current.next.next;
+      else current = current.next;
     return head;
   }
 
--- src/leetcode/RemoveDuplicatesfromSortedListII.java
+++ src/leetcode/RemoveDuplicatesfromSortedListII.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static leetcode.common.ListNode.list;
@@ -12,10 +7,8 @@ import org.junit.Test;
 
 import leetcode.common.ListNode;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年10月21日<p>
-//-------------------------------------------------------
 public class RemoveDuplicatesfromSortedListII {
+
   /*
   Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
   
@@ -26,41 +19,19 @@ public class RemoveDuplicatesfromSortedListII {
 
   @Test
   public void test() {
-    assertEquals(list(1), deleteDuplicatesII(list(1, 2, 2)));
-    assertEquals(list(1, 2, 5), deleteDuplicatesII(list(1, 2, 3, 3, 4, 4, 5)));
-    assertEquals(list(2, 3), deleteDuplicatesII(list(1, 1, 1, 2, 3)));
-    assertEquals(null, deleteDuplicatesII(list(1, 1)));
+    assertEquals(list(1, 2, 5), deleteDuplicates(list(1, 2, 3, 3, 4, 4, 5)));
+    assertEquals(list(2, 3), deleteDuplicates(list(1, 1, 1, 2, 3)));
+
+    assertEquals(list(1), deleteDuplicates(list(1, 2, 2)));
+    assertEquals(null, deleteDuplicates(list(1, 1)));
   }
 
-  //递归
   public ListNode deleteDuplicates(ListNode head) {
-    if (head == null) return null;
-    boolean hasDup = false, hasNext = false;
-    for (; head.next != null; head = head.next)
-      if (head.val == head.next.val) hasDup = true;
-      else if (hasDup) hasDup = false;
-      else {
-        hasNext = true;
-        break;
-      }
-    if (hasDup && !hasNext) return null;
-    head.next = deleteDuplicates(head.next);
-    return head;
-  }
-
-  //迭代
-  public ListNode deleteDuplicatesII(ListNode head) {
     if (head == null || head.next == null) return head;
-    ListNode HEAD = new ListNode(-1), insert = HEAD;
-    boolean hasDup = false;
-    for (; head.next != null; head = head.next)
-      if (head.val == head.next.val) hasDup = true;
-      else if (hasDup) hasDup = false;
-      else {
-        insert.next = head;
-        insert = insert.next;
-      }
-    insert.next = hasDup ? null : head;
-    return HEAD.next;
+    if (head.val == head.next.val) {
+      for (; head.next != null && head.val == head.next.val; head = head.next);
+      return deleteDuplicates(head.next);
+    } else head.next = deleteDuplicates(head.next);
+    return head;
   }
 }
