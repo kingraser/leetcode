7291914eb116ab4db6340e055c7cc569734f0add
kinghost <kingraser@gmail.com>
2017-03-08 12:28:46 +0800

    math string

--- src/leetcode/AddBinary.java
+++ src/leetcode/AddBinary.java
@@ -1,19 +1,13 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月13日<p>
-//-------------------------------------------------------
 public class AddBinary {
+
   /*
+  Given two binary strings, return their sum (also a binary string). 
   For example,
   a = "11"
   b = "1"
@@ -23,13 +17,22 @@ public class AddBinary {
   @Test
   public void test() {
     assertEquals("100", addBinary("11", "1"));
+    assertEquals("110", addBinary("11", "11"));
   }
 
   public String addBinary(String a, String b) {
-    int al = a.length(), bl = b.length(), k = Math.max(al, bl);
-    char[] A = new char[k + 1];
-    for (int i = --al, j = --bl, v, c = 0; i >= 0 || j >= 0 || c > 0; c = v > 1 ? 1 : 0)
-      A[k--] = (char) ((v = (i < 0 ? 0 : a.charAt(i--) - '0') + (j < 0 ? 0 : b.charAt(j--) - '0') + c) % 2 + '0');
-    return new String(A, ++k, A.length - k);
+    int al = a.length(), bl = b.length(), idx = Math.max(al, bl);
+    char[] A = new char[idx + 1];
+    for (int aIdx = --al, bIdx = --bl, carry = 0; aIdx >= 0 || bIdx >= 0 || carry > 0; carry >>= 1)
+      A[idx--] = digit((carry += getDigit(a, aIdx--) + getDigit(b, bIdx--)) & 1);
+    return new String(A, ++idx, A.length - idx);
+  }
+
+  private int getDigit(String s, int idx) {
+    return idx < 0 ? 0 : s.charAt(idx) - '0';
+  }
+
+  private char digit(int i) {
+    return (char) (i + '0');
   }
 }
