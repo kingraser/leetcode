359454ba15fcdcc39f368b382218de47319a68a5
kinghost <kingraser@gmail.com>
2017-03-08 16:21:53 +0800

    dynamic-programming

--- src/leetcode/ScrambleString.java
+++ src/leetcode/ScrambleString.java
@@ -1,17 +1,9 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年10月13日<p>
-//-------------------------------------------------------
 public class ScrambleString {
   /*
   Given a string s1, 
@@ -53,34 +45,28 @@ public class ScrambleString {
   */
 
   /*
-          首先想到的是递归(即深搜),对两个string进行分割,然后比较四对字符串。
-          代码虽然简单,但是复杂度比较高。
-          有两种加速策略,一种是剪枝,提前返回;一种是加缓存,缓存中间结果,即memorization(翻译为记忆化搜索)。
-          剪枝可以五花八门,要充分观察,充分利用信息,找到能让节点提前返回的条件。
-          例如,判断两个字符串是否互为scamble,至少要求每个字符在两个字符串中出现的次数要相等,如果不相等则返回false。
-          加缓存,可以用数组或HashMap。本题维数较高,用HashMap,map和unordered_map均可。
-          既然可以用记忆化搜索,这题也一定可以用动规。
-          设状态为f[i][j][n],表示长度为n,起点为s1[i]和起点为s2[j]两个字符串是否互为scramble,
-          则状态转移方程为f[i][j][n] = (f[i][j][k] && f[i+k][j+k][n-k]) || (f[i][j+n-k][k] && f[i+k][j][n-k])
+  Let f[i][j][n] represents whether s1[i, i + n] is a scramble string of s2[j, j + n],
+  Thus f[i][j][n] = (f[i][j][k] && f[i+k][j+k][n-k]) || (f[i][j+n-k][k] && f[i+k][j][n-k])
   */
 
   public boolean isScramble(String s1, String s2) {
-    boolean[][][] dp = new boolean[s1.length()][s1.length()][s1.length() + 1];
-    for (int i = s1.length() - 1; i >= 0; i--)
-      for (int j = s1.length() - 1; j >= 0; j--)
-        for (int k = 1, size = s1.length() - Math.max(i, j); k <= size; k++)
-          if (equals(s1, s2, i, j, k)) dp[i][j][k] = true;
-          else for (int l = 1; l < k; l++)
-            if (dp[i][j][l] && dp[i + l][j + l][k - l] || dp[i][j + k - l][l] && dp[i + l][j][k - l]) {
-              dp[i][j][k] = true;
+    boolean[][][] dp = new boolean[s1.length()][s2.length()][s1.length() + 1];
+    for (int idx1 = s1.length() - 1; idx1 >= 0; idx1--)
+      for (int idx2 = s2.length() - 1; idx2 >= 0; idx2--)
+        for (int len = 1, size = s1.length() - Math.max(idx1, idx2); len <= size; len++)
+          if (equals(s1, s2, idx1, idx2, len)) dp[idx1][idx2][len] = true;
+          else for (int l = 1; l < len; l++)
+            if (dp[idx1][idx2][l] && dp[idx1 + l][idx2 + l][len - l]
+                || dp[idx1][idx2 + len - l][l] && dp[idx1 + l][idx2][len - l]) {
+              dp[idx1][idx2][len] = true;
               break;
             }
     return dp[0][0][s1.length()];
   }
 
-  private boolean equals(String s1, String s2, int i, int j, int len) {
+  private boolean equals(String s1, String s2, int idx1, int idx2, int len) {
     while (len-- > 0)
-      if (s1.charAt(i++) != s2.charAt(j++)) return false;
+      if (s1.charAt(idx1++) != s2.charAt(idx2++)) return false;
     return true;
   }
 
