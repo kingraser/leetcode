2f277e85af9875e195de676d7deb2ebee3f3dcf0
kinghost <kingraser@gmail.com>
2017-03-08 19:18:14 +0800

    dfs

--- src/leetcode/SurroundedRegions.java
+++ src/leetcode/SurroundedRegions.java
@@ -1,18 +1,11 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年10月16日<p>
-//-------------------------------------------------------
 public class SurroundedRegions {
+
   /*
   Given a 2D board containing 'X' and 'O', capture all regions surrounded by 'X'.    
   A region is captured by flipping all 'O's into 'X's in that surrounded region.    
@@ -28,27 +21,26 @@ public class SurroundedRegions {
   X O X X    
   */
 
-  //深搜。从上下左右四个边界往里走,凡是能碰到的'O',都是跟边界接壤的,应该保留。
-
   public void solve(char[][] board) {
     boolean[][] reached = new boolean[board.length][board[0].length];
-    for (int i = 0; i < board.length; i += board.length - 1)//从上下边搜
-      for (int j = 0; j < board[0].length; j++)
-        if (board[i][j] == 'O') dfs(board, i + (i == 0 ? 1 : -1), j, reached);
-    for (int j = 0; j < board[0].length; j += board[0].length - 1)//从左右边搜
-      for (int i = 1; i < board.length - 1; i++)
-        if (board[i][j] == 'O') dfs(board, i, j + (j == 0 ? 1 : -1), reached);
-    for (int i = 1; i < board.length - 1; i++)//change 0 to X
-      for (int j = 1; j < board[0].length - 1; j++)
-        if (board[i][j] == 'O' && !reached[i][j]) board[i][j] = 'X';
+    for (int row = 0; row < board.length; row += board.length - 1) // search from top and bottom
+      for (int col = 0; col < board[0].length; col++)
+        if (board[row][col] == 'O') dfs(board, row + (row == 0 ? 1 : -1), col, reached);
+    for (int col = 0; col < board[0].length; col += board[0].length - 1) // search from left and right
+      for (int row = 1; row < board.length - 1; row++)
+        if (board[row][col] == 'O') dfs(board, row, col + (col == 0 ? 1 : -1), reached);
+    for (int row = 1; row < board.length - 1; row++) // change 0 to X
+      for (int col = 1; col < board[0].length - 1; col++)
+        if (board[row][col] == 'O' && !reached[row][col]) board[row][col] = 'X';
   }
 
-  private int[] dx = new int[] { 1, -1, 0, 0 }, dy = new int[] { 0, 0, 1, -1 };
+  private int[] dRow = new int[] { 1, -1, 0, 0 }, dCol = new int[] { 0, 0, 1, -1 };
 
-  private void dfs(char[][] A, int i, int j, boolean[][] reached) {
-    if (i < 1 || i > A.length - 2 || j < 1 || j > A[0].length - 2 || reached[i][j] || A[i][j] != 'O') return;
-    reached[i][j] = true;
-    for (int k = 0; k < 4; dfs(A, i + dx[k], j + dy[k++], reached));
+  private void dfs(char[][] A, int row, int col, boolean[][] reached) {
+    if (row < 1 || row > A.length - 2 || col < 1 || col > A[0].length - 2 || reached[row][col] || A[row][col] != 'O')
+      return;
+    reached[row][col] = true;
+    for (int i = 0; i < 4; dfs(A, row + dRow[i], col + dCol[i++], reached));
   }
 
   @Test
