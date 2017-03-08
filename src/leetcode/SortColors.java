49bff98783c0f1d717d911b282d1639201c1b776
kinghost <kingraser@gmail.com>
2017-03-08 15:10:31 +0800

    array sort

--- src/leetcode/SortColors.java
+++ src/leetcode/SortColors.java
@@ -31,8 +31,7 @@ public class SortColors {
   }
 
   public void sortColors(int A[]) {
-    int red = 0, blue = A.length - 1;
-    for (int i = 0; i < blue + 1;)
+    for (int red = 0, blue = A.length - 1, i = 0; i < blue + 1;)
       if (A[i] == 0) swap(A, i++, red++);
       else if (A[i] == 2) swap(A, i, blue--);
       else i++;
