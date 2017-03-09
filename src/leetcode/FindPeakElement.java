599bab1917bd03dc6bf4ff3cd1e61aee799bcf4a
kinghost <kingraser@gmail.com>
2017-03-09 12:26:29 +0800

    binary search

--- src/leetcode/FindPeakElement.java
+++ src/leetcode/FindPeakElement.java
@@ -1,17 +1,9 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月14日<p>
-//-------------------------------------------------------
 public class FindPeakElement {
 
   /*
