813fd93457e0379467bd1d6073db05e9ab0388e2
kinghost <kingraser@gmail.com>
2017-03-09 12:22:42 +0800

    design stack

--- src/leetcode/MinStack.java
+++ src/leetcode/MinStack.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -13,9 +8,6 @@ import java.util.stream.Stream;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月12日<p>
-//-------------------------------------------------------
 public class MinStack {
 
   /*
@@ -29,36 +21,39 @@ public class MinStack {
 
   @Test
   public void test() {
-    Stream.of(3, 2, 1).forEach(i -> push(i));
-    assertEquals(1, getMin());
-    assertEquals(1, top());
-    pop();
-    assertEquals(2, getMin());
-    assertEquals(2, top());
-    pop();
-    assertEquals(3, getMin());
-    assertEquals(3, top());
-    push(1);
-    assertEquals(1, getMin());
-    assertEquals(1, top());
+    MyStack stack = new MyStack();
+    Stream.of(3, 2, 1).forEach(i -> stack.push(i));
+    assertEquals(1, stack.getMin());
+    assertEquals(1, stack.top());
+    stack.pop();
+    assertEquals(2, stack.getMin());
+    assertEquals(2, stack.top());
+    stack.pop();
+    assertEquals(3, stack.getMin());
+    assertEquals(3, stack.top());
+    stack.push(1);
+    assertEquals(1, stack.getMin());
+    assertEquals(1, stack.top());
   }
 
-  Stack<Integer> stack = new Stack<>(), minStack = new Stack<>();
+  class MyStack {
+    Stack<Integer> stack = new Stack<>(), minStack = new Stack<>();
 
-  public void push(int x) {
-    if (minStack.isEmpty() || x <= minStack.peek()) minStack.push(x);
-    stack.push(x);
-  }
+    public void push(int x) {
+      if (minStack.isEmpty() || x <= minStack.peek()) minStack.push(x);
+      stack.push(x);
+    }
 
-  public void pop() {
-    if (Objects.equals(stack.pop(), minStack.peek())) minStack.pop();
-  }
+    public void pop() {
+      if (Objects.equals(stack.pop(), minStack.peek())) minStack.pop();
+    }
 
-  public int top() {
-    return stack.peek();
-  }
+    public int top() {
+      return stack.peek();
+    }
 
-  public int getMin() {
-    return minStack.peek();
+    public int getMin() {
+      return minStack.peek();
+    }
   }
 }
