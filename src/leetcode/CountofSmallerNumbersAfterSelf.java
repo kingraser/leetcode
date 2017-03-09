e817813e42c6d3340b2c3fe18351ba3be47d275a
kinghost <kingraser@gmail.com>
2017-03-09 17:59:30 +0800

    merge-sort binary-search-tree

--- src/leetcode/CountofSmallerNumbersAfterSelf.java
+++ src/leetcode/CountofSmallerNumbersAfterSelf.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -10,13 +5,12 @@ import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年9月8日;
-//-------------------------------------------------------
 public class CountofSmallerNumbersAfterSelf {
+
   /*
   You are given an integer array nums and you have to return a new counts array. 
   The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
@@ -35,17 +29,18 @@ public class CountofSmallerNumbersAfterSelf {
     assertEquals(Arrays.asList(2, 1, 1, 0), countSmaller(new int[] { 5, 2, 6, 1 }));
   }
 
-  public List<Integer> countSmaller(int[] nums) {
-    Node root = null;
+  // binary search tree
+  public List<Integer> countSmallerZero(int[] nums) {
+    TreeNode root = null;
     int[] result = new int[nums.length];
     for (int i = nums.length - 1; i >= 0; i--)
       root = insert(root, nums[i], result, i, 0);
     return Arrays.stream(result).boxed().collect(Collectors.toList());
   }
 
-  public Node insert(Node root, int val, int[] ans, int index, int preSum) {
+  public TreeNode insert(TreeNode root, int val, int[] ans, int index, int preSum) {
     if (root == null) {
-      root = new Node(val, 0);
+      root = new TreeNode(val, 0);
       ans[index] = preSum;
     } else if (root.val > val) {
       root.small++;
@@ -54,13 +49,52 @@ public class CountofSmallerNumbersAfterSelf {
     return root;
   }
 
-  class Node {
+  class TreeNode {
     int val, small;
-    Node left, right;
+    TreeNode left, right;
 
-    public Node(int val, int small) {
+    public TreeNode(int val, int small) {
       this.val = val;
       this.small = small;
+    }
+  }
+
+  // merge sort
+  public List<Integer> countSmaller(int[] nums) {
+    int[] result = new int[nums.length];
+    mergeCount(result, IntStream.range(0, nums.length).mapToObj(i -> new Node(nums[i], i)).toArray(l -> new Node[l]), 0,
+        nums.length - 1);
+    return Arrays.stream(result).boxed().collect(Collectors.toList());
+  }
+
+  /**
+   * @param result
+   * @param nums
+   * @param left inclusive
+   * @param right inclusive
+   */
+  private void mergeCount(int[] result, Node[] nums, int left, int right) {
+    if (left >= right) return;
+    int mid = (left + right) >> 1;
+    mergeCount(result, nums, left, mid);
+    mergeCount(result, nums, mid + 1, right);
+    Node[] cache = new Node[right - left + 1];
+    int rightIdx1 = mid + 1, rightIdx2 = mid + 1;
+    for (int leftIdx = left, idx = 0; leftIdx <= mid; leftIdx++, idx++) {
+      for (; rightIdx1 <= right && nums[leftIdx].val > nums[rightIdx1].val; rightIdx1++);
+      for (; rightIdx2 <= right && nums[rightIdx2].val < nums[leftIdx].val; cache[idx++] = nums[rightIdx2++]);
+      cache[idx] = nums[leftIdx];
+      result[nums[leftIdx].idx] += rightIdx1 - mid - 1;
+    }
+    System.arraycopy(cache, 0, nums, left, rightIdx2 - left);
+  }
+
+  class Node {
+    int val, idx;
+
+    public Node(int val, int idx) {
+      this.val = val;
+      this.idx = idx;
     }
   }
 }
