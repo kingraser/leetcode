db6ded3f239eaf878fa2d4bf4bc0f3f36ad6bc39
kinghost <kingraser@gmail.com>
2017-03-09 14:09:42 +0800

    hash-table math

--- src/leetcode/HappyNumber.java
+++ src/leetcode/HappyNumber.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertTrue;
@@ -13,9 +8,6 @@ import org.junit.Test;
 
 import com.google.common.collect.Sets;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月13日<p>
-//-------------------------------------------------------
 public class HappyNumber {
 
   /*
