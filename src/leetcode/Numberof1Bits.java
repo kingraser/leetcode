028b9de8c991029d121843445a48f8e2e02df591
kinghost <kingraser@gmail.com>
2017-03-09 13:50:52 +0800

    bit manipulation

--- src/leetcode/Numberof1Bits.java
+++ src/leetcode/Numberof1Bits.java
@@ -1,17 +1,9 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月11日<p>
-//-------------------------------------------------------
 public class Numberof1Bits {
   /*
   Write a function that takes an unsigned integer 
@@ -23,16 +15,16 @@ public class Numberof1Bits {
 
   @Test
   public void test() {
+    assertEquals(3, hammingWeightZero(11));
     assertEquals(3, hammingWeight(11));
-    assertEquals(3, hammingWeightII(11));
   }
 
   //cheat
-  public int hammingWeight(int n) {
+  public int hammingWeightZero(int n) {
     return Integer.bitCount(n);
   }
 
-  public int hammingWeightII(int n) {
+  public int hammingWeight(int n) {
     int result = 0;
     for (; n != 0; n >>= 1)
       if ((n & 1) == 1) result++;
--- src/leetcode/ReverseBits.java
+++ src/leetcode/ReverseBits.java
@@ -1,33 +1,32 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月13日<p>
-//-------------------------------------------------------
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class ReverseBits {
+
   /*
   Reverse bits of a given 32 bits unsigned integer.
   For example, 
   given input 43261596 (represented in binary as 00000010100101000001111010011100), 
-  return 964176192 (represented in binary as 00111001011110000010100101000000).
-  
-  1作弊法,系统自带
-  2模拟
+  return 964176192 (represented in binary as 00111001011110000010100101000000). 
   */
 
-  // you need treat n as an unsigned value
-  public int reverseBits(int n) {
+  @Test
+  public void test() {
+    assertEquals(964176192, reverseBits(43261596));
+  }
+
+  // cheat with built-in function
+  public int reverseBitsZero(int n) {
     return Integer.reverse(n);
   }
 
-  public int reverseBitsII(int n) {
+  public int reverseBits(int n) {
     int result = 0;
     for (int i = 0; i < 32; i++) {
-      result += n & 1;
+      result |= n & 1;
       n >>>= 1;
       if (i < 31) result <<= 1;
     }
