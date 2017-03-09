c8db9380186a683e2f6e9be1bd976d2eee69001a
kinghost <kingraser@gmail.com>
2017-03-09 17:21:27 +0800

    binary search

--- src/leetcode/LongestIncreasingSubsequence.java
+++ src/leetcode/LongestIncreasingSubsequence.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -11,9 +6,6 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年10月11日;
-//-------------------------------------------------------
 public class LongestIncreasingSubsequence {
 
   /*
