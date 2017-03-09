5b0b00b778e0e3fdbc0bccb25cf945c306d9d0c2
kinghost <kingraser@gmail.com>
2017-03-09 14:57:27 +0800

    stack math

--- src/leetcode/BasicCalculatorII.java
+++ src/leetcode/BasicCalculatorII.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -15,9 +10,7 @@ import org.junit.Test;
 
 import com.google.common.collect.ImmutableMap;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年3月1日;
-//-------------------------------------------------------
+
 public class BasicCalculatorII {
   /*
   implement a basic calculator to evaluate a simple expression string.
