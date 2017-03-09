40a7866686e1cd91f159ccf4a767d3297712b49b
kinghost <kingraser@gmail.com>
2017-03-09 16:56:27 +0800

    math

--- src/leetcode/WaterandJugProblem.java
+++ src/leetcode/WaterandJugProblem.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertFalse;
@@ -10,9 +5,6 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年6月24日;
-//-------------------------------------------------------
 public class WaterandJugProblem {
 
   /*
