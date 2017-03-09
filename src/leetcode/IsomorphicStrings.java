3f1bb45c8d7260a0738dd62864c3325c8a1ac1fd
kinghost <kingraser@gmail.com>
2017-03-09 14:17:20 +0800

    hash-table

--- src/leetcode/IsomorphicStrings.java
+++ src/leetcode/IsomorphicStrings.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertFalse;
@@ -10,12 +5,10 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月13日<p>
-//-------------------------------------------------------
 public class IsomorphicStrings {
 
   /*
@@ -32,13 +25,10 @@ public class IsomorphicStrings {
     assertFalse(isIsomorphic("foo", "bar"));
   }
 
-  private int i;
-
   public boolean isIsomorphic(String s, String t) {
     if (s.length() != t.length()) return false;
     Map<Character, Character> map = new HashMap<>();
-    for (i = 0; i < s.length(); i++)
-      if (map.computeIfAbsent(s.charAt(i), k -> t.charAt(i)) != t.charAt(i)) return false;
-    return true;
+    return IntStream.range(0, s.length())
+        .allMatch(i -> map.computeIfAbsent(s.charAt(i), k -> t.charAt(i)) == t.charAt(i));
   }
 }
