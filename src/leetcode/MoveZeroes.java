96d51452777543160edb11c556299d278381041a
kinghost <kingraser@gmail.com>
2017-03-09 17:02:10 +0800

    array

--- src/leetcode/MoveZeroes.java
+++ src/leetcode/MoveZeroes.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -11,9 +6,6 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月21日<p>
-//-------------------------------------------------------
 public class MoveZeroes {
 
   /*
