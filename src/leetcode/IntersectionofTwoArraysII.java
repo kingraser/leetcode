f6115d629d70175014eaba9656a9cbb99fa6df5e
kinghost <kingraser@gmail.com>
2017-03-09 19:07:22 +0800

    array

--- src/leetcode/IntersectionofTwoArraysII.java
+++ src/leetcode/IntersectionofTwoArraysII.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -13,9 +8,6 @@ import java.util.stream.Collectors;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年5月18日;
-//-------------------------------------------------------
 public class IntersectionofTwoArraysII {
 
   /*
