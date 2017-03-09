ed26bd55ab0cf096c9caf1afc3876cfda2195222
kinghost <kingraser@gmail.com>
2017-03-09 12:12:40 +0800

    math hash-table

--- src/leetcode/MaxPointsonaLine.java
+++ src/leetcode/MaxPointsonaLine.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -14,9 +9,6 @@ import org.junit.Test;
 
 import leetcode.common.Point;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月16日<p>
-//-------------------------------------------------------
 public class MaxPointsonaLine {
 
   /*
