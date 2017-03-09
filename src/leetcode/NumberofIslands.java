24aa2285435978e32cb5e155649bdfa52d50b059
kinghost <kingraser@gmail.com>
2017-03-09 14:07:57 +0800

    dfs

--- src/leetcode/NumberofIslands.java
+++ src/leetcode/NumberofIslands.java
@@ -1,20 +1,14 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
 import java.util.BitSet;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年10月16日<p>
-//-------------------------------------------------------
 public class NumberofIslands {
+
   /*
   Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
   An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
@@ -40,7 +34,7 @@ public class NumberofIslands {
         new char[][] { "11000".toCharArray(), "11000".toCharArray(), "00100".toCharArray(), "00011".toCharArray() }));
   }
 
-  int[] dx = { 0, 0, 1, -1 }, dy = { -1, 1, 0, 0 };
+  int[][] dirs = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
 
   public int numIslands(char[][] grid) {
     if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
@@ -60,8 +54,7 @@ public class NumberofIslands {
         || visited.get(i * grid[0].length + j))
       return;
     visited.set(i * grid[0].length + j);
-    for (int k = 0; k < 4; k++)
-      dfs(grid, i + dx[k], j + dy[k], visited);
+    Arrays.stream(dirs).forEach(dir -> dfs(grid, i + dir[0], j + dir[1], visited));
   }
 
 }
