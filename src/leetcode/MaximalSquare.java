9b409796acb6e5bd4be15b7b315a3c1490fbe900
kinghost <kingraser@gmail.com>
2017-03-09 14:45:30 +0800

    dynamic-programming

--- src/leetcode/MaximalSquare.java
+++ src/leetcode/MaximalSquare.java
@@ -1,17 +1,11 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年10月18日<p>
-//-------------------------------------------------------
+import leetcode.util.MathUtil;
+
 public class MaximalSquare {
 
   /*
@@ -27,9 +21,11 @@ public class MaximalSquare {
   Return 4. 
   */
 
-  //令A[x][y]表示以(x,y)为右下角的最大正方形的边长
-  //A[x][y]=0 if matrix[x][y]==0
-  //A[x][y]=min(A[x - 1][y - 1], A[x][y - 1], A[x - 1][y]) + 1 if matrix[x][y]==1
+  /*
+  Let A[x][y] represents the length of edges of the largest square with right bottom(x,y).
+  A[x][y] = 0 if matrix[x][y] == 0
+  A[x][y] = min(A[x - 1][y - 1], A[x][y - 1], A[x - 1][y]) + 1 if matrix[x][y] == 1
+  */
 
   @Test
   public void test() {
@@ -54,14 +50,7 @@ public class MaximalSquare {
     for (int i = 1; i < matrix.length; i++)
       for (int j = 1; j < matrix[0].length; max = Math.max(max, A[i][j++]))
         if (matrix[i][j] == '0') A[i][j] = 0;
-        else A[i][j] = 1 + min(A[i - 1][j], A[i - 1][j - 1], A[i][j - 1]);
+        else A[i][j] = 1 + MathUtil.min(A[i - 1][j], A[i - 1][j - 1], A[i][j - 1]);
     return max * max;
-  }
-
-  public int min(int... nums) {
-    int min = Integer.MAX_VALUE;
-    for (int num : nums)
-      if (num < min) min = num;
-    return min;
   }
 }
