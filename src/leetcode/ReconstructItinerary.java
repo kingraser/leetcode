0db7432f35889e9275254814feb71469d2037835
kinghost <kingraser@gmail.com>
2017-03-09 18:45:29 +0800

    dfs

--- src/leetcode/ReconstructItinerary.java
+++ src/leetcode/ReconstructItinerary.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -16,9 +11,6 @@ import java.util.PriorityQueue;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年2月15日;
-//-------------------------------------------------------
 public class ReconstructItinerary {
 
   /*
