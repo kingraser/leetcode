7cd4b8afaa818ef79dc3b4a1485095170d87c971
kinghost <kingraser@gmail.com>
2017-03-09 12:34:48 +0800

    2 pointers

--- src/leetcode/TwoSumII.java
+++ src/leetcode/TwoSumII.java
@@ -1,17 +1,10 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年9月1日;
-//-------------------------------------------------------
+
 public class TwoSumII {
   /*
   Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.  
