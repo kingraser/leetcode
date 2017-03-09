f8492e2372479c0b2800539dc408c463f03dd5d0
kinghost <kingraser@gmail.com>
2017-03-09 16:30:33 +0800

    binary search

--- src/leetcode/MissingNumber.java
+++ src/leetcode/MissingNumber.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -11,9 +6,6 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月2日<p>
-//-------------------------------------------------------
 public class MissingNumber {
 
   /*
