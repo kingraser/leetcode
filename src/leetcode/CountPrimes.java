81ad0907e453144b510a92fd6f7d5e4870fec4fd
kinghost <kingraser@gmail.com>
2017-03-09 14:13:42 +0800

    math

--- src/leetcode/CountPrimes.java
+++ src/leetcode/CountPrimes.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -11,9 +6,6 @@ import java.util.BitSet;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月14日<p>
-//-------------------------------------------------------
 public class CountPrimes {
 
   //Count the number of prime numbers less than a non-negative number, n.
