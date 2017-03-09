a59ca578dbc5ef6d3c18372eb2cc538115c6727e
kinghost <kingraser@gmail.com>
2017-03-09 19:11:45 +0800

    dynamic-programming

--- src/leetcode/ArithmeticSlicesIISubsequence.java
+++ src/leetcode/ArithmeticSlicesIISubsequence.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -14,9 +9,6 @@ import java.util.Map;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年11月8日;
-//-------------------------------------------------------
 public class ArithmeticSlicesIISubsequence {
   /*
   A sequence of numbers is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
