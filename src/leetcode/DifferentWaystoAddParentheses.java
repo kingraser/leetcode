126af8a459b4c8d3e4ac22afe85ce2be35d8c1eb
kinghost <kingraser@gmail.com>
2017-03-09 16:15:35 +0800

    divide and conquer

--- src/leetcode/DifferentWaystoAddParentheses.java
+++ src/leetcode/DifferentWaystoAddParentheses.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -18,9 +13,7 @@ import org.junit.Test;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Sets;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年3月3日;
-//-------------------------------------------------------
+
 public class DifferentWaystoAddParentheses {
   /*
   Given a string of numbers and operators, 
