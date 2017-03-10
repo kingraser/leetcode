76487f9a8a95be513b0debf31de1012e921e81be
kinghost <kingraser@gmail.com>
2017-03-10 13:49:29 +0800

    array sort

--- src/leetcode/QueueReconstructionbyHeight.java
+++ src/leetcode/QueueReconstructionbyHeight.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -13,9 +8,6 @@ import java.util.List;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年9月26日;
-//-------------------------------------------------------
 public class QueueReconstructionbyHeight {
 
   /*
