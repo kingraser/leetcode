b4295433c40939c612ab08de91d7f90400b816de
kinghost <kingraser@gmail.com>
2017-03-10 18:30:07 +0800

    update

--- src/leetcode/MaxSumofRectangleNoLargerThanK.java
+++ src/leetcode/MaxSumofRectangleNoLargerThanK.java
@@ -1,13 +1,5 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年6月22日;
-//-------------------------------------------------------
 public class MaxSumofRectangleNoLargerThanK {
 
   /*
--- src/leetcode/MaxSumofSubMatrixNoLargerThanK.java
+++ src/leetcode/MaxSumofSubMatrixNoLargerThanK.java
@@ -9,17 +9,18 @@ import org.junit.Test;
 public class MaxSumofSubMatrixNoLargerThanK {
 
   /*
-  Given a non-empty 2D matrix matrix and an integer k, find the max sum of a rectangle in the matrix such that its sum is no larger than k.
+  Given a non-empty 2D matrix matrix and an integer k, 
+  find the max sum of a rectangle in the matrix such that its sum is no larger than k.
   
-  Example:
-  
+  Example:  
   Given matrix = [
   [1,  0, 1],
   [0, -2, 3]
   ]
   k = 2
   
-  The answer is 2. Because the sum of rectangle [[0, 1], [-2, 3]] is 2 and 2 is the max number no larger than k (k = 2).
+  The answer is 2. 
+  Because the sum of rectangle [[0, 1], [-2, 3]] is 2 and 2 is the max number no larger than k (k = 2).
   
   Note:
     The rectangle inside the matrix must have an area > 0.
