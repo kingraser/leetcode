8eb40bbc501b63343c94287cfdbfe8df836f69c4
kinghost <kingraser@gmail.com>
2017-03-10 13:42:56 +0800

    string math

--- src/leetcode/RemoveKDigits.java
+++ src/leetcode/RemoveKDigits.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -11,32 +6,26 @@ import java.math.BigInteger;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年9月20日;
-//-------------------------------------------------------
 public class RemoveKDigits {
+
   /*
   Given a non-negative integer num represented as a string, remove k digits from the number so that the new number is the smallest possible.
   
-  Note:
-  
+  Note:  
   The length of num is less than 10002 and will be ≥ k.
   The given num does not contain any leading zero.
   
-  Example 1:
-  
+  Example 1:  
   Input: num = "1432219", k = 3
   Output: "1219"
   Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
   
-  Example 2:
-  
+  Example 2:  
   Input: num = "10200", k = 1
   Output: "200"
   Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
   
-  Example 3:
-  
+  Example 3:  
   Input: num = "10", k = 2
   Output: "0"
   Explanation: Remove all the digits from the number and it is left with nothing which is 0.    
