379a18b16849508ac4ec7959eacdf8d9d8bc740d
kinghost <kingraser@gmail.com>
2017-03-10 14:51:39 +0800

    hash-table

--- src/leetcode/NumberofBoomerangs.java
+++ src/leetcode/NumberofBoomerangs.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -13,25 +8,16 @@ import java.util.Objects;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年11月7日;
-//-------------------------------------------------------
 public class NumberofBoomerangs {
 
   /*
   Given n points in the plane that are all pairwise distinct, a "boomerang" is a tuple of points (i, j, k) such that the distance between i and j equals the distance between i and k (the order of the tuple matters). 
   Find the number of boomerangs. You may assume that n will be at most 500 and coordinates of points are all in the range [-10000, 10000] (inclusive).
   
-  Example:
-  
-  Input:
-  [[0,0],[1,0],[2,0]]
-  
-  Output:
-  2
-  
-  Explanation:
-  The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]] 
+  Example:  
+  Input: [[0,0],[1,0],[2,0]]
+  Output: 2  
+  Explanation: The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]] 
   */
 
   @Test
