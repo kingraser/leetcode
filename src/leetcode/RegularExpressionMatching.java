a01cca8eaf9d5a478cdcf34c534b94ccf1d0031c
kinghost <kingraser@gmail.com>
2017-03-10 15:35:51 +0800

    dynamic-programming

--- src/leetcode/RegularExpressionMatching.java
+++ src/leetcode/RegularExpressionMatching.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2012 Qunar.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertFalse;
@@ -10,49 +5,28 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-// <p>@author wit Initial Created at 2015年9月10日<p>
-//-------------------------------------------------------
 public class RegularExpressionMatching {
 
   /*
-  1作弊
-  2递归
-  3动规
-  dp[i][j]表示字串 s.subString(0,i), p.subString(0,j) 是否可以匹配
-  dp[i][j]=
-      if(p.charAt(j - 1) != '*') 
-          matches(s.charAt(i - 1), p.charAt(j - 1)) && dp[i - 1][j - 1] && i > 0
-      else
-          j >= 2 && (dp[i][j - 2] || (i > 0 && matches(s.charAt(i - 1), p.charAt(j - 2)) && dp[i - 1][j]))
+  Implement regular expression matching with support for '.' and '*'.
+  
+  '.' Matches any single character.
+  '*' Matches zero or more of the preceding element.
+  
+  The matching should cover the entire input string (not partial).
+  
+  The function prototype should be:
+  bool isMatch(const char *s, const char *p)
+  
+  Some examples:
+  isMatch("aa","a") → false
+  isMatch("aa","aa") → true
+  isMatch("aaa","aa") → false
+  isMatch("aa", "a*") → true
+  isMatch("aa", ".*") → true
+  isMatch("ab", ".*") → true
+  isMatch("aab", "c*a*b") → true
   */
-
-  public boolean isMatch(String s, String p) {
-    return s.matches(p);
-  }
-
-  public boolean isMatchII(String s, String p) {
-    if (p.isEmpty()) return s.isEmpty();
-    if (p.length() > 1 && p.charAt(1) == '*') return isMatch(s, p.substring(2))
-        || (p.charAt(0) == '.' && !s.isEmpty() || s.charAt(0) == p.charAt(0)) && isMatch(s.substring(1), p);
-    if (p.charAt(0) == '.') return !s.isEmpty() && isMatch(s.substring(1), p.substring(1));
-    return s.charAt(0) == p.charAt(0) && isMatch(s.substring(1), p.substring(1));
-  }
-
-  public boolean isMatchIII(String s, String p) {
-    boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
-    dp[0][0] = true;
-    for (int i = 0; i <= s.length(); i++)
-      for (int j = 1; j <= p.length(); j++)
-        if (p.charAt(j - 1) != '*') dp[i][j] = i > 0 && matches(s.charAt(i - 1), p.charAt(j - 1)) && dp[i - 1][j - 1];
-        else
-          dp[i][j] = j >= 2 && (dp[i][j - 2] || (i > 0 && matches(s.charAt(i - 1), p.charAt(j - 2)) && dp[i - 1][j]));
-    return dp[s.length()][p.length()];
-  }
-
-  private boolean matches(char c, char p) {
-    return p == '.' ? true : c == p;
-  }
 
   @Test
   public void test() {
@@ -64,6 +38,35 @@ public class RegularExpressionMatching {
     assertTrue(isMatch("ab", ".*"));
     assertTrue(isMatch("aab", "c*a*b"));
     assertTrue(isMatch("aab", "a*b"));
+  }
+
+  public boolean isMatchZero(String s, String p) {
+    return s.matches(p);
+  }
+
+  // recursive
+  public boolean isMatchII(String s, String p) {
+    if (p.isEmpty()) return s.isEmpty();
+    if (p.length() > 1 && p.charAt(1) == '*') return isMatch(s, p.substring(2))
+        || (p.charAt(0) == '.' && !s.isEmpty() || s.charAt(0) == p.charAt(0)) && isMatch(s.substring(1), p);
+    if (p.charAt(0) == '.') return !s.isEmpty() && isMatch(s.substring(1), p.substring(1));
+    return s.charAt(0) == p.charAt(0) && isMatch(s.substring(1), p.substring(1));
+  }
+
+  // dp[i][j] represents whether s.subString(0, i) matches p.subString(0, j)
+  public boolean isMatch(String s, String p) {
+    boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+    dp[0][0] = true;
+    for (int i = 0; i <= s.length(); i++)
+      for (int j = 1; j <= p.length(); j++)
+        if (p.charAt(j - 1) != '*') dp[i][j] = i > 0 && matches(s.charAt(i - 1), p.charAt(j - 1)) && dp[i - 1][j - 1];
+        else
+          dp[i][j] = j >= 2 && (dp[i][j - 2] || (i > 0 && matches(s.charAt(i - 1), p.charAt(j - 2)) && dp[i - 1][j]));
+    return dp[s.length()][p.length()];
+  }
+
+  private boolean matches(char c, char p) {
+    return p == '.' || c == p;
   }
 
 }
