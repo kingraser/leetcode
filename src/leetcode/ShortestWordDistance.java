59b6f501d604abda3db7bd70b8baae3f338e0659
kinghost <kingraser@gmail.com>
2017-03-10 17:30:17 +0800

    array

--- src/leetcode/ShortestWordDistance.java
+++ src/leetcode/ShortestWordDistance.java
@@ -2,9 +2,6 @@ package leetcode;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.junit.Test;
 
 public class ShortestWordDistance {
@@ -18,17 +15,17 @@ public class ShortestWordDistance {
 
   @Test
   public void test() {
-    List<String> list = Arrays.asList("practice", "makes", "perfect", "coding", "makes");
-    assertEquals(3, shortestDistance(list, "coding", "practice"));
-    assertEquals(1, shortestDistance(list, "coding", "makes"));
+    String[] words = { "practice", "makes", "perfect", "coding", "makes" };
+    assertEquals(3, shortestDistance(words, "coding", "practice"));
+    assertEquals(1, shortestDistance(words, "coding", "makes"));
   }
 
-  public int shortestDistance(List<String> words, String word1, String word2) {
+  public int shortestDistance(String[] words, String word1, String word2) {
     int result = Integer.MAX_VALUE;
-    for (int i = 0, idx1 = -1, idx2 = -1; i < words.size(); result = Math.min(result,
-        idx1 == -1 || idx2 == -1 ? result : Math.abs(idx1 - idx2)), i++)
-      if (word1.equals(words.get(i))) idx1 = i;
-      else if (word2.equals(words.get(i))) idx2 = i;
+    for (int idx = 0, idx1 = -1, idx2 = -1; idx < words.length; result = Math.min(result,
+        idx1 == -1 || idx2 == -1 ? result : Math.abs(idx1 - idx2)), idx++)
+      if (word1.equals(words[idx])) idx1 = idx;
+      else if (word2.equals(words[idx])) idx2 = idx;
     return result;
   }
 
