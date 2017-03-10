bd9560dc15408434a38b3e771bb3ca36332d5407
kinghost <kingraser@gmail.com>
2017-03-10 14:55:22 +0800

    array math

--- src/leetcode/MinimumMovestoEqualArrayElements.java
+++ src/leetcode/MinimumMovestoEqualArrayElements.java
@@ -1,33 +1,19 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import java.util.Arrays;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年11月7日;
-//-------------------------------------------------------
 public class MinimumMovestoEqualArrayElements {
 
   /*
   Given a non-empty integer array of size n, find the minimum number of moves required to make all array elements equal, where a move is incrementing n - 1 elements by 1.
   
-  Example:
-  
-  Input:
-  [1,2,3]
-  
-  Output:
-  3
-  
+  Example:  
+  Input: [1,2,3]
+  Output: 3  
   Explanation:
-  Only three moves are needed (remember each move increments two elements):
-  
+  Only three moves are needed (remember each move increments two elements):  
   [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
   */
 
