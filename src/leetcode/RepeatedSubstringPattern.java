6cad88752fe574ba60b67f7572b4836e2594d52a
kinghost <kingraser@gmail.com>
2017-03-10 15:02:02 +0800

    dfs

--- src/leetcode/RepeatedSubstringPattern.java
+++ src/leetcode/RepeatedSubstringPattern.java
@@ -6,29 +6,22 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class RepeatedSubstringPattern {
+
   /*
   Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together. You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
   
-  Example 1:
-  
-  Input: "abab"
-  
-  Output: True
-  
+  Example 1:  
+  Input: "abab"  
+  Output: True  
   Explanation: It's the substring "ab" twice.
   
-  Example 2:
-  
-  Input: "aba"
-  
-  Output: False
-  
-  Example 3:
-  
-  Input: "abcabcabcabc"
-  
-  Output: True
-  
+  Example 2:  
+  Input: "aba"  
+  Output: False 
+   
+  Example 3:  
+  Input: "abcabcabcabc"  
+  Output: True  
   Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
   */
 
