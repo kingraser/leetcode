d2e0f6f7687c7877f082584590f234887c5ae547
kinghost <kingraser@gmail.com>
2017-03-10 10:05:49 +0800

    tree-map design

--- src/leetcode/DataStreamasDisjointIntervals.java
+++ src/leetcode/DataStreamasDisjointIntervals.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import java.util.ArrayList;
@@ -16,9 +11,6 @@ import org.junit.Test;
 
 import leetcode.common.Interval;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年6月1日;
-//-------------------------------------------------------
 public class DataStreamasDisjointIntervals {
 
   /*
