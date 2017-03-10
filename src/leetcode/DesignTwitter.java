669b4f9b745cd634a317e73d9975d6f12f590b95
kinghost <kingraser@gmail.com>
2017-03-10 10:12:59 +0800

    design

--- src/leetcode/DesignTwitter.java
+++ src/leetcode/DesignTwitter.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -19,9 +14,6 @@ import java.util.TreeSet;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年9月30日;
-//-------------------------------------------------------
 public class DesignTwitter {
 
   /*
