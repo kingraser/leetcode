fcffdb64c71800605c98a24236c0dbf130a5b486
kinghost <kingraser@gmail.com>
2017-03-10 13:19:05 +0800

    binary search

--- src/leetcode/SplitArrayLargestSum.java
+++ src/leetcode/SplitArrayLargestSum.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -11,10 +6,25 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年10月8日;
-//-------------------------------------------------------
 public class SplitArrayLargestSum {
+
+  /*
+  Given an array which consists of non-negative integers and an integer m, 
+  you can split the array into m non-empty continuous subarrays. 
+  Write an algorithm to minimize the largest sum among these m subarrays.
+  Note: Given m satisfies the following constraint: 1 ≤ m ≤ length(nums) ≤ 14,000.
+  
+  Examples:  
+  Input: nums = [7,2,5,10,8]
+  m = 2  
+  Output:
+  18
+  
+  Explanation:
+  There are four ways to split nums into two subarrays.
+  The best way is to split it into [7,2,5] and [10,8],
+  where the largest sum among the two subarrays is only 18.
+  */
 
   @Test
   public void test() {
