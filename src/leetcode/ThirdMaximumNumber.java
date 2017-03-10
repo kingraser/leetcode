6a83bb50728d98c26a0da0150b220f55a5094410
kinghost <kingraser@gmail.com>
2017-03-10 13:13:27 +0800

    tree-set

--- src/leetcode/ThirdMaximumNumber.java
+++ src/leetcode/ThirdMaximumNumber.java
@@ -6,39 +6,24 @@ import java.util.TreeSet;
 
 import org.junit.Test;
 
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
-
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年10月24日;
-//-------------------------------------------------------
 public class ThirdMaximumNumber {
+
   /*
-  Given a non-empty array of integers, return the third maximum number in this array. If it does not exist, return the maximum number. The time complexity must be in O(n).
+  Given a non-empty array of integers, return the third maximum number in this array. 
+  If it does not exist, return the maximum number. The time complexity must be in O(n).
   
-  Example 1:
-  
-  Input: [3, 2, 1]
-  
-  Output: 1
-  
+  Example 1:  
+  Input: [3, 2, 1]  
+  Output: 1  
   Explanation: The third maximum is 1.
   
-  Example 2:
-  
-  Input: [1, 2]
-  
-  Output: 2
-  
+  Example 2:  
+  Input: [1, 2]  
+  Output: 2  
   Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
   
-  Example 3:
-  
-  Input: [2, 2, 3, 1]
-  
+  Example 3:  
+  Input: [2, 2, 3, 1]  
   Output: 1
   
   Explanation: Note that the third maximum here means the third maximum distinct number.
