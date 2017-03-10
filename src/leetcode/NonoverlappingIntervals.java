cb822db199e1b1f934fa312e45b1d7cdc5f4c469
kinghost <kingraser@gmail.com>
2017-03-10 14:14:30 +0800

    greedy

--- src/leetcode/NonoverlappingIntervals.java
+++ src/leetcode/NonoverlappingIntervals.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -13,41 +8,28 @@ import org.junit.Test;
 
 import leetcode.common.Interval;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年11月15日;
-//-------------------------------------------------------
 public class NonoverlappingIntervals {
 
   /*
   Given a collection of intervals, find the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
   
-  Note:
-  
+  Note:  
     You may assume the interval's end point is always bigger than its start point.
     Intervals like [1,2] and [2,3] have borders "touching" but they don't overlap each other.
   
-  Example 1:
-  
-  Input: [ [1,2], [2,3], [3,4], [1,3] ]
-  
-  Output: 1
-  
+  Example 1:  
+  Input: [ [1,2], [2,3], [3,4], [1,3] ]  
+  Output: 1  
   Explanation: [1,3] can be removed and the rest of intervals are non-overlapping.
   
-  Example 2:
-  
-  Input: [ [1,2], [1,2], [1,2] ]
-  
-  Output: 2
-  
+  Example 2:  
+  Input: [ [1,2], [1,2], [1,2] ]  
+  Output: 2  
   Explanation: You need to remove two [1,2] to make the rest of intervals non-overlapping.
   
-  Example 3:
-  
-  Input: [ [1,2], [2,3] ]
-  
-  Output: 0
-  
+  Example 3:  
+  Input: [ [1,2], [2,3] ]  
+  Output: 0  
   Explanation: You don't need to remove any of the intervals since they're already non-overlapping. 
   */
 
