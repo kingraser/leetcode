9ecfd0aa7a883cafdbe8219edbb1874221691905
kinghost <kingraser@gmail.com>
2017-03-10 17:25:06 +0800

    string

--- src/leetcode/ValidWordSquare.java
+++ src/leetcode/ValidWordSquare.java
@@ -12,11 +12,13 @@ public class ValidWordSquare {
 
   /*
   Given a sequence of words, check whether it forms a valid word square.  
-  A sequence of words forms a valid word square if the kth row and column read the exact same string, where 0 ≤k < max(numRows, numColumns).
+  A sequence of words forms a valid word square if the kth row and column read the exact same string, 
+  where 0 ≤k < max(numRows, numColumns).
   Note:  
     The number of words given is at least 1 and does not exceed 500.
     Word length will be at least 1 and does not exceed 500.
-    Each word contains only lowercase English alphabet a-z.  
+    Each word contains only lowercase English alphabet a-z.
+      
   Example 1:  
   Input:
   [
@@ -40,8 +42,7 @@ public class ValidWordSquare {
   "bnrt",
   "crm",
   "dt"
-  ]
-  
+  ]  
   Output: true  
   Explanation:
   The first row and first column both read "abcd".
