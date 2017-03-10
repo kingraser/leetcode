4f8b2c854a03d90f115c9967de6a7aa5918e1dc3
kinghost <kingraser@gmail.com>
2017-03-10 13:57:33 +0800

    dfs

--- src/leetcode/PartitionEqualSubsetSum.java
+++ src/leetcode/PartitionEqualSubsetSum.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertTrue;
@@ -12,9 +7,6 @@ import java.util.BitSet;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年10月10日;
-//-------------------------------------------------------
 public class PartitionEqualSubsetSum {
 
   /*
