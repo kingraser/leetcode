02599f3632ff48981d90a890eae6bd708d73ea71
kinghost <kingraser@gmail.com>
2017-03-10 14:59:07 +0800

    greedy

--- src/leetcode/AssignCookies.java
+++ src/leetcode/AssignCookies.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -11,9 +6,6 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年11月16日;
-//-------------------------------------------------------
 public class AssignCookies {
   /*
   Assume you are an awesome parent and want to give your children some cookies. 
@@ -23,22 +15,16 @@ public class AssignCookies {
   You may assume the greed factor is always positive.
   You cannot assign more than one cookie to one child.
   
-  Example 1:
-  
-  Input: [1,2,3], [1,1]
-  
-  Output: 1
-  
+  Example 1:  
+  Input: [1,2,3], [1,1]  
+  Output: 1  
   Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3. 
   And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content.
   You need to output 1.
   
-  Example 2:
-  
-  Input: [1,2], [1,2,3]
-  
-  Output: 2
-  
+  Example 2:  
+  Input: [1,2], [1,2,3]  
+  Output: 2  
   Explanation: You have 2 children and 3 cookies. The greed factors of 2 children are 1, 2. 
   You have 3 cookies and their sizes are big enough to gratify all of the children, 
   You need to output 2.
