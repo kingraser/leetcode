a977b0c99b61e22ee8c8e2147e2fe8bfd53aeb2b
kinghost <kingraser@gmail.com>
2017-03-10 12:14:51 +0800

    stack string

--- src/leetcode/LongestAbsoluteFilePath.java
+++ src/leetcode/LongestAbsoluteFilePath.java
@@ -1,8 +1,3 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
@@ -11,9 +6,6 @@ import java.util.Stack;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年8月23日;
-//-------------------------------------------------------
 public class LongestAbsoluteFilePath {
 
   /*
