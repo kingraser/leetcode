f0c5e4c2e20431e2b482e692d9f244310529ed2b
kinghost <kingraser@gmail.com>
2017-03-10 18:29:59 +0800

    math

--- src/leetcode/SuperPow.java
+++ src/leetcode/SuperPow.java
@@ -5,37 +5,36 @@ public class SuperPow {
   /*
   Your task is to calculate ab mod 1337 where a is a positive integer and b is an extremely large positive integer given in the form of an array.
   
-  Example1:
-  
+  Example1:  
   a = 2
-  b = [3]
-  
+  b = [3]  
   Result: 8
   
-  Example2:
-  
+  Example2:  
   a = 2
-  b = [1,0]
-  
+  b = [1,0]  
   Result: 1024 
   */
 
+  /*
+  An integer can be represented as sum(k[i] * 10^i). So a^b can be represented as multiply(a^(k[i] * 10^i))
+  We know (a*b) mod n = ((a mod n) * (b mod n)) mod n
+  */
+
   public int superPow(int a, int[] b) {
-    if (a % 1337 == 0) return 0;
-    int p = 0;
-    for (int i : b)
-      p = (p * 10 + i) % 1140;
-    return power(a, p == 0 ? 1440 : p, 1337);
+    return superPow(a, b, 1337);
   }
 
-  public int power(int a, int n, int mod) {
-    a %= mod;
+  private int superPow(int a, int[] b, int mod) {
     int result = 1;
-    while (n != 0) {
-      if ((n & 1) != 0) result = result * a % mod;
-      a = a * a % mod;
-      n >>= 1;
-    }
+    for (int i = b.length - 1; i >= 0; i--)
+      result = (result * pow(a, b[i], b.length - 1 - i, mod)) % mod;
     return result;
   }
+
+  private int pow(int a, int k, int i, int mod) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
 }
