8ab879aa06c93c02fb353dd72f3a69919a5bbadb
kinghost <kingraser@gmail.com>
2017-03-10 18:26:41 +0800

    string

--- src/leetcode/NumberofSegmentsinaString.java
+++ src/leetcode/NumberofSegmentsinaString.java
@@ -1,6 +1,24 @@
 package leetcode;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class NumberofSegmentsinaString {
+
+  /*
+  Count the number of segments in a string, 
+  where a segment is defined to be a contiguous sequence of non-space characters.  
+  Please note that the string does not contain any non-printable characters.  
+  Example:  
+  Input: "Hello, my name is John"
+  Output: 5  
+  */
+
+  @Test
+  public void test() {
+    assertEquals(5, countSegments("Hello, my name is John"));
+  }
 
   public int countSegments(String s) {
     int result = 0;
