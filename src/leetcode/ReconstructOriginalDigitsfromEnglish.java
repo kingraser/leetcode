6499f651a56587031de81170e1887039155db8c8
kinghost <kingraser@gmail.com>
2017-03-10 14:08:01 +0800

    string

--- src/leetcode/ReconstructOriginalDigitsfromEnglish.java
+++ src/leetcode/ReconstructOriginalDigitsfromEnglish.java
@@ -1,18 +1,28 @@
-/*
- * $Id$
- *
- * Copyright (c) 2015 Sogou.com. All Rights Reserved.
- */
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-//--------------------- Change Logs----------------------
-//@author wangwenlong Initial Created at 2016年10月17日;
-//-------------------------------------------------------
 public class ReconstructOriginalDigitsfromEnglish {
+
+  /*
+  Given a non-empty string containing an out-of-order English representation of digits 0-9, 
+  output the digits in ascending order.
+  
+  Note:  
+    Input contains only lowercase English letters.
+    Input is guaranteed to be valid and can be transformed to its original digits. That means invalid inputs such as "abc" or "zerone" are not permitted.
+    Input length is less than 50,000.
+  
+  Example 1:  
+  Input: "owoztneoer"  
+  Output: "012"
+  
+  Example 2:  
+  Input: "fviefuro"  
+  Output: "45"  
+  */
 
   @Test
   public void test() {
