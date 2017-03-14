81148263c2a5bd2883273841004038d440b1930e
kinghost <kingraser@gmail.com>
2017-03-14 12:25:55 +0800

    string math

--- src/leetcode/StringtoIntegeratoi.java
+++ src/leetcode/StringtoIntegeratoi.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class StringtoInteger {
+public class StringtoIntegeratoi {
 
   /*
   implement atoi to convert a string to an integer.  
