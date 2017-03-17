bf1787232626490d3d36a508881fb4874cdf58a5
kinghost <kingraser@gmail.com>
2017-03-17 15:13:45 +0800

    Topological Sort

--- src/leetcode/AlienDictionary.java
+++ src/leetcode/AlienDictionary.java
@@ -1,14 +1,18 @@
 package leetcode;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.BitSet;
+import java.util.Deque;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
 import org.junit.Test;
+
+import leetcode.common.Pair;
 
 public class AlienDictionary {
 
@@ -26,7 +30,7 @@ public class AlienDictionary {
   "ett",
   "rftt"
   ]  
-  The correct order is: "werft".  
+  The correct order is: "wertf".  
   Note:  
         You may assume all letters are in lowercase.
         If the order is invalid, return an empty string.
@@ -35,57 +39,40 @@ public class AlienDictionary {
 
   @Test
   public void test() {
-    assertTrue(alienOrder(Arrays.asList("wrt", "wrf", "er", "ett", "rftt")).matches("[we]{2}rft"));
-    assertEquals("", alienOrder(Arrays.asList("ab", "ba")));
+    assertEquals("wertf", alienOrder(Arrays.asList("wrt", "wrf", "er", "ett", "rftt")));
+    assertEquals("", alienOrder(Arrays.asList("wrtkj", "wrt")));
   }
 
   public String alienOrder(List<String> words) {
-    Map<Character, Node> map = new HashMap<>();
-    for (String word : words)
-      if (!add(word, map)) return "";
-    return sort(map);
+    return sort(getOrders(words), words);
   }
 
-  private String sort(Map<Character, Node> map) {
-    return new String(map.values().stream().sorted((n1, n2) -> n1.val - n2.val).mapToInt(n -> n.c).toArray(), 0,
-        map.size());
+  private String sort(Pair<boolean[][], int[]> orders, List<String> words) {
+    if (Objects.isNull(orders)) return "";
+    BitSet letters = new BitSet();
+    words.forEach(word -> word.chars().forEach(c -> letters.set(c)));
+    int count = letters.cardinality(), result[] = new int[count];
+    Deque<Integer> zeroDegrees = new ArrayDeque<>();
+    IntStream.range(0, 26).filter(i -> orders.value[i] == 0 && letters.get(i + 'a')).forEach(i -> zeroDegrees.add(i));
+    for (int node, i = 0, j; !zeroDegrees.isEmpty(); result[i++] = node + 'a', count--)
+      for (node = zeroDegrees.poll(), j = 0; j < 26; j++)
+        if (orders.key[node][j] && --orders.value[j] == 0) zeroDegrees.add(j);
+    return count == 0 ? new String(result, 0, result.length) : "";
   }
 
-  private boolean add(String word, Map<Character, Node> map) {
-    Node current, previous = null;
-    for (int i = word.length() - 1; i >= 0; previous = current) {
-      char c = word.charAt(i--);
-      current = map.computeIfAbsent(c, k -> new Node(c));
-      if (previous == null || previous.c == current.c) continue;
-      if (current.hasChild(previous)) return false;
-      previous.add(current);
+  private Pair<boolean[][], int[]> getOrders(List<String> words) {
+    boolean[][] map = new boolean[26][26];
+    int[] degrees = new int[26];
+    A: for (int wordIdx = 0; wordIdx < words.size() - 1;) {
+      String former = words.get(wordIdx), latter = words.get(++wordIdx);
+      for (int idx = 0, len = Math.min(former.length(), latter.length()), c1, c2; idx < len; idx++)
+        if ((c1 = former.charAt(idx) - 'a') != (c2 = latter.charAt(idx) - 'a')) {
+          degrees[c2]++;
+          map[c1][c2] = true;
+          continue A;
+        }
+      if (former.length() > latter.length()) return null;
     }
-    return true;
-  }
-
-  public class Node {
-    public char c;
-    public int val = Integer.MAX_VALUE - 1;
-    public Map<Character, Node> nextMap = new HashMap<>();
-
-    public Node(char c) {
-      this.c = c;
-    }
-
-    private void sub(Node node, Node prev) {
-      if (node.val < prev.val) return;
-      node.val = prev.val - 1;
-      node.nextMap.values().forEach(next -> sub(next, node));
-    }
-
-    public void add(Node next) {
-      nextMap.computeIfAbsent(next.c, k -> next);
-      sub(next, this);
-    }
-
-    public boolean hasChild(Node node) {
-      return node == null ? false
-          : nextMap.containsKey(node.c) ? true : nextMap.values().stream().anyMatch(next -> next.hasChild(node));
-    }
+    return new Pair<>(map, degrees);
   }
 }
