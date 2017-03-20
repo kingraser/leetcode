1a6d7844f65f1180a049121c9347cf0346a2fde4
kinghost <kingraser@gmail.com>
2017-03-20 14:12:17 +0800

    Topological Sort

--- src/leetcode/AlienDictionary.java
+++ src/leetcode/AlienDictionary.java
@@ -47,32 +47,32 @@ public class AlienDictionary {
     return sort(getOrders(words), words);
   }
 
-  private String sort(Pair<boolean[][], int[]> orders, List<String> words) {
+  private String sort(Pair<BitSet, int[]> orders, List<String> words) {
     if (Objects.isNull(orders)) return "";
     BitSet letters = new BitSet();
     words.forEach(word -> word.chars().forEach(c -> letters.set(c)));
     int count = letters.cardinality(), result[] = new int[count];
     Deque<Integer> zeroDegrees = new ArrayDeque<>();
     IntStream.range(0, 26).filter(i -> orders.value[i] == 0 && letters.get(i + 'a')).forEach(i -> zeroDegrees.add(i));
-    for (int node, i = 0, j; !zeroDegrees.isEmpty(); result[i++] = node + 'a', count--)
-      for (node = zeroDegrees.poll(), j = 0; j < 26; j++)
-        if (orders.key[node][j] && --orders.value[j] == 0) zeroDegrees.add(j);
+    for (int node, resultIdx = 0, idx; !zeroDegrees.isEmpty(); result[resultIdx++] = node + 'a', count--)
+      for (node = zeroDegrees.poll(), idx = 0; idx < 26; idx++)
+        if (orders.key.get(node * 26 + idx) && --orders.value[idx] == 0) zeroDegrees.add(idx);
     return count == 0 ? new String(result, 0, result.length) : "";
   }
 
-  private Pair<boolean[][], int[]> getOrders(List<String> words) {
-    boolean[][] map = new boolean[26][26];
+  private Pair<BitSet, int[]> getOrders(List<String> words) {
+    BitSet bitSet = new BitSet();
     int[] degrees = new int[26];
     A: for (int wordIdx = 0; wordIdx < words.size() - 1;) {
       String former = words.get(wordIdx), latter = words.get(++wordIdx);
       for (int idx = 0, len = Math.min(former.length(), latter.length()), c1, c2; idx < len; idx++)
         if ((c1 = former.charAt(idx) - 'a') != (c2 = latter.charAt(idx) - 'a')) {
           degrees[c2]++;
-          map[c1][c2] = true;
+          bitSet.set(c1 * 26 + c2);
           continue A;
         }
       if (former.length() > latter.length()) return null;
     }
-    return new Pair<>(map, degrees);
+    return new Pair<>(bitSet, degrees);
   }
 }
