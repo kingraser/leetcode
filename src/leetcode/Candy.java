d9bea8670c078557d121e6551fe6d36c280f4d88
kinghost <kingraser@gmail.com>
2017-03-20 17:41:45 +0800

    array greedy

--- src/leetcode/Candy.java
+++ src/leetcode/Candy.java
@@ -7,11 +7,10 @@ import java.util.Arrays;
 import org.junit.Test;
 
 public class Candy {
+
   /*
-  There are N children standing in a line. Each child is assigned a rating value.
-  
-  You are giving candies to these children subjected to the following requirements:
-  
+  There are N children standing in a line. Each child is assigned a rating value.  
+  You are giving candies to these children subjected to the following requirements:  
   Each child must have at least one candy.
   Children with a higher rating get more candies than their neighbors.
   
