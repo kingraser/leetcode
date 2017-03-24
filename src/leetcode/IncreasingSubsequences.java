fea71d67fb836010dba4385db7555d77e1318376
kinghost <kingraser@gmail.com>
2017-03-24 15:55:10 +0800

    dfs

--- src/leetcode/IncreasingSubsequences.java
+++ src/leetcode/IncreasingSubsequences.java
@@ -9,23 +9,21 @@ import java.util.Deque;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.junit.Test;
 
 public class IncreasingSubsequences {
 
   /*
-  Given an integer array, your task is to find all the different possible increasing subsequences of the given array, and the length of an increasing subsequence should be at least 2 .
+  Given an integer array, 
+  your task is to find all the different possible increasing subsequences of the given array, 
+  and the length of an increasing subsequence should be at least 2 .
   
-  Example:
-  
+  Example:  
   Input: [4, 6, 7, 7]
   Output: [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7,7], [4,7,7]]
   
-  Note:
-  
+  Note:  
     The length of the given array will not exceed 15.
     The range of integer in the given array is [-100,100].
     The given array may contain duplicates, and two equal integers should also be considered as a special case of increasing sequence.
@@ -33,10 +31,10 @@ public class IncreasingSubsequences {
 
   @Test
   public void test() {
-    assertEquals(Stream
-        .of(Arrays.asList(4, 6), Arrays.asList(4, 6, 7), Arrays.asList(4, 6, 7, 7), Arrays.asList(4, 7),
-            Arrays.asList(4, 7, 7), Arrays.asList(6, 7), Arrays.asList(6, 7, 7), Arrays.asList(7, 7))
-        .collect(Collectors.toSet()), new HashSet<>(findSubsequences(new int[] { 4, 6, 7, 7 })));
+    assertEquals(
+        Arrays.asList(Arrays.asList(4, 6), Arrays.asList(4, 6, 7), Arrays.asList(4, 6, 7, 7), Arrays.asList(4, 7),
+            Arrays.asList(4, 7, 7), Arrays.asList(6, 7), Arrays.asList(6, 7, 7), Arrays.asList(7, 7)),
+        findSubsequences(new int[] { 4, 6, 7, 7 }));
   }
 
   public List<List<Integer>> findSubsequences(int[] nums) {
