c4a63b052c4ffd74d03b37db60436b145586c1ef
kinghost <kingraser@gmail.com>
2017-03-31 10:53:46 +0800

    math string

--- src/leetcode/AddBinary.java
+++ src/leetcode/AddBinary.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import static leetcode.AddStrings.addStrings;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -21,17 +22,6 @@ public class AddBinary {
   }
 
   public String addBinary(String a, String b) {
-    StringBuilder result = new StringBuilder();
-    for (int aIdx = a.length() - 1, bIdx = b.length() - 1, carry = 0; aIdx >= 0 || bIdx >= 0 || carry > 0; carry >>= 1)
-      result.append(digit((carry += getDigit(a, aIdx--) + getDigit(b, bIdx--)) & 1));
-    return result.reverse().toString();
-  }
-
-  public static int getDigit(String s, int idx) {
-    return idx < 0 ? 0 : s.charAt(idx) - '0';
-  }
-
-  public static char digit(int i) {
-    return (char) (i + '0');
+    return addStrings(a, b, 2);
   }
 }
--- src/leetcode/AddStrings.java
+++ src/leetcode/AddStrings.java
@@ -1,7 +1,5 @@
 package leetcode;
 
-import static leetcode.AddBinary.digit;
-import static leetcode.AddBinary.getDigit;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -24,10 +22,22 @@ public class AddStrings {
   }
 
   public String addStrings(String a, String b) {
+    return addStrings(a, b, 10);
+  }
+
+  public static String addStrings(String a, String b, int radix) {
     StringBuilder result = new StringBuilder();
-    for (int aIdx = a.length() - 1, bIdx = b.length() - 1, carry = 0; aIdx >= 0 || bIdx >= 0 || carry > 0; carry /= 10)
-      result.append(digit((carry += getDigit(a, aIdx--) + getDigit(b, bIdx--)) % 10));
+    for (int aIdx = a.length() - 1, bIdx = b.length() - 1, carry = 0; aIdx >= 0 || bIdx >= 0
+        || carry > 0; carry /= radix)
+      result.append(digit((carry += getDigit(a, aIdx--) + getDigit(b, bIdx--)) % radix));
     return result.reverse().toString();
   }
 
+  public static int getDigit(String s, int idx) {
+    return idx < 0 ? 0 : s.charAt(idx) - '0';
+  }
+
+  public static char digit(int i) {
+    return (char) (i + '0');
+  }
 }
