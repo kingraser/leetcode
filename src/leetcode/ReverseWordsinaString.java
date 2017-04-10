fffd13afc4da693023e1b16f0a063c0b7e50d205
kinghost <kingraser@gmail.com>
2017-04-10 15:22:37 +0800

    array string

--- src/leetcode/ReverseWordsinaString.java
+++ src/leetcode/ReverseWordsinaString.java
@@ -24,34 +24,9 @@ public class ReverseWordsinaString {
   }
 
   public static String reverseWords(String s) {
-    if (s == null) return null;
-    char[] array = s.toCharArray();
-    reverseWords(array);
-    return removeUnnecessarySpaces(array);
-  }
-
-  static void reverseWords(char[] array) {
-    ArrayUtil.reverse(array);
-    reverseSingleWordOneByOne(array);
-  }
-
-  static void reverseSingleWordOneByOne(char[] array) {
-    for (int left, right = 0, end = ArrayUtil.findLastNot(array, ' '); right < end;)
-      if ((left = ArrayUtil.findFirstNot(array, right, end, ' ')) == -1) return;
-      else ArrayUtil.reverse(array, left,
-          (right = ArrayUtil.findFirst(array, left, end, ' ')) == -1 ? right = end : right - 1);
-  }
-
-  static String removeUnnecessarySpaces(char[] array) {
-    int start = ArrayUtil.findFirstNot(array, ' '), end = ArrayUtil.findLastNot(array, ' '), writeIdx = start;
-    if (start == -1) return "";
-    for (int readIdx = start; readIdx <= end;) {
-      for (; readIdx <= end && array[readIdx] != ' '; array[writeIdx++] = array[readIdx++]);
-      if (readIdx > end) break;
-      array[writeIdx++] = ' ';
-      for (; readIdx <= end && array[readIdx] == ' '; readIdx++);
-    }
-    return new String(array, start, writeIdx - start);
+    String[] result = s.trim().split(" +");
+    ArrayUtil.reverse(result);
+    return String.join(" ", result);
   }
 
 }
