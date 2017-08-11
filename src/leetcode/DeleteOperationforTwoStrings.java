ccb0a17a55cf1fd9f5f8c37ac8039f9e561f8112
kingraser <kingraser@gmail.com>
2017-08-11 15:28:42 +0800

    dynamic-programming

--- src/leetcode/DeleteOperationforTwoStrings.java
+++ src/leetcode/DeleteOperationforTwoStrings.java
@@ -26,13 +26,16 @@ public class DeleteOperationforTwoStrings {
     assertEquals(2, minDistance("sea", "eat"));
   }
 
+  // let dp[i][j] represents the minimum delete distance of s1(0, i) and s2(0, j)
   public int minDistance(String s1, String s2) {
     int[][] dp = new int[s1.length() + 1][s2.length() + 1];
     for (int i = 0; i <= s1.length(); i++)
       for (int j = 0; j <= s2.length(); j++)
-        if (i > 0 && j > 0)
-          dp[i][j] = s1.charAt(i - 1) == s2.charAt(j - 1) ? 1 + dp[i - 1][j - 1] : Math.max(dp[i - 1][j], dp[i][j - 1]);
-    return s1.length() + s2.length() - (dp[s1.length()][s2.length()] << 1);
+        if (i == 0) dp[i][j] = j;
+        else if (j == 0) dp[i][j] = i;
+        else if (s1.charAt(i - 1) == s2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1];
+        else dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
+    return dp[s1.length()][s2.length()];
   }
 
 }
