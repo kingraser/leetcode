b445f9684b9ee37ebef0d751382c32a8a655d6bb
wit <kingraser@gmail.com>
2024-01-10 15:46:08 +0800

    Array

---
+++ src/leetcode/MatrixSimilarityAfterCyclicShifts.java
@@ -0,0 +1,64 @@
+package leetcode;
+
+import leetcode.util.TestUtil;
+import org.junit.Test;
+
+/**
+ * @author Wit
+ */
+public class MatrixSimilarityAfterCyclicShifts {
+	/*
+	You are given a 0-indexed m x n integer matrix mat and an integer k. You have to cyclically right shift odd indexed rows k times and cyclically left shift even indexed rows k times.
+	Return true if the initial and final matrix are exactly the same and false otherwise.
+
+	Example 1:
+	Input: mat = [[1,2,1,2],[5,5,5,5],[6,3,6,3]], k = 2
+	Output: true
+	Explanation:
+
+	Initially, the matrix looks like the first figure.
+	Second figure represents the state of the matrix after one right and left cyclic shifts to even and odd indexed rows.
+	Third figure is the final state of the matrix after two cyclic shifts which is similar to the initial matrix.
+	Therefore, return true.
+
+	Example 2:
+	Input: mat = [[2,2],[2,2]], k = 3
+	Output: true
+	Explanation: As all the values are equal in the matrix, even after performing cyclic shifts the matrix will remain the same. Therefeore, we return true.
+
+	Example 3:
+	Input: mat = [[1,2]], k = 1
+	Output: false
+	Explanation: After one cyclic shift, mat = [[2,1]] which is not equal to the initial matrix. Therefore we return false.
+
+	Constraints:
+		1 <= mat.length <= 25
+		1 <= mat[i].length <= 25
+		1 <= mat[i][j] <= 25
+		1 <= k <= 50
+	*/
+	@Test
+	public void test() {
+		TestUtil.testEquals(new Object[][]{
+				{false, TestUtil.getArray("[[15,15,8]]"), 1},
+				{false, TestUtil.getArray("[[7,11,11]]"), 31},
+				{true, TestUtil.getArray("[[1,2,1,2],[5,5,5,5],[6,3,6,3]]"), 2},
+				{true, TestUtil.getArray("[[2,2],[2,2]]"), 3},
+				{false, TestUtil.getArray("[[1,2]]"), 1},
+		});
+	}
+
+	int rowLength, half;
+
+	public boolean areSimilar(int[][] mat, int k) {
+		if ((k %= (rowLength = mat[0].length)) == 0) return true;
+		if (k > (half = rowLength >> 1)) k = k - rowLength;
+		for (int[] row : mat) if (!areSimilar(row, k = -k)) return false;
+		return true;
+	}
+
+	boolean areSimilar(int[] row, int k) {
+		for (int i = k > 0 ? 0 : half, j = k > 0 ? half : rowLength; i < j; ) if (row[i] != row[i++ + k]) return false;
+		return true;
+	}
+}
