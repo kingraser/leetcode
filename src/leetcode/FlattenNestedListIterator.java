f3be75b7b19e1a586f8263c8e9c9b60ea5787796
udsjz3o <wenlong.wang@mobilityasia.com.cn>
2024-04-29 14:04:09 +0800

    create CollectionUtils.java to convert iterator to list

--- src/leetcode/FlattenNestedListIterator.java
+++ src/leetcode/FlattenNestedListIterator.java
@@ -2,15 +2,10 @@ package leetcode;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Stack;
+import java.util.*;
 
+import leetcode.util.CollectionUtils;
 import org.junit.Test;
-
-import com.google.common.collect.Lists;
 
 import leetcode.common.NestedInteger;
 
@@ -27,42 +22,42 @@ public class FlattenNestedListIterator {
   By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,4,6]. 
   */
 
-  @Test
-  public void test() {
-    assertEquals(Arrays.asList(1, 1, 2, 1, 1),
-        Lists.newArrayList(new NestedIterator(NestedInteger.fromString("[[1,1],2,[1,1]]").getList())));
-    assertEquals(Arrays.asList(1, 4, 6),
-        Lists.newArrayList(new NestedIterator(NestedInteger.fromString("[1,[4,[6]]]").getList())));
-  }
-
-  public class NestedIterator implements Iterator<Integer> {
-    private Integer next;
-    private Stack<Iterator<NestedInteger>> stack = new Stack<>();
-
-    public NestedIterator(List<NestedInteger> nestedList) {
-      if (nestedList != null) stack.push(nestedList.iterator());
-      getNext();
+    @Test
+    public void test() {
+        assertEquals(Arrays.asList(1, 1, 2, 1, 1),
+                CollectionUtils.asList(new NestedIterator(NestedInteger.fromString("[[1,1],2,[1,1]]").getList())));
+        assertEquals(Arrays.asList(1, 4, 6),
+                CollectionUtils.asList(new NestedIterator(NestedInteger.fromString("[1,[4,[6]]]").getList())));
     }
 
-    private void getNext() {
-      next = null;
-      for (NestedInteger i; next == null && !stack.isEmpty();)
-        if (!stack.peek().hasNext()) stack.pop();
-        else if ((i = stack.peek().next()).isInteger()) next = i.getInteger();
-        else stack.push(i.getList().iterator());
-    }
+    public static class NestedIterator implements Iterator<Integer> {
+        private Integer next;
+        private final Stack<Iterator<NestedInteger>> stack = new Stack<>();
 
-    @Override
-    public Integer next() {
-      if (next == null) throw new NoSuchElementException();
-      Integer val = next;
-      getNext();
-      return val;
-    }
+        public NestedIterator(List<NestedInteger> nestedList) {
+            if (nestedList != null) stack.push(nestedList.iterator());
+            getNext();
+        }
 
-    @Override
-    public boolean hasNext() {
-      return next != null;
+        private void getNext() {
+            next = null;
+            for (NestedInteger i; next == null && !stack.isEmpty(); )
+                if (!stack.peek().hasNext()) stack.pop();
+                else if ((i = stack.peek().next()).isInteger()) next = i.getInteger();
+                else stack.push(i.getList().iterator());
+        }
+
+        @Override
+        public Integer next() {
+            if (next == null) throw new NoSuchElementException();
+            Integer val = next;
+            getNext();
+            return val;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return next != null;
+        }
     }
-  }
 }
