c5f579b405b0795dab0f45eb547b73851644d9c7
udsjz3o <wenlong.wang@mobilityasia.com.cn>
2024-04-29 13:36:27 +0800

    use java api instead of guava

--- src/leetcode/BinaryTreeVerticalOrderTraversal.java
+++ src/leetcode/BinaryTreeVerticalOrderTraversal.java
@@ -3,16 +3,10 @@ package leetcode;
 import static leetcode.common.TreeNode.tree;
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import org.junit.Test;
-
-import com.google.common.collect.Lists;
 
 import leetcode.common.Pair;
 import leetcode.common.TreeNode;
@@ -58,31 +52,33 @@ public class BinaryTreeVerticalOrderTraversal {
   ]  
   */
 
-  @Test
-  public void test() {
-    assertEquals(Arrays.asList(Arrays.asList(9), Arrays.asList(3, 15), Arrays.asList(20), Arrays.asList(7)),
-        verticalOrder(tree("3,9,n,n,20,15,n,n,7,n,n")));
-    assertEquals(
-        Arrays.asList(Arrays.asList(4), Arrays.asList(9), Arrays.asList(3, 5, 2), Arrays.asList(20), Arrays.asList(7)),
-        verticalOrder(tree("3,9,4,n,n,5,n,n,20,2,n,n,7,n,n")));
-  }
+    @Test
+    public void test() {
+        assertEquals(List.of(List.of(9), List.of(3, 15), List.of(20), List.of(7)),
+                verticalOrder(tree("3,9,n,n,20,15,n,n,7,n,n")));
+        assertEquals(
+                Arrays.asList(List.of(4), List.of(9), List.of(3, 5, 2), List.of(20), List.of(7)),
+                verticalOrder(tree("3,9,4,n,n,5,n,n,20,2,n,n,7,n,n")));
+    }
 
-  public List<List<Integer>> verticalOrder(TreeNode root) {
-    Deque<Pair<Integer, List<Integer>>> result = new ArrayDeque<>();
-    preOrder(root, result, 0);
-    return result.stream().map(pair -> pair.value).collect(Collectors.toList());
-  }
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        Deque<Pair<Integer, List<Integer>>> result = new ArrayDeque<>();
+        preOrder(root, result, 0);
+        return result.stream().map(pair -> pair.value).collect(Collectors.toList());
+    }
 
-  private void preOrder(TreeNode root, Deque<Pair<Integer, List<Integer>>> result, int idx) {
-    if (Objects.isNull(root)) return;
-    if (result.isEmpty()) result.add(new Pair<Integer, List<Integer>>(idx, Lists.newArrayList(root.val)));
-    else if (idx < result.peekFirst().key)
-      result.addFirst(new Pair<Integer, List<Integer>>(idx, Lists.newArrayList(root.val)));
-    else if (idx > result.peekLast().key)
-      result.addLast(new Pair<Integer, List<Integer>>(idx, Lists.newArrayList(root.val)));
-    else result.stream().filter(pair -> idx == pair.key).findFirst().get().value.add(root.val);
-    preOrder(root.left, result, idx - 1);
-    preOrder(root.right, result, idx + 1);
-  }
+    private void preOrder(TreeNode root, Deque<Pair<Integer, List<Integer>>> result, int idx) {
+        if (Objects.isNull(root)) return;
+        if (result.isEmpty()) result.add(new Pair<>(idx, new ArrayList<>(List.of(root.val))));
+        else if (idx < result.peekFirst().key)
+            result.addFirst(new Pair<>(idx, new ArrayList<>(List.of(root.val))));
+        else if (idx > result.peekLast().key)
+            result.addLast(new Pair<>(idx, new ArrayList<>(List.of(root.val))));
+        else result.stream().filter(pair -> idx == pair.key)
+                    .findFirst()
+                    .ifPresent(pair -> pair.value.add(root.val));
+        preOrder(root.left, result, idx - 1);
+        preOrder(root.right, result, idx + 1);
+    }
 
 }
