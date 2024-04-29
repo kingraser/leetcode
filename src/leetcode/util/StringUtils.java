dd2dd03b3efd23ab6448c42e4bab29f79ff2c421
udsjz3o <wenlong.wang@mobilityasia.com.cn>
2024-04-29 13:03:11 +0800

    add StringUtils.java to replace same Util in apache

---
+++ src/leetcode/util/StringUtils.java
@@ -0,0 +1,36 @@
+package leetcode.util;
+
+public class StringUtils {
+    public static boolean isAnyBlank(final CharSequence... css) {
+        if (ArrayUtil.isEmpty(css)) {
+            return false;
+        }
+        for (final CharSequence cs : css) {
+            if (isBlank(cs)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isBlank(final CharSequence cs) {
+        final int strLen = length(cs);
+        if (strLen == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if (!Character.isWhitespace(cs.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int length(final CharSequence cs) {
+        return cs == null ? 0 : cs.length();
+    }
+
+    public static boolean isEmpty(final CharSequence cs) {
+        return cs == null || cs.isEmpty();
+    }
+}
