04aad8467420263da4b90c33f20238d6886ca396
udsjz3o <wenlong.wang@mobilityasia.com.cn>
2024-04-29 14:07:15 +0800

    create CollectionUtils.java to convert iterator to list

--- src/leetcode/FourSum.java
+++ src/leetcode/FourSum.java
@@ -8,8 +8,6 @@ import java.util.List;
 
 import org.junit.Test;
 
-import com.google.common.collect.Lists;
-
 public class FourSum {
 
   /*
@@ -27,48 +25,50 @@ public class FourSum {
   ]  
   */
 
-  @Test
-  public void test() {
-    List<List<Integer>> expected = Arrays.asList(Arrays.asList(-2, -1, 1, 2), Arrays.asList(-2, 0, 0, 2),
-        Arrays.asList(-1, 0, 0, 1));
-    assertEquals(expected, fourSum(new int[] { 1, 0, -1, 0, -2, 2 }, 0));
+    @Test
+    public void test() {
+        List<List<Integer>> expected = List.of(List.of(-2, -1, 1, 2), List.of(-2, 0, 0, 2),
+                List.of(-1, 0, 0, 1));
+        assertEquals(expected, fourSum(new int[]{1, 0, -1, 0, -2, 2}, 0));
 
-    expected = Arrays.asList(Arrays.asList(0, 0, 0, 0));
-    assertEquals(expected, fourSum(new int[] { 0, 0, 0, 0, 0, 0 }, 0));
-  }
+        expected = List.of(List.of(0, 0, 0, 0));
+        assertEquals(expected, fourSum(new int[]{0, 0, 0, 0, 0, 0}, 0));
+    }
 
-  public List<List<Integer>> fourSum(int[] nums, int target) {
-    Arrays.sort(nums);
-    return kSum(nums, target, 4, 0);
-  }
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        return kSum(nums, target, 4, 0);
+    }
 
-  /**
-   * @param nums sorted array
-   * @param target sum
-   * @param k k-sum
-   * @param start start index
-   * @return list of k pairs
-   */
-  public static List<List<Integer>> kSum(int[] nums, int target, int k, int start) {
-    if (k == 2) return TwoSum(nums, target, start);
-    List<List<Integer>> result = new ArrayList<>();
-    for (int i = start; i < nums.length - k; i++)
-      if (i > start && nums[i - 1] == nums[i]) continue;
-      else for (List<Integer> list : kSum(nums, target - nums[i], k - 1, i + 1)) {
-        list.add(0, nums[i]);
-        result.add(list);
-      }
-    return result;
-  }
+    /**
+     * @param nums   sorted array
+     * @param target sum
+     * @param k      k-sum
+     * @param start  start index
+     * @return list of k pairs
+     */
+    public static List<List<Integer>> kSum(int[] nums, int target, int k, int start) {
+        if (k == 2) return TwoSum(nums, target, start);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = start; i < nums.length - k; i++) {
+            if (i > start && nums[i - 1] == nums[i]) continue;
+            for (List<Integer> list : kSum(nums, target - nums[i], k - 1, i + 1)) {
+                list.addFirst(nums[i]);
+                result.add(list);
+            }
+        }
+        return result;
+    }
 
-  private static List<List<Integer>> TwoSum(int[] nums, int target, int start) {
-    List<List<Integer>> result = new ArrayList<>();
-    for (int left = start, right = nums.length - 1, value; left < right;)
-      if (left > start && nums[left - 1] == nums[left]) left++;
-      else if ((value = nums[left] + nums[right]) == target) result.add(Lists.newArrayList(nums[left++], nums[right]));
-      else if (value < target) left++;
-      else right--;
-    return result;
-  }
+    private static List<List<Integer>> TwoSum(int[] nums, int target, int start) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int left = start, right = nums.length - 1, value; left < right; )
+            if (left > start && nums[left - 1] == nums[left]) left++;
+            else if ((value = nums[left] + nums[right]) == target)
+                result.add(new ArrayList<>(List.of(nums[left++], nums[right])));
+            else if (value < target) left++;
+            else right--;
+        return result;
+    }
 
 }
