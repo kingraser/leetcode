968f15b0793a2c928d4b9addd3e9dde031cdff84
udsjz3o <wenlong.wang@mobilityasia.com.cn>
2024-04-29 13:32:18 +0800

    use java api instead of guava

--- src/leetcode/BasicCalculatorII.java
+++ src/leetcode/BasicCalculatorII.java
@@ -8,8 +8,6 @@ import java.util.function.BiFunction;
 
 import org.junit.Test;
 
-import com.google.common.collect.ImmutableMap;
-
 public class BasicCalculatorII {
   /*
   implement a basic calculator to evaluate a simple expression string.
@@ -24,36 +22,47 @@ public class BasicCalculatorII {
   " 3+5 / 2 " = 5
   */
 
-  public static final Map<Character, BiFunction<Integer, Integer, Integer>> OPERATOR_MAP = ImmutableMap.of('+',
-      (a, b) -> b + a, '-', (a, b) -> b - a, '*', (a, b) -> b * a, '/', (a, b) -> b / a);
+    public static final Map<Character, BiFunction<Integer, Integer, Integer>> OPERATOR_MAP = Map.of(
+            '+', Integer::sum,
+            '-', (a, b) -> b - a,
+            '*', (a, b) -> b * a,
+            '/', (a, b) -> b / a);
 
-  @Test
-  public void test() {
-    assertEquals(27, calculate("100000000/1/2/3/4/5/6/7/8/9/10"));
-    assertEquals(7, calculate("3+2*2"));
-    assertEquals(1, calculate(" 3/2 "));
-    assertEquals(5, calculate(" 3+5 / 2 "));
-  }
+    @Test
+    public void test() {
+        assertEquals(27, calculate("100000000/1/2/3/4/5/6/7/8/9/10"));
+        assertEquals(7, calculate("3+2*2"));
+        assertEquals(1, calculate(" 3/2 "));
+        assertEquals(5, calculate(" 3+5 / 2 "));
+    }
 
-  public int calculate(String s) {
-    int num = 0;
-    Stack<Integer> operators = new Stack<>(), nums = new Stack<>();
-    for (char c : s.toCharArray())
-      if (c == ' ') continue;
-      else if ('0' <= c && c <= '9') num = num * 10 + c - '0';
-      else {
-        for (nums.push(num), num = 0; !hasHigherPriority(c, operators); calculate(nums, operators));
-        operators.push((int) c);
-      }
-    for (nums.push(num); nums.size() != 1; calculate(nums, operators));
-    return nums.peek();
-  }
+    public int calculate(String s) {
+        int num = 0;
+        Stack<Character> operators = new Stack<>();
+        Stack<Integer> numStack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == ' ') continue;
+            if ('0' <= c && c <= '9') num = num * 10 + c - '0';
+            else {
+                numStack.push(num);
+                num = 0;
+                while (!hasHigherPriority(c, operators)) calculate(numStack, operators);
+                operators.push(c);
+            }
+        }
+        numStack.push(num);
+        while (numStack.size() != 1) calculate(numStack, operators);
+        return numStack.peek();
+    }
 
-  private void calculate(Stack<Integer> nums, Stack<Integer> operators) {
-    nums.push(OPERATOR_MAP.get((char) (operators.pop().intValue())).apply(nums.pop(), nums.pop()));
-  }
+    private void calculate(Stack<Integer> numStack, Stack<Character> operators) {
+        numStack.push(OPERATOR_MAP
+                .get(operators.pop())
+                .apply(numStack.pop(), numStack.pop()));
+    }
 
-  private boolean hasHigherPriority(char c, Stack<Integer> operators) {
-    return operators.isEmpty() || ((c == '*' || c == '/') && (operators.peek() == '+' || operators.peek() == '-'));
-  }
+    private boolean hasHigherPriority(char c, Stack<Character> operators) {
+        return operators.isEmpty()
+                || ((c == '*' || c == '/') && (operators.peek() == '+' || operators.peek() == '-'));
+    }
 }
