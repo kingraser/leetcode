027dd0e2095a61eb74e65105730290fe8fdeade7
wit <kingraser@gmail.com>
2024-05-09 17:05:50 +0800

    Bit manipulation

---
+++ src/leetcode/MinimumArrayEnd.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+import leetcode.util.TestUtil;
+import org.junit.Test;
+
+/**
+ * @author Wit
+ */
+public class MinimumArrayEnd {
+	/*
+	You are given two integers n and x. You have to construct an array of positive integers nums of size n where for every 0 <= i < n - 1, nums[i + 1] is greater than nums[i], and the result of the bitwise AND operation between all elements of nums is x.
+	Return the minimum possible value of nums[n - 1].
+
+	Example 1:
+	Input: n = 3, x = 4
+	Output: 6
+	Explanation:
+	nums can be [4,5,6] and its last element is 6.
+
+	Example 2:
+	Input: n = 2, x = 7
+	Output: 15
+	Explanation:
+	nums can be [7,15] and its last element is 15.
+
+	Constraints:
+	    1 <= n, x <= 10^8
+	*/
+
+	@Test
+	public void test() {
+		TestUtil.testEquals(new Object[][]{
+				{6L, 3, 4},
+				{15L, 2, 7},
+		});
+	}
+
+	public long minEnd(int n, int x) {
+		long result = x;
+		for (long i = 1, nMinusOne = --n; nMinusOne > 0; nMinusOne >>= 1, i <<= 1) {
+			while ((i & x) != 0) i <<= 1;
+			if ((nMinusOne & 1) == 1) result |= i;
+		}
+		return result;
+	}
+}
