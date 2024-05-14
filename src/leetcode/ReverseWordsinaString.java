3d43820e6c627b5f25f779ab6ff2f2f51138356e
wit <kingraser@gmail.com>
2024-05-14 15:22:07 +0800

    Array

--- src/leetcode/ReverseWordsinaString.java
+++ src/leetcode/ReverseWordsinaString.java
@@ -1,10 +1,9 @@
 package leetcode;
 
-import static org.junit.Assert.assertEquals;
-
+import leetcode.util.TestUtil;
 import org.junit.Test;
 
-import leetcode.util.ArrayUtil;
+import static leetcode.util.ArrayUtil.reverse;
 
 public class ReverseWordsinaString {
   /*
@@ -15,18 +14,54 @@ public class ReverseWordsinaString {
   return "blue is sky the". 
   */
 
-  @Test
-  public void test() {
-    assertEquals("", reverseWords("  "));
-    assertEquals("blue is sky the", reverseWords("the sky is blue"));
-    assertEquals("1", reverseWords("1"));
-    assertEquals("a", reverseWords(" a "));
-  }
+	@Test
+	public void test() {
+		TestUtil.testEquals(new Object[][]{
+				{"", " "},
+				{"blue is sky the", "the sky is blue"},
+				{"1", "1"},
+				{"a", " a "},
+		});
+	}
 
-  public static String reverseWords(String s) {
-    String[] result = s.trim().split(" +");
-    ArrayUtil.reverse(result);
-    return String.join(" ", result);
-  }
+	public String reverseWords(String s) {
+		String[] result = s.trim().split(" +");
+		reverse(result);
+		return String.join(" ", result);
+	}
+
+	static final char EMPTY = ' ';
+
+	public String reverseWordsII(String s) {
+		char[] result = s.toCharArray();
+		int last = result.length - 1, end = last;
+		for (int i = end, wordStart, wordEnd; i >= 0; end -= wordEnd - wordStart + 2) {
+			while (i >= 0 && result[i] == EMPTY) i--;
+			wordEnd = i--;
+			while (i >= 0 && result[i] != EMPTY) i--;
+			if ((wordStart = i-- + 1) < 0) break;
+			reverse(result, wordStart, end);
+		}
+		if (end == last) return "";
+		reverse(result, end += 2, result.length - 1);
+		return new String(result, end, result.length - end);
+	}
+
+	public String reverseWordsIII(String s) {
+		int rIdx = s.length() - 1, sIdx = 0, size = rIdx;
+		char[] result = new char[s.length()], stack = new char[s.length()], array = s.toCharArray();
+		for (char c : array)
+			if (c == ' ') {
+				if (sIdx == 0) continue;
+				if (rIdx != size) result[rIdx--] = ' ';
+				for (; sIdx > 0; result[rIdx--] = stack[--sIdx]);
+				sIdx = 0;
+			} else stack[sIdx++] = c;
+		if (sIdx != 0) {
+			if (rIdx != size) result[rIdx--] = ' ';
+			for (; sIdx > 0; result[rIdx--] = stack[--sIdx]);
+		}
+		return new String(result, ++rIdx, s.length() - rIdx);
+	}
 
 }
