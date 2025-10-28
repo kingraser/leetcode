e12d837a46534079878251bd421cdf8b61a9747e
wit <kingraser@gmail.com>
2025-10-28 16:07:09 +0800

    Array

---
+++ src/leetcode/NumberOfSubArraysOfSizeKAndAverageGreaterThanOrEqualToThreshold.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+import leetcode.util.TestUtil;
+import org.junit.Test;
+
+public class NumberOfSubArraysOfSizeKAndAverageGreaterThanOrEqualToThreshold {
+    /*
+    Given an array of integers arr and two integers k and threshold, return the number of sub-arrays of size k and average greater than or equal to threshold.
+
+    Example 1:
+    Input: arr = [2,2,2,2,5,5,5,8], k = 3, threshold = 4
+    Output: 3
+    Explanation: Sub-arrays [2,5,5],[5,5,5] and [5,5,8] have averages 4, 5 and 6 respectively. All other sub-arrays of size 3 have averages less than 4 (the threshold).
+
+    Example 2:
+    Input: arr = [11,13,17,23,29,31,7,5,2,3], k = 3, threshold = 5
+    Output: 6
+    Explanation: The first 6 sub-arrays of size 3 have averages greater than 5. Note that averages are not integers.
+
+    Constraints:
+    1 <= arr.length <= 10^5
+    1 <= arr[i] <= 10^4
+    1 <= k <= arr.length
+    0 <= threshold <= 10^4
+    */
+    @Test
+    public void test() {
+        TestUtil.testEquals(new Object[][]{
+                {3, new int[]{2, 2, 2, 2, 5, 5, 5, 8}, 3, 4},
+                {6, new int[]{11, 13, 17, 23, 29, 31, 7, 5, 2, 3}, 3, 5},
+        });
+    }
+
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int result = 0, limit = threshold * k, sum = 0;
+        for (int i = 0; i < k; ) sum += arr[i++];
+        if (sum >= limit) result++;
+        for (int i = k; i < arr.length; ) if ((sum += arr[i] - arr[i++ - k]) >= limit) result++;
+        return result;
+    }
+}
