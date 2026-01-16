cdb5f9a82208b72a3b7d36a28ea21190402ee6b2
wit <kingraser@gmail.com>
2026-01-16 10:20:38 +0800

    String

---
+++ src/leetcode/LargestEvenNumber.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import leetcode.util.TestUtil;
+import org.junit.Test;
+
+public class LargestEvenNumber {
+    /*
+    You are given a string s consisting only of the characters '1' and '2'.
+    You may delete any number of characters from s without changing the order of the remaining characters.
+    Return the largest possible resultant string that represents an even integer. If there is no such string, return the empty string "".
+
+    Example 1:
+    Input: s = "1112"
+    Output: "1112"
+    Explanation:
+    The string already represents the largest possible even number, so no deletions are needed.
+
+    Example 2:
+    Input: s = "221"
+    Output: "22"
+    Explanation:
+    Deleting '1' results in the largest possible even number which is equal to 22.
+
+    Example 3:
+    Input: s = "1"
+    Output: ""
+    Explanation:
+    There is no way to get an even number.
+
+    Constraints:
+        1 <= s.length <= 100
+        s consists only of the characters '1' and '2'.
+    */
+    @Test
+    public void test() {
+        TestUtil.testEquals(new Object[][]{
+                {"1112", "1112"},
+                {"22", "221"},
+                {"", "1"}
+        });
+    }
+
+    public String largestEven(String s) {
+        byte[] result = s.getBytes();
+        int size = result.length;
+        while (size > 0 && result[size - 1] == '1') size--;
+        return new String(result, 0, size);
+    }
+}
